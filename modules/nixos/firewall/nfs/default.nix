f195167b1713a0634f5e7b9f62b549b32255c0b8
ourgal <git@fairever.aleeas.com>
2024-07-10 09:28:08 +0800

    open firewall for nfs

---
+++ modules/nixos/firewall/nfs/default.nix
@@ -0,0 +1,32 @@
+{ ... }@args:
+let
+  inherit (args) namespace lib;
+  inherit (lib.${namespace}) nixosModule;
+  ports = [
+    # rpcinfo -p
+    111 # portmapper
+    2049 # nfs
+    20048 # mountd
+  ];
+  value = {
+    networking.firewall = {
+      allowedTCPPorts = (
+        ports
+        ++ [
+          36789 # nlockmgr
+        ]
+      );
+      allowedUDPPorts = (
+        ports
+        ++ [
+          54598 # nlockmgr
+        ]
+      );
+    };
+  };
+  path = ./.;
+  _args = {
+    inherit value path args;
+  };
+in
+nixosModule _args
