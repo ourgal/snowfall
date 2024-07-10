af826de5d262a22c6c313c111ae78779fbd55b97
ourgal <git@fairever.aleeas.com>
2024-07-10 15:15:58 +0800

    add plymouth

---
+++ modules/nixos/plymouth/default.nix
@@ -0,0 +1,20 @@
+{ ... }@args:
+let
+  inherit (args) namespace lib pkgs;
+  inherit (lib.${namespace}) nixosModule;
+  value = {
+    boot = {
+      kernelParams = [ "quiet" ];
+      plymouth = {
+        enable = true;
+        theme = "catppuccin-macchiato";
+        themePackages = [ pkgs.catppuccin-plymouth ];
+      };
+    };
+  };
+  path = ./.;
+  _args = {
+    inherit value path args;
+  };
+in
+nixosModule _args
