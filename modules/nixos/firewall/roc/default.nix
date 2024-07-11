6ee34cf168be219640d53b435464d5a1edd12de5
ourgal <git@fairever.aleeas.com>
2024-07-11 13:00:39 +0800

    open firewall for roc

---
+++ modules/nixos/firewall/roc/default.nix
@@ -0,0 +1,20 @@
+{ ... }@args:
+let
+  inherit (args) namespace lib;
+  inherit (lib.${namespace}) nixosModule;
+  ports = [
+    10001
+    10002
+  ];
+  value = {
+    networking.firewall = {
+      allowedTCPPorts = ports;
+      allowedUDPPorts = ports;
+    };
+  };
+  path = ./.;
+  _args = {
+    inherit value path args;
+  };
+in
+nixosModule _args
