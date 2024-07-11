618d94ccf6c125a3d015e338174b57fca7c90099
ourgal <git@fairever.aleeas.com>
2024-07-11 09:03:35 +0800

    open firewall for kdeconnect

---
+++ modules/nixos/firewall/kdeconnect/default.nix
@@ -0,0 +1,22 @@
+{ ... }@args:
+let
+  inherit (args) namespace lib;
+  inherit (lib.${namespace}) nixosModule;
+  range = [
+    {
+      from = 1714;
+      to = 1764;
+    }
+  ];
+  value = {
+    networking.firewall = {
+      allowedTCPPortRanges = range;
+      allowedUDPPortRanges = range;
+    };
+  };
+  path = ./.;
+  _args = {
+    inherit value path args;
+  };
+in
+nixosModule _args
