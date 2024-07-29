47c31e225b8a15be7a24279f3088ca4956dc5e74
ourgal <git@fairever.aleeas.com>
2024-07-30 07:21:28 +0800

    add emacs vterm

--- modules/home/desktop/editor/emacs/default.nix
+++ modules/home/desktop/editor/emacs/default.nix
@@ -3,7 +3,11 @@ args.module (
   args
   // {
     path = ./.;
-    progs = "emacs";
+    progs = {
+      emacs = {
+        extraPackages = epkgs: [ epkgs.vterm ];
+      };
+    };
     servs = "emacs";
     value = with args; {
       systemd.user.services.emacs.Service.ExecStartPre = "${pkgs.coreutils-full}/bin/sleep 10";
