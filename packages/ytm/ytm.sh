b701285987da15c1fbc5ee668ffd6679644b670f
ourgal <git@fairever.aleeas.com>
2024-08-01 01:34:46 +0800

    avoid downloading subs for ytm

--- packages/ytm/ytm.sh
+++ packages/ytm/ytm.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-yt-dlp --trim-filenames 50 -P ~/Music -f 'ba' -x --audio-format mp3 "$1"
+dir=~/Music/youtube_dl/
+mkdir -p $dir
+yt-dlp --trim-filenames 50 --no-write-subs --no-embed-subs --no-write-auto-subs -P $dir -f 'ba' -x --audio-format mp3 "$1"
