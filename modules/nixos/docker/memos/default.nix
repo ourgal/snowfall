a567304527a9acecaa7145072c25da52304f0177
ourgal <git@fairever.aleeas.com>
2024-08-03 12:27:17 +0800

    feat: add memos docker

---
+++ modules/nixos/docker/memos/default.nix
@@ -0,0 +1,46 @@
+{ ... }@args:
+let
+  inherit (args) namespace lib config;
+  inherit (lib.${namespace}) nixosModule cfgNixos mkOpt';
+  cfg = cfgNixos config.${namespace} ./.;
+  value = {
+    virtualisation.arion.projects.${cfg.name}.settings = {
+      services.${cfg.name}.service = {
+        name = cfg.name;
+        image = "docker.io/neosmemo/memos:${cfg.version}";
+        ports = [ "${toString cfg.ports}:5230" ];
+        volumes = [ "config:/var/opt/memos" ];
+        restart = "unless-stopped";
+        networks = [ "proxy" ];
+      };
+      networks.proxy.name = "proxy";
+      docker-compose.volumes = {
+        config = {
+          driver_opts = {
+            type = "nfs";
+            o = "addr=${cfg.nfs},nfsvers=4";
+            device = ":${cfg.nfsPath}/${cfg.name}_config";
+          };
+        };
+      };
+    };
+  };
+  extraOpts = with lib.types; {
+    name = mkOpt' str "memos";
+    ports = mkOpt' port 5230;
+    nfs = mkOpt' str "";
+    nfsPath = mkOpt' str "/docker";
+    mount = mkOpt' str "";
+    version = mkOpt' str "latest";
+  };
+  path = ./.;
+  _args = {
+    inherit
+      value
+      path
+      args
+      extraOpts
+      ;
+  };
+in
+nixosModule _args
