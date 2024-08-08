de7f93992c5267e05194770955628f6cf0e8c856
ourgal <git@fairever.aleeas.com>
2024-08-08 13:02:04 +0800

    revert: disable sponsorblock of yt-dlp

--- modules/home/cli/download/yt-dlp/default.nix
+++ modules/home/cli/download/yt-dlp/default.nix
@@ -11,7 +11,6 @@ args.module (
       yt-dlp = {
         settings = {
           format = "bestvideo[ext=mp4][width<2000][height<=1200]+bestaudio[ext=m4a]/bestvideo[ext=webm][width<2000][height<=1200]+bestaudio[ext=webm]/bestvideo[width<2000][height<=1200]+bestaudio/best[width<2000][height<=1200]/best";
-          sponsorblock-remove = "default";
           concurrent-fragments = 5;
           trim-filenames = 20;
           embed-subs = true;
