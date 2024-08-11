65d096b6c9b3b06f735610b0a99741ed9ad281b1
ourgal <git@fairever.aleeas.com>
2024-08-11 19:18:02 +0800

    feat: add ffsend

--- modules/home/cli/system/share/default.nix
+++ modules/home/cli/system/share/default.nix
@@ -5,17 +5,22 @@ args.module (
     path = ./.;
     pyPkgs = "qrcode";
     nixPkgs = [
+      # keep-sorted start
       "croc"
       "dufs"
+      "ffsend"
       "nfs-utils"
       "qrcp"
       "rsync"
       "sshfs"
+      # keep-sorted end
     ];
     enable = [
+      # keep-sorted start
       "pbgopy"
       "rclone"
       "syncthing"
+      # keep-sorted end
     ];
   }
 )
