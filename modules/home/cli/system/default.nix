44599a3e225288909111e02c3580431009d1eac8
ourgal <git@fairever.aleeas.com>
2024-08-12 00:39:35 +0800

    revert: disable cert

--- modules/home/cli/system/default.nix
+++ modules/home/cli/system/default.nix
@@ -4,6 +4,7 @@ args.module (
   // {
     path = ./.;
     nixPkgs = [
+      # keep-sorted start
       "bluetuith"
       "charm-freeze"
       "fastfetch"
@@ -16,23 +17,27 @@ args.module (
       "ventoy-bin-full"
       "wget"
       "xdotool"
+      # keep-sorted end
     ];
     myPkgs = [
+      # keep-sorted start
       "archwiki"
       "c"
+      # keep-sorted end
     ];
     # nvtop = "full";
     enable = [
+      # keep-sorted start
       "clipboard"
+      "disk"
       "file-manager"
-      "cert"
       "monitor"
       "notification"
       "share"
       "ssh"
-      "disk"
       "utils"
       "zip"
+      # keep-sorted end
     ];
   }
 )
