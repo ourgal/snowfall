d0f2b9e9f640bdf9bea65cc551cb71b5adc711f0
ourgal <git@fairever.aleeas.com>
2024-08-12 12:41:17 +0800

    feat: add duckdns plugin to caddy

--- lib/caddy/default.nix
+++ lib/caddy/default.nix
@@ -10,7 +10,7 @@ with lib;
     else
       "";
 
-  # virtualHosts."http://alist.zxc.cn".extraConfig =''
+  # "http://alist.zxc.cn".extraConfig =''
   #   reverse_proxy http://brix.local:${toString config.dot.docker.alist.port}
   # '';
   mkDockerProxy =
@@ -31,8 +31,37 @@ with lib;
         acc // { "http://${name}.zxc.cn".extraConfig = "reverse_proxy http://${host}:${toString ports}"; }
       ) { } containerEnabled;
     in
+    configs;
+
+  mkDockerProxyDuckdns =
     {
-      enable = true;
-      virtualHosts = configs;
-    };
+      docker,
+      host,
+      domain,
+      token,
+    }:
+    let
+      containerEnabled = attrsets.filterAttrs (n: v: (n != "enable") && (v.enable == true)) docker;
+      configs = attrsets.foldlAttrs (
+        acc: name: value:
+        let
+          ports =
+            if builtins.isInt value.ports then
+              value.ports
+            else if builtins.isList value.ports then
+              builtins.head value.ports
+            else
+              builtins.throw "not supported port type";
+        in
+        acc
+        // {
+          "${name}.${domain}.duckdns.org".extraConfig = ''
+            tls {
+                dns duckdns ${token}
+            }
+            reverse_proxy http://${host}:${toString ports}'';
+        }
+      ) { } containerEnabled;
+    in
+    configs;
 }
