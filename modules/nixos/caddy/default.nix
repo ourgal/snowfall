cab8882388abda0e104dbb628f17da8c63995a3b
ourgal <git@fairever.aleeas.com>
2024-08-13 22:16:25 +0800

    chore: add xcaddy option for caddy

--- modules/nixos/caddy/default.nix
+++ modules/nixos/caddy/default.nix
@@ -6,51 +6,71 @@ let
     pkgs
     config
     ;
-  inherit (lib.${namespace}) nixosModule mkDockerProxy enabled;
+  inherit (lib.${namespace})
+    nixosModule
+    mkDockerProxy
+    enabled
+    cfgNixos
+    switch
+    ;
+  cfg = cfgNixos config.${namespace} ./.;
   docker = config.${namespace}.docker;
   host = "brix.local";
   token = lib.strings.fileContents ./token.key;
   domain = lib.strings.fileContents ./domain.key;
-  value = {
-    services.caddy =
-      enabled
+  virtualHosts =
+    if cfg.xcaddy.enable then
+      (mkDockerProxy { inherit docker host; })
       // {
-        virtualHosts = (mkDockerProxy { inherit docker host; }) // {
-          "alist.${domain}.duckdns.org".extraConfig = ''
-            tls {
-                dns duckdns ${token}
-            }
-            reverse_proxy brix.local:5244
-          '';
-        };
+        "alist.${domain}.duckdns.org".extraConfig = ''
+          tls {
+              dns duckdns ${token}
+          }
+          reverse_proxy brix.local:5244
+        '';
       }
-      // {
-        package = (
-          pkgs.callPackage
-            "${builtins.fetchurl {
-              url = "https://raw.githubusercontent.com/jpds/nixpkgs/a33b02fa9d664f31dadc8a874eb1a5dbaa9f4ecf/pkgs/servers/caddy/default.nix";
-              sha256 = "1x1g6qyhmclz2jyc5nmfjsri3xx4pw5rd15n2xjkxlgdcvywcv5f";
-            }}"
+    else
+      mkDockerProxy { inherit docker host; };
+  package =
+    if cfg.xcaddy.enable then
+      (pkgs.callPackage
+        "${builtins.fetchurl {
+          url = "https://raw.githubusercontent.com/jpds/nixpkgs/a33b02fa9d664f31dadc8a874eb1a5dbaa9f4ecf/pkgs/servers/caddy/default.nix";
+          sha256 = "1x1g6qyhmclz2jyc5nmfjsri3xx4pw5rd15n2xjkxlgdcvywcv5f";
+        }}"
+        {
+          externalPlugins = [
             {
-              externalPlugins = [
-                {
-                  name = "duckdns";
-                  repo = "github.com/caddy-dns/duckdns";
-                  version = "v0.4.0";
-                }
-              ];
-              vendorHash = "sha256-UBn/jQIoi9yq3ZLd+LvDM7PUE0kqP0GKTU+oQIdHztw="; # Add this, as explained in https://github.com/NixOS/nixpkgs/pull/259275#issuecomment-1763478985
+              name = "duckdns";
+              repo = "github.com/caddy-dns/duckdns";
+              version = "v0.4.0";
             }
-        );
-      };
+          ];
+          vendorHash = "sha256-UBn/jQIoi9yq3ZLd+LvDM7PUE0kqP0GKTU+oQIdHztw="; # Add this, as explained in https://github.com/NixOS/nixpkgs/pull/259275#issuecomment-1763478985
+        }
+      )
+    else
+      pkgs.caddy;
+  value = {
+    services.caddy = enabled // {
+      inherit virtualHosts package;
+    };
     networking.firewall.allowedTCPPorts = [
       80
       443
     ];
   };
+  extraOpts = {
+    xcaddy = switch;
+  };
   path = ./.;
   _args = {
-    inherit value path args;
+    inherit
+      value
+      path
+      args
+      extraOpts
+      ;
   };
 in
 nixosModule _args
