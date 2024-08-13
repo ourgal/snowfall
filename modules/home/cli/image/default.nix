acd5ac2823e48dbcaac97db78833105a61a5e47f
ourgal <git@fairever.aleeas.com>
2024-08-13 16:29:05 +0800

    feat: add menyoki

--- modules/home/cli/image/default.nix
+++ modules/home/cli/image/default.nix
@@ -4,15 +4,20 @@ args.module (
   // {
     path = ./.;
     nixPkgs = [
+      # keep-sorted start
       "exiftool"
       "graphviz"
       "imagemagick"
+      "menyoki"
       "viu"
+      # keep-sorted end
     ];
     myPkgs = [
+      # keep-sorted start
       "autocrop"
       "pik"
       "rimage"
+      # keep-sorted end
     ];
   }
 )
