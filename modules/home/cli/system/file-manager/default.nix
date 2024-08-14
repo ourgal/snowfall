88f3afd7cee41f43fd2c055c1d77154d6ef9f1b3
ourgal <git@fairever.aleeas.com>
2024-08-14 23:02:17 +0800

    feat: add sfm

--- modules/home/cli/system/file-manager/default.nix
+++ modules/home/cli/system/file-manager/default.nix
@@ -4,15 +4,20 @@ args.module (
   // {
     path = ./.;
     nixPkgs = [
+      # keep-sorted start
       "chezmoi"
       "clifm"
+      "sfm"
       "superfile"
+      # keep-sorted end
     ];
     enable = [
+      # keep-sorted start
       "lf"
       "nnn"
       "ranger"
       "yazi"
+      # keep-sorted end
     ];
   }
 )
