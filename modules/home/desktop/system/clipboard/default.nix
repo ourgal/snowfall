989ac06c15a12c8f46d861c90ad6710ef5ba85c0
ourgal <git@fairever.aleeas.com>
2024-08-18 01:21:43 +0800

    chore(clipboard): simplify clipboard enablement
    Removes "clipmenu" from the default clipboard package.
    
    This simplifies the clipboard setup and reduces the number of
    dependencies.

--- modules/home/desktop/system/clipboard/default.nix
+++ modules/home/desktop/system/clipboard/default.nix
@@ -3,11 +3,6 @@ args.module (
   args
   // {
     path = ./.;
-    enable = [
-      # keep-sorted start
-      "clipcat"
-      "clipmenu"
-      # keep-sorted end
-    ];
+    enable = "clipcat";
   }
 )
