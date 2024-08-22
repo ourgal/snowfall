94518499fd4e329e610a690c26dce803fd282cc0
ourgal <git@fairever.aleeas.com>
2024-08-22 17:09:06 +0800

    feat: add atuin

---
+++ modules/nixos/atuin/default.nix
@@ -0,0 +1,25 @@
+{ ... }@args:
+let
+  inherit (args) namespace lib;
+  inherit (lib.${namespace}) nixosModule enabled;
+  value = {
+    services.atuin = enabled // {
+      openFirewall = true;
+      openRegistration = true;
+      host = "0.0.0.0";
+    };
+    services.postgresql = enabled;
+    services.caddy = enabled // {
+      virtualHosts = {
+        "http://atuin.zxc.cn".extraConfig = ''
+          reverse_proxy http://localhost:8888
+        '';
+      };
+    };
+  };
+  path = ./.;
+  _args = {
+    inherit value path args;
+  };
+in
+nixosModule _args
