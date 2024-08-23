8baee364fa3fdf61aa86448ae00ae749f88fcc09
ourgal <git@fairever.aleeas.com>
2024-08-23 21:44:48 +0800

    chore: update sbsrf

--- packages/sbsrf-rime-data/default.nix
+++ packages/sbsrf-rime-data/default.nix
@@ -1,11 +1,11 @@
 { fetchzip, stdenv, ... }:
 stdenv.mkDerivation rec {
   name = "sbsrf-rime-data";
-  version = "20240708";
+  version = "20240809";
 
   src = fetchzip {
     url = "https://github.com/sbsrf/sbsrf/releases/download/${version}/sbsrf.zip";
-    hash = "sha256-fx/vxIAMg0YDuaXikaSOUOk0XYNI1KAyIbOEYZtPBKQ=";
+    hash = "sha256-6XRCQP7cvK25BZdv37Yi9o+/O62Eovw7TeU7PHq225Q=";
     stripRoot = false;
   };
 
