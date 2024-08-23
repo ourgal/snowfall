f3e3a11c1d54c1f4c4cab59566199898bed26918
ourgal <git@fairever.aleeas.com>
2024-08-23 15:57:53 +0800

    feat: add ssh-tpm-agent 0.6.0

--- modules/home/cli/system/ssh/ssh-tpm-agent/default.nix
+++ modules/home/cli/system/ssh/ssh-tpm-agent/default.nix
@@ -3,47 +3,51 @@ args.module (
   args
   // {
     path = ./.;
-    nixPkgs = "ssh-tpm-agent";
-    value = with args; {
-      home.sessionVariablesExtra = ''
-        if [[ -z "$SSH_AUTH_SOCK" ]]; then
-          export SSH_AUTH_SOCK="$(ssh-tpm-agent --print-socket)"
-        fi
-      '';
+    myPkgs = "ssh-tpm-agent";
+    value =
+      let
+        ssh-tpm-agent = args.pkgs.${args.namespace}.ssh-tpm-agent;
+      in
+      {
+        home.sessionVariablesExtra = ''
+          if [[ -z "$SSH_AUTH_SOCK" ]]; then
+            export SSH_AUTH_SOCK="$(ssh-tpm-agent --print-socket)"
+          fi
+        '';
 
-      systemd.user.services.ssh-tpm-agent = {
-        Install.Also = [ "ssh-agent.socket" ];
+        systemd.user.services.ssh-tpm-agent = {
+          Install.Also = [ "ssh-agent.socket" ];
 
-        Unit = {
-          ConditionEnvironment = "!SSH_AGENT_PID";
-          Description = "ssh-tpm-agent service";
-          Documentation = "man:ssh-agent(1) man:ssh-add(1) man:ssh(1)";
-          Requires = "ssh-tpm-agent.socket";
+          Unit = {
+            ConditionEnvironment = "!SSH_AGENT_PID";
+            Description = "ssh-tpm-agent service";
+            Documentation = "man:ssh-agent(1) man:ssh-add(1) man:ssh(1)";
+            Requires = "ssh-tpm-agent.socket";
+          };
+
+          Service = {
+            Environment = "SSH_AUTH_SOCK=%t/ssh-tpm-agent.sock";
+            ExecStart = "${ssh-tpm-agent}/bin/ssh-tpm-agent";
+            PassEnvironment = "SSH_AGENT_PID";
+            SuccessExitStatus = 2;
+            Type = "simple";
+          };
         };
 
-        Service = {
-          Environment = "SSH_AUTH_SOCK=%t/ssh-tpm-agent.sock";
-          ExecStart = "${pkgs.ssh-tpm-agent}/bin/ssh-tpm-agent";
-          PassEnvironment = "SSH_AGENT_PID";
-          SuccessExitStatus = 2;
-          Type = "simple";
+        systemd.user.sockets.ssh-tpm-agent = {
+          Install.WantedBy = [ "sockets.target" ];
+
+          Unit = {
+            Description = "SSH TPM agent socket";
+            Documentation = "man:ssh-agent(1) man:ssh-add(1) man:ssh(1)";
+          };
+
+          Socket = {
+            ListenStream = "%t/ssh-tpm-agent.sock";
+            SocketMode = "0600";
+            Service = "ssh-tpm-agent.service";
+          };
         };
       };
-
-      systemd.user.sockets.ssh-tpm-agent = {
-        Install.WantedBy = [ "sockets.target" ];
-
-        Unit = {
-          Description = "SSH TPM agent socket";
-          Documentation = "man:ssh-agent(1) man:ssh-add(1) man:ssh(1)";
-        };
-
-        Socket = {
-          ListenStream = "%t/ssh-tpm-agent.sock";
-          SocketMode = "0600";
-          Service = "ssh-tpm-agent.service";
-        };
-      };
-    };
   }
 )
