8190c60b840014eb51a79db6f05edf460b69c571
ourgal <git@fairever.aleeas.com>
2024-08-24 21:03:28 +0800

    feat: add pairdrop

---
+++ modules/nixos/pairdrop/default.nix
@@ -0,0 +1,32 @@
+{ ... }@args:
+let
+  inherit (args) namespace lib pkgs;
+  inherit (lib.${namespace}) nixosModule enabled;
+  PORT = "3000";
+  value = {
+    systemd.services.pairdrop = {
+      description = "Pairdrop service";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Restart = "on-failure";
+        ExecStart = "${pkgs.pairdrop}/bin/pairdrop";
+      };
+      environment = {
+        inherit PORT;
+      };
+    };
+    services.caddy = enabled // {
+      virtualHosts = {
+        "http://pairdrop.zxc.cn".extraConfig = ''
+          reverse_proxy http://localhost:${PORT}
+        '';
+      };
+    };
+  };
+  path = ./.;
+  _args = {
+    inherit value path args;
+  };
+in
+nixosModule _args
