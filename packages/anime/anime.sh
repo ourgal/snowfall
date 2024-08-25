b99ff1fdbea43ad28c450d2c0f6b0f6a15adb28a
ourgal <git@fairever.aleeas.com>
2024-08-25 20:05:14 +0800

    chore: clean up anime script

--- packages/anime/anime.sh
+++ packages/anime/anime.sh
@@ -1,13 +1,56 @@
-#!/usr/bin/env bash
-if [ "$#" -gt 0 ] && [[ $1 =~ ^[0-9]+$ ]]; then
-  transmission-remote -t "$1" -r
-elif [ "$#" -gt 0 ] && [[ $1 == "clear" ]]; then
+function removeAllDownloads() {
   ids=$(transmission-remote --list | awk 'NR > 1 && $1 != "Sum:" && $2 == "100%" {print $1}' | sed 's/\*//')
   for id in $ids; do
     transmission-remote -t "$id" -r
   done
-elif [ "$#" -gt 0 ]; then
+}
+
+function removeDownload() {
+  transmission-remote -t "$1" -r
+}
+
+function download() {
   transmission-remote -w ~/Videos/anime --trash-torrent --add "$1"
-else
+}
+
+function watch() {
   viddy transmission-remote --list
+}
+
+function help() {
+  echo "\
+  anime <URL>
+    download
+  anime clear
+    remove all downloads
+  anime <ID>
+    remove the download by id"
+}
+
+if [ "$#" -eq 0 ]; then
+  watch
+else
+  case "$1" in
+  "clear")
+    removeAllDownloads
+    ;;
+  "")
+    viddy transmission-remote --list
+    ;;
+  "-h")
+    help
+    ;;
+  "--help")
+    help
+    ;;
+  "help")
+    help
+    ;;
+  *[!0-9]*)
+    download "$1"
+    ;;
+  *)
+    removeDownload "$1"
+    ;;
+  esac
 fi
