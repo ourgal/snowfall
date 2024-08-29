5994441cda615fd6bc9459c58ff0b61b1e966b03
ourgal <git@fairever.aleeas.com>
2024-08-29 09:47:56 +0800

    chore: add transmisson service option

--- modules/home/cli/download/transmission/default.nix
+++ modules/home/cli/download/transmission/default.nix
@@ -1,26 +1,41 @@
 { ... }@args:
 args.module (
   args
-  // {
-    path = ./.;
-    nixPkgs = "transmission_4";
-    myPkgs = "anime";
-    value = with args; {
-      systemd.user.services.transmission = {
-        Unit = {
-          Description = "Transmission Service";
-          Wants = "network-online.target";
-          After = "network-online.target";
-        };
-        Service = {
-          ExecStart = "${pkgs.transmission_4}/bin/transmission-daemon -f -g ${config.xdg.configHome}/transmission/transmission-daemon";
-          ExecReload = "${pkgs.coreutils-full}/bin/kill -HUP $MAINPID";
-          Restart = "on-abort";
-        };
-        Install = {
-          WantedBy = [ "default.target" ];
+  // (
+    let
+      inherit (args)
+        lib
+        namespace
+        config
+        pkgs
+        ;
+      inherit (lib.${namespace}) cfgHome switch;
+      cfg = cfgHome config.${namespace} ./.;
+    in
+    {
+      path = ./.;
+      nixPkgs = "transmission_4";
+      myPkgs = "anime";
+      value = {
+        systemd.user.services.transmission = lib.mkIf cfg.service.enable {
+          Unit = {
+            Description = "Transmission Service";
+            Wants = "network-online.target";
+            After = "network-online.target";
+          };
+          Service = {
+            ExecStart = "${pkgs.transmission_4}/bin/transmission-daemon -f -g ${config.xdg.configHome}/transmission/transmission-daemon";
+            ExecReload = "${pkgs.coreutils-full}/bin/kill -HUP $MAINPID";
+            Restart = "on-abort";
+          };
+          Install = {
+            WantedBy = [ "default.target" ];
+          };
         };
       };
-    };
-  }
+      extraOpts = {
+        service = switch;
+      };
+    }
+  )
 )
