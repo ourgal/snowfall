a56862e2cb31d9a4330e127b5d66f32481bb9fa9
ourgal <git@fairever.aleeas.com>
2024-08-29 21:10:30 +0800

    refactor: replace mkdir with tmpfiles in rclone

--- lib/rclone/default.nix
+++ lib/rclone/default.nix
@@ -14,7 +14,6 @@
         After = "network-online.target";
       };
       Service = {
-        ExecStartPre = "/run/current-system/sw/bin/mkdir -p ${dir}";
         ExecStart = "${pkgs.rclone}/bin/rclone mount ${name}: ${dir} --allow-other --dir-cache-time 48h --vfs-cache-max-age 48h --vfs-cache-mode full --vfs-read-chunk-size 10M --vfs-read-chunk-size-limit 512M --buffer-size 512M";
         ExecStop = "${pkgs.fuse}fusermount -uz ${dir}";
         Type = "notify";
