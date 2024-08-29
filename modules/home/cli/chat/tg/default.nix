0b2eca2dd16ab99a9cebec8f26fd0537e0eb7a80
ourgal <git@fairever.aleeas.com>
2024-08-29 20:06:00 +0800

    feat: add sops option to tg

--- modules/home/cli/chat/tg/default.nix
+++ modules/home/cli/chat/tg/default.nix
@@ -1,18 +1,34 @@
 { ... }@args:
 args.module (
   args
-  // {
-    path = ./.;
-    nixPkgs = "tg";
-    value = with args; {
-      sops = {
-        secrets = {
-          "tg" = {
-            mode = "0600";
-            path = "${config.xdg.configHome}/tg/conf.py";
+  // (
+    let
+      inherit (args)
+        lib
+        config
+        namespace
+        cfgHome
+        ;
+      inherit (lib) mkIf;
+      inherit (lib.${namespace}) switch;
+      cfg = cfgHome config.${namespace} ./.;
+    in
+    {
+      path = ./.;
+      nixPkgs = "tg";
+      value = {
+        sops = mkIf cfg.sops.enable {
+          secrets = {
+            "tg" = {
+              mode = "0600";
+              path = "${config.xdg.configHome}/tg/conf.py";
+            };
           };
         };
       };
-    };
-  }
+      extraOpts = {
+        sops = switch;
+      };
+    }
+  )
 )
