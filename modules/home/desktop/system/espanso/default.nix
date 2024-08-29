bcc5dae704670b4965fb6564c6d2d6ce3c26c69d
ourgal <git@fairever.aleeas.com>
2024-08-29 20:20:47 +0800

    feat: add sops option to espanso

--- modules/home/desktop/system/espanso/default.nix
+++ modules/home/desktop/system/espanso/default.nix
@@ -1,43 +1,59 @@
 { ... }@args:
 args.module (
   args
-  // {
-    path = ./.;
-    myPkgs = "espanso";
-    confs = [
-      { "espanso/config" = ./default.yml; }
-      {
-        "espanso/match" = [
-          ./base.yml
-          ./github.yml
-          ./git.yml
-          ./translate.yml
-        ];
-      }
-    ];
-    value = with args; {
-      systemd.user.services.espanso = {
-        Unit = {
-          Description = "Espanso: cross platform text expander in Rust";
+  // (
+    let
+      inherit (args)
+        lib
+        config
+        namespace
+        cfgHome
+        ;
+      inherit (lib) mkIf;
+      inherit (lib.${namespace}) switch;
+      cfg = cfgHome config.${namespace} ./.;
+    in
+    {
+      path = ./.;
+      myPkgs = "espanso";
+      confs = [
+        { "espanso/config" = ./default.yml; }
+        {
+          "espanso/match" = [
+            ./base.yml
+            ./github.yml
+            ./git.yml
+            ./translate.yml
+          ];
+        }
+      ];
+      value = with args; {
+        systemd.user.services.espanso = {
+          Unit = {
+            Description = "Espanso: cross platform text expander in Rust";
+          };
+          Service = {
+            Type = "exec";
+            ExecStartPre = "${pkgs.coreutils-full}/bin/sleep 10";
+            ExecStart = "${pkgs.${namespace}.espanso}/bin/espanso daemon";
+            Restart = "on-failure";
+          };
+          Install = {
+            WantedBy = [ "default.target" ];
+          };
         };
-        Service = {
-          Type = "exec";
-          ExecStartPre = "${pkgs.coreutils-full}/bin/sleep 10";
-          ExecStart = "${pkgs.${namespace}.espanso}/bin/espanso daemon";
-          Restart = "on-failure";
-        };
-        Install = {
-          WantedBy = [ "default.target" ];
-        };
-      };
-      sops = {
-        secrets = {
-          "espanso/mail.yml" = {
-            mode = "0600";
-            path = "${config.xdg.configHome}/espanso/match/mail.yml";
+        sops = mkIf cfg.sops.enable {
+          secrets = {
+            "espanso/mail.yml" = {
+              mode = "0600";
+              path = "${config.xdg.configHome}/espanso/match/mail.yml";
+            };
           };
         };
       };
-    };
-  }
+      extraOpts = {
+        sops = switch;
+      };
+    }
+  )
 )
