37ad125f50e84c5045846d25e181648e955b4586
ourgal <git@fairever.aleeas.com>
2024-08-29 20:29:07 +0800

    feat: add sops option to geminicommit

--- modules/home/cli/versionControl/git/geminicommit/default.nix
+++ modules/home/cli/versionControl/git/geminicommit/default.nix
@@ -1,18 +1,34 @@
 { ... }@args:
 args.module (
   args
-  // {
-    path = ./.;
-    myPkgs = "geminicommit";
-    value = with args; {
-      sops = {
-        secrets = {
-          "geminicommit" = {
-            mode = "0600";
-            path = "${config.xdg.configHome}/geminicommit/config.toml";
+  // (
+    let
+      inherit (args)
+        lib
+        config
+        namespace
+        cfgHome
+        ;
+      inherit (lib) mkIf;
+      inherit (lib.${namespace}) switch;
+      cfg = cfgHome config.${namespace} ./.;
+    in
+    {
+      path = ./.;
+      myPkgs = "geminicommit";
+      value = {
+        sops = mkIf cfg.sops.enable {
+          secrets = {
+            "geminicommit" = {
+              mode = "0600";
+              path = "${config.xdg.configHome}/geminicommit/config.toml";
+            };
           };
         };
       };
-    };
-  }
+      extraOpts = {
+        sops = switch;
+      };
+    }
+  )
 )
