9e8bdc333496fa161d6f8bfc7ae45f411ab1083e
ourgal <git@fairever.aleeas.com>
2024-08-29 20:03:17 +0800

    feat: move jerry token to nixos sops module

--- modules/home/desktop/anime/jerry/default.nix
+++ modules/home/desktop/anime/jerry/default.nix
@@ -1,23 +1,39 @@
 { ... }@args:
 args.module (
   args
-  // {
-    path = ./.;
-    nixPkgs = "chafa";
-    myPkgs = "jerry";
-    value = with args; {
-      sops = {
-        secrets = {
-          "jerry/token" = {
-            mode = "0600";
-            path = "${config.xdg.dataHome}/jerry/anilist_token.txt";
-          };
-          "jerry/id" = {
-            mode = "0600";
-            path = "${config.xdg.dataHome}/jerry/anilist_user_id.txt";
+  // (
+    let
+      inherit (args)
+        lib
+        config
+        namespace
+        cfgHome
+        ;
+      inherit (lib) mkIf;
+      inherit (lib.${namespace}) switch;
+      cfg = cfgHome config.${namespace} ./.;
+    in
+    {
+      path = ./.;
+      nixPkgs = "chafa";
+      myPkgs = "jerry";
+      value = {
+        sops = mkIf cfg.sops.enable {
+          secrets = {
+            "jerry/token" = {
+              mode = "0600";
+              path = "${config.xdg.dataHome}/jerry/anilist_token.txt";
+            };
+            "jerry/id" = {
+              mode = "0600";
+              path = "${config.xdg.dataHome}/jerry/anilist_user_id.txt";
+            };
           };
         };
       };
-    };
-  }
+      extraOpts = {
+        sops = switch;
+      };
+    }
+  )
 )
