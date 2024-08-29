9ab13043a1be5bc169c57f245854dbe9f41faa5a
ourgal <git@fairever.aleeas.com>
2024-08-29 12:42:31 +0800

    refactor: replace enable = true with enabled in autorandr

--- modules/nixos/autorandr/home/default.nix
+++ modules/nixos/autorandr/home/default.nix
@@ -1,11 +1,12 @@
 { ... }@args:
 let
   inherit (args) namespace lib;
-  inherit (lib.${namespace}) nixosModule;
+  inherit (lib.${namespace}) nixosModule enabled;
   value =
     let
       display1 = {
         name = "DVI-D-0";
+        # xrandr --props
         fp = "00ffffffffffff001e6d1f5aa2c106000919010380331d78ea6275a3554fa027125054a76b80714f81c08100818095009040a9c0b300023a801871382d40582c4500fe221100001e000000fd00384b1e530f000a202020202020000000fc0032344d5035350a202020202020000000ff003530394e54595444303738360a0002";
       };
       display2 = {
@@ -14,21 +15,18 @@ let
       };
     in
     {
-      services.autorandr = {
-        enable = true;
+      services.autorandr = enabled // {
         profiles = {
           both = {
             config = {
-              "${display1.name}" = {
-                enable = true;
+              "${display1.name}" = enabled // {
                 mode = "1920x1080";
                 primary = true;
                 position = "0x0";
                 rate = "60.00";
                 crtc = 1;
               };
-              "${display2.name}" = {
-                enable = true;
+              "${display2.name}" = enabled // {
                 mode = "1920x1080";
                 position = "1920x0";
                 rate = "60.00";
@@ -42,8 +40,7 @@ let
           };
           external = {
             config = {
-              "${display2.name}" = {
-                enable = true;
+              "${display2.name}" = enabled // {
                 mode = "1920x1080";
                 position = "0x0";
                 primary = true;
@@ -58,8 +55,7 @@ let
           };
           primary = {
             config = {
-              "${display1.name}" = {
-                enable = true;
+              "${display1.name}" = enabled // {
                 mode = "1920x1080";
                 position = "0x0";
                 primary = true;
