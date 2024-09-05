d568c66d1b1cf1925ebd0e78bdeb14041c9a7dd3
ourgal <git@fairever.aleeas.com>
2024-09-05 17:56:49 +0800

    chore: enable hyprland sway qtile config

--- homes/x86_64-linux/zxc@surface/default.nix
+++ homes/x86_64-linux/zxc@surface/default.nix
@@ -5,6 +5,9 @@ in
 {
   dot = {
     awesome.minimal = enabled;
+    hyprland.minimal = enabled;
+    sway.minimal = enabled;
+    qtile.Cozytile = enabled;
     fonts = enabled;
     cli = enabled;
     desktop = enabled;
--- modules/home/sway/minimal/default.nix
+++ modules/home/sway/minimal/default.nix
@@ -1,29 +1,40 @@
 { ... }@args:
 args.module (
   args
-  // {
-    path = ./.;
-    value = with args; {
-      wayland.windowManager.sway = enabled // {
-        config = {
-          bars = [ ];
-          modifier = "Mod4";
-          terminal = "kitty";
-          startup = [
-            { command = "kitty"; }
-            { command = "${pkgs.${namespace}.swww-wallsetter}/bin/swww-wallsetter"; }
-          ];
-          keybindings =
-            let
-              inherit (config.wayland.windowManager.sway.config) modifier;
-            in
-            lib.mkOptionDefault {
-              "${modifier}+Return" = "exec ${pkgs.kitty}/bin/kitty";
-              "${modifier}+Shift+c" = "kill";
-              "${modifier}+d" = "exec ${pkgs.rofi-wayland}/bin/rofi -show drun";
-            };
+  // (
+    let
+      inherit (args)
+        enabled
+        pkgs
+        namespace
+        config
+        lib
+        ;
+    in
+    {
+      path = ./.;
+      value = {
+        wayland.windowManager.sway = enabled // {
+          config = {
+            bars = [ ];
+            modifier = "Mod4";
+            terminal = "kitty";
+            startup = [
+              { command = "kitty"; }
+              { command = "${pkgs.${namespace}.swww-wallsetter}/bin/swww-wallsetter"; }
+            ];
+            keybindings =
+              let
+                inherit (config.wayland.windowManager.sway.config) modifier;
+              in
+              lib.mkOptionDefault {
+                "${modifier}+Return" = "exec ${pkgs.kitty}/bin/kitty";
+                "${modifier}+Shift+c" = "kill";
+                "${modifier}+d" = "exec ${pkgs.rofi-wayland}/bin/rofi -show drun";
+              };
+          };
         };
       };
-    };
-  }
+    }
+  )
 )
