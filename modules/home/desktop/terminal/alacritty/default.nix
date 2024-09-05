9ca50d0da71949f8ee87a6a6b34d93e890130522
ourgal <git@fairever.aleeas.com>
2024-09-05 16:48:18 +0800

    chore: add some alacritty settings

--- modules/home/desktop/terminal/alacritty/default.nix
+++ modules/home/desktop/terminal/alacritty/default.nix
@@ -6,11 +6,31 @@ args.module (
     progs = {
       alacritty = {
         settings = {
+          live_config_reload = true;
           env.TERM = "xterm-256color";
-          window.opacity = 0.8;
+          window = {
+            decorations = "none";
+            dynamic_title = true;
+            opacity = 0.8;
+          };
+          cursor = {
+            blink_interval = 550;
+            unfocused_hollow = false;
+            thickness = 0.15;
+            style = {
+              blinking = "On";
+              shape = "Block";
+            };
+          };
+          bell = {
+            animation = "Linear";
+            duration = 0;
+          };
+          selection.save_to_clipboard = true;
           font = {
             normal.family = "FiraCode Nerd Font";
             size = 14.0;
+            builtin_box_drawing = true;
           };
         };
       };
