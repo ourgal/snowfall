ea927f3c20bb6e4bb5d1d655e4f1e71e2be4e5cf
ourgal <git@fairever.aleeas.com>
2024-09-05 10:05:36 +0800

    feat: add more fonts

--- modules/home/fonts/default.nix
+++ modules/home/fonts/default.nix
@@ -4,14 +4,18 @@ args.module (
   // {
     path = ./.;
     nixPkgs = [
+      # keep-sorted start
       "fira-code"
       "font-awesome"
       "iosevka"
+      "jetbrains-mono"
       "joypixels"
+      "material-icons"
       "nerdfonts"
       "quivira"
       "source-code-pro"
       "symbola"
+      # keep-sorted end
     ];
   }
 )
