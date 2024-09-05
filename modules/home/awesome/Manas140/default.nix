665dece46657313e65dfd6af83666f1592a88eef
ourgal <git@fairever.aleeas.com>
2024-09-05 09:55:02 +0800

    feat: add awesome Manas140 config

---
+++ modules/home/awesome/Manas140/default.nix
@@ -0,0 +1,31 @@
+{ ... }@args:
+args.module (
+  args
+  // {
+    path = ./.;
+    nixPkgs = [
+      "xsettingsd"
+      "elogind"
+      "jetbrains-mono"
+      "material-icons"
+    ];
+    confs = {
+      awesome = [
+        ./rc.lua
+        ./client.lua
+        ./help.lua
+        ./keys.lua
+        ./menu.lua
+        ./music.lua
+        ./notif.lua
+        ./rule.lua
+        ./signals.lua
+        ./titlebar.lua
+        ./bar
+        ./dashboard
+        ./theme
+        ./wid
+      ];
+    };
+  }
+)
