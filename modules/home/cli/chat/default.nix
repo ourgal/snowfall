b4999cf97296f61057088918c39747300bae12cf
ourgal <git@fairever.aleeas.com>
2024-09-07 03:05:55 +0800

    chore: remove gomuks for now

--- modules/home/cli/chat/default.nix
+++ modules/home/cli/chat/default.nix
@@ -3,12 +3,7 @@ args.module (
   args
   // {
     path = ./.;
-    nixPkgs = [
-      # keep-sorted start
-      "gomuks"
-      "nchat"
-      # keep-sorted end
-    ];
+    nixPkgs = "nchat";
     enable = "tg";
   }
 )
