97e306002913b3f69d5f2fb4916eb08040920d45
ourgal <git@fairever.aleeas.com>
2024-09-07 07:20:28 +0800

    refactor: clean git-pre-commit script

--- packages/git-pre-commit/default.nix
+++ packages/git-pre-commit/default.nix
@@ -3,6 +3,9 @@
   lib,
   pkgs,
   namespace,
+  git,
+  gitleaks,
+  keep-sorted,
   ...
 }:
 writeShellApplication rec {
@@ -15,16 +18,19 @@ writeShellApplication rec {
 
   checkPhase = "";
 
-  runtimeInputs = [ ];
+  runtimeInputs = [
+    git
+    gitleaks
+    keep-sorted
+    pkgs.${namespace}.whitespace-format
+  ];
 
   text = # bash
     ''
-      _files=$(${pkgs.git}/bin/git ls-files)
+      _files=$(git ls-files)
       files="''${_files//$'\n'/ }"
-      ${pkgs.gitleaks}/bin/gitleaks protect -v --staged
-      ${pkgs.keep-sorted}/bin/keep-sorted --mode fix $files
-      ${
-        pkgs.${namespace}.whitespace-format
-      }/bin/whitespace-format --remove-trailing-whitespace --remove-trailing-empty-lines --exclude "(.gpg|.png|.jpg|.jpeg|.webp|.gif|.dat|.age|.mp3|.mp4|.mkv|.ttf|.ico|.xcf)$" $files
+      gitleaks protect -v --staged
+      keep-sorted --mode fix $files
+      whitespace-format --remove-trailing-whitespace --remove-trailing-empty-lines --exclude "(.gpg|.png|.jpg|.jpeg|.webp|.gif|.dat|.age|.mp3|.mp4|.mkv|.ttf|.ico|.xcf)$" $files
     '';
 }
