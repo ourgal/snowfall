bd61750fe7ed8163e26555b59ed01d3fc2d5546e
ourgal <git@fairever.aleeas.com>
2024-09-06 21:03:56 +0800

    feat: add more hooks to git pre-commit
    add gitleaks whitespace-format and treefmt to git global pre-commit hook

--- packages/git-pre-commit/default.nix
+++ packages/git-pre-commit/default.nix
@@ -1,34 +1,33 @@
-{ pkgs, ... }:
-pkgs.writeScriptBin "git-pre-commit.py" ''
-  #!${pkgs.python3}/bin/python
-  """Helper script to be used as a pre-commit hook."""
-  import os
-  import sys
-  import subprocess
+{
+  writeShellApplication,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}:
+writeShellApplication rec {
+  name = "git-pre-commit";
 
+  meta = {
+    mainProgram = name;
+    platforms = lib.platforms.linux;
+  };
 
-  def gitleaksEnabled():
-      """Determine if the pre-commit hook for gitleaks is enabled."""
-      out = subprocess.getoutput("git config --bool hooks.gitleaks")
-      if out == "false":
-          return False
-      return True
+  checkPhase = "";
 
+  runtimeInputs = [ ];
 
-  if gitleaksEnabled():
-      exitCode = os.WEXITSTATUS(os.system("gitleaks protect -v --staged"))
-      if exitCode == 1:
-          print(
-              """Warning: gitleaks has detected sensitive information in your changes.
-  To disable the gitleaks precommit hook run the following command:
-
-      git config hooks.gitleaks false
-  """
-          )
-          sys.exit(1)
-  else:
-      print(
-          "gitleaks precommit disabled\
-       (enable with `git config hooks.gitleaks true`)"
-      )
-''
+  text = # bash
+    ''
+      root=$(${pkgs.git}/bin/git rev-parse --show-toplevel)
+      _files=$(${pkgs.git}/bin/git ls-files)
+      files="''${_files//$'\n'/ }"
+      ${pkgs.gitleaks}/bin/gitleaks protect -v --staged
+      ${pkgs.gitleaks}/bin/gitleaks detect -v
+      ${pkgs.treefmt2}/bin/treefmt --config-file="~/.config/treefmt/config.toml" -C "$root"
+      ${pkgs.keep-sorted}/bin/keep-sorted --mode fix $files
+      ${
+        pkgs.${namespace}.whitespace-format
+      }/bin/whitespace-format --remove-trailing-whitespace --remove-trailing-empty-lines --exclude "(.gpg|.png|.jpg|.jpeg|.webp|.gif|.dat|.age|.mp3|.mp4|.mkv|.ttf|.ico|.xcf)$" $files
+    '';
+}
