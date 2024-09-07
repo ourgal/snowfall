fbbd93a870e96a4d50991bd7ccf5387608479208
ourgal <git@fairever.aleeas.com>
2024-09-07 23:25:13 +0800

    fix: check empty files in git-pre-commit

--- packages/git-pre-commit/default.nix
+++ packages/git-pre-commit/default.nix
@@ -30,8 +30,10 @@ writeShellApplication rec {
       _files=$(git diff --name-only --cached)
       files="''${_files//$'\n'/ }"
       gitleaks protect -v --staged
-      keep-sorted --mode fix $files
-      whitespace-format --remove-trailing-whitespace --remove-trailing-empty-lines --exclude "(.gpg|.png|.jpg|.jpeg|.webp|.gif|.dat|.age|.mp3|.mp4|.mkv|.ttf|.ico|.xcf)$" $files
+      if test -n "$files"; then
+        keep-sorted --mode fix $files
+        whitespace-format --remove-trailing-whitespace --remove-trailing-empty-lines --exclude "(.gpg|.png|.jpg|.jpeg|.webp|.gif|.dat|.age|.mp3|.mp4|.mkv|.ttf|.ico|.xcf)$" $files
+      fi
       if test -f .git/hooks/pre-commit; then
          .git/hooks/pre-commit "$@"
       fi
