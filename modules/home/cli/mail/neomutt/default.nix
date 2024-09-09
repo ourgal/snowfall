692db19239542c394be7734c26a36b068aaab040
ourgal <git@fairever.aleeas.com>
2024-09-09 22:00:20 +0800

    feat: add sops option for neomutt

--- modules/home/cli/mail/neomutt/default.nix
+++ modules/home/cli/mail/neomutt/default.nix
@@ -1,27 +1,42 @@
 { ... }@args:
 args.module (
   args
-  // {
-    path = ./.;
-    nixPkgs = "neomutt";
-    confs = {
-      neomutt = [
-        ./neomuttrc
-        ./mappings
-        ./settings
-        ./colors
-        ./mailcap
-      ];
-    };
-    value = {
-      sops = {
-        secrets = {
-          "neomutt/hotmail" = {
-            mode = "0600";
-            path = "${args.config.xdg.configHome}/neomutt/accounts/hotmail";
+  // (
+    let
+      inherit (args)
+        switch
+        cfgHome
+        config
+        namespace
+        lib
+        ;
+      cfg = cfgHome config.${namespace} ./.;
+    in
+    {
+      path = ./.;
+      nixPkgs = "neomutt";
+      confs = {
+        neomutt = [
+          ./neomuttrc
+          ./mappings
+          ./settings
+          ./colors
+          ./mailcap
+        ];
+      };
+      value = {
+        sops = lib.mkIf cfg.sops.enable {
+          secrets = {
+            "neomutt/hotmail" = {
+              mode = "0600";
+              path = "${args.config.xdg.configHome}/neomutt/accounts/hotmail";
+            };
           };
         };
       };
-    };
-  }
+      extraOpts = {
+        sops = switch;
+      };
+    }
+  )
 )
