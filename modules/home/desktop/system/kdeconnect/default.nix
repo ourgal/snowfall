42b295daac750b381d79a628982f237565efa73f
ourgal <git@fairever.aleeas.com>
2024-09-12 01:13:45 +0800

    chore: add kdeconnect send-clipboard alias

--- modules/home/desktop/system/kdeconnect/default.nix
+++ modules/home/desktop/system/kdeconnect/default.nix
@@ -8,5 +8,17 @@ args.module (
         indicator = true;
       };
     };
+    progs = {
+      fish = {
+        functions = {
+          kde-clip = {
+            body = ''
+              kdeconnect-cli -l --name-only | fzf | xargs -I device kdeconnect-cli --send-clipboard -n device
+            '';
+            description = "kde-connect cli send clipboard";
+          };
+        };
+      };
+    };
   }
 )
