796a40253b0ab74568a6d68e7181ce25a42b3b83
ourgal <git@fairever.aleeas.com>
2024-09-11 13:00:39 +0800

    refactor: build focus from source

--- packages/focus-timer/default.nix
+++ packages/focus-timer/default.nix
@@ -1,36 +1,41 @@
 {
-  stdenv,
   lib,
-  fetchurl,
+  buildGoModule,
+  fetchFromGitHub,
   alsa-lib,
-  autoPatchelfHook,
-  ...
+  pkg-config,
 }:
 
-stdenv.mkDerivation rec {
-  pname = "focus-timer";
+buildGoModule rec {
+  pname = "focus";
   version = "1.4.3";
 
-  src = fetchurl {
-    url = "https://github.com/ayoisaiah/focus/releases/download/v${version}/focus_${version}_linux_amd64.tar.gz";
-    hash = "sha256-d4eiF2FxtUruZRjHkrJhuYPzTF1CdmLoJUvnt7ySXbo=";
+  src = fetchFromGitHub {
+    owner = "ayoisaiah";
+    repo = "focus";
+    rev = "v${version}";
+    hash = "sha256-p9gTFUYHdyOY2EFT/ZmBHT7dRNm64s/3ExUXG7bfQ70=";
   };
 
-  nativeBuildInputs = [ autoPatchelfHook ];
+  vendorHash = "sha256-wWTDo4zbvQjosI+fsBwEq5wKhJX2gw9VAbih1urRTO8=";
+
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  nativeBuildInputs = [ pkg-config ];
 
   buildInputs = [ alsa-lib ];
 
-  sourceRoot = ".";
-
-  installPhase = ''
-    runHook preInstall
-    install -m755 -D focus $out/bin/focus
-    runHook postInstall
-  '';
+  doCheck = false;
 
   meta = with lib; {
+    description = "A fully featured productivity timer for the command line, based on the Pomodoro Technique. Supports Linux, Windows, and macOS";
     homepage = "https://github.com/ayoisaiah/focus";
-    description = "A fully featured productivity timer for the command line, based on the Pomodoro Technique.";
-    platforms = platforms.linux;
+    changelog = "https://github.com/ayoisaiah/focus/blob/${src.rev}/CHANGELOG.md";
+    license = licenses.mit;
+    maintainers = with maintainers; [ zxc ];
+    mainProgram = "focus";
   };
 }
