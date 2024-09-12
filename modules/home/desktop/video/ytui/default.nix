588c4c50ac5ac7f56f6068da7c15b3ddf8106054
ourgal <git@fairever.aleeas.com>
2024-09-12 19:36:07 +0800

    feat: add ytui

---
+++ modules/home/desktop/video/ytui/default.nix
@@ -0,0 +1,23 @@
+{ ... }@args:
+args.module (
+  args
+  // {
+    path = ./.;
+    myPkgs = "ytui";
+    confs = {
+      "ytui/config.yaml" = builtins.toJSON {
+        channels = {
+          local = true;
+          subscribed = [
+            "UCTt2AnK--mnRmICnf-CCcrw"
+            "UCutXfzLC5wrV3SInT_tdY0w"
+          ];
+        };
+        invidious = {
+          instance = "invidious.jing.rocks";
+        };
+        loglevel = "info";
+      };
+    };
+  }
+)
