8adc3d43611543d9d756cf54433d827d4e236635
ourgal <git@fairever.aleeas.com>
2024-09-12 13:59:03 +0800

    feat: add i3lock-color module

---
+++ modules/home/cli/system/i3lock-color/default.nix
@@ -0,0 +1,17 @@
+{ ... }@args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) pkgs namespace;
+    in
+    {
+      path = ./.;
+      servs = {
+        screen-locker = {
+          lockCmd = "${pkgs.${namespace}.i3lock-color}/bin/i3lock-color";
+        };
+      };
+    }
+  )
+)
