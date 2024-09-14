d247d0af1c5e107d7310826327a74e11494e0806
ourgal <git@fairever.aleeas.com>
2024-09-14 09:06:45 +0800

    chore: update jerry

--- packages/jerry/default.nix
+++ packages/jerry/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation {
   pname = "jerry";
-  version = "unstable-2024-07-08";
+  version = "unstable-2024-08-14";
 
   src = fetchFromGitHub {
     owner = "justchokingaround";
     repo = "jerry";
-    rev = "761b7b6f19af063bc908f56842dab167b0ce0af8";
-    sha256 = "0vbp48xx0a4mdsdvbjamiafzmmhvwlqsfdch26r5s1fs5nrsp6g2";
+    rev = "5ba8beccec4f53cf87b0bef6116de27b69e86924";
+    sha256 = "18qwyvph4vvchjq22kg32fxq0zs03rdwlsiydnmwpvlwyj0632cw";
   };
 
   dontBuild = true;
