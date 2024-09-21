236372ae48d2b96ec3a8ef2d48c447f464ce7af2
ourgal <git@fairever.aleeas.com>
2024-09-22 00:21:26 +0800

    feat: add xdg portal for home manager

--- modules/home/desktop/system/default.nix
+++ modules/home/desktop/system/default.nix
@@ -1,59 +1,73 @@
 { ... }@args:
 args.module (
   args
-  // {
-    path = ./.;
-    myPkgs = "blobdrop";
-    nixPkgs = [
-      # keep-sorted start
-      "brightnessctl"
-      "clash-verge"
-      "clipqr"
-      "fuzzel"
-      "libnotify"
-      "polkit"
-      "ripdrag"
-      "scrcpy"
-      "swww"
-      "variety"
-      "xclip"
-      "xcolor"
-      # keep-sorted end
-    ];
-    enable = [
-      # keep-sorted start
-      "barrier"
-      "clipboard"
-      "cursor"
-      "espanso"
-      "flameshot"
-      "gammastep"
-      "gtk"
-      "kdeconnect"
-      "kmonad"
-      "maim"
-      "picom"
-      "playerctl"
-      "qt"
-      "redshift"
-      "rofi"
-      "sbsrf"
-      "scream"
-      "sxhkd"
-      "wired"
-      "xbanish"
-      # keep-sorted end
-    ];
-    value = with args; {
-      colorScheme = inputs.nix-colors.colorSchemes.gigavolt;
-      ${namespace}.desktop.system = {
-        waybar = {
-          hyprland = {
-            slickbar = enabled;
-            clock24h = enabled;
+  // (
+    let
+      inherit (args)
+        namespace
+        inputs
+        enabled
+        pkgs
+        ;
+    in
+    {
+      path = ./.;
+      myPkgs = "blobdrop";
+      nixPkgs = [
+        # keep-sorted start
+        "brightnessctl"
+        "clash-verge"
+        "clipqr"
+        "fuzzel"
+        "libnotify"
+        "polkit"
+        "ripdrag"
+        "scrcpy"
+        "swww"
+        "variety"
+        "xclip"
+        "xcolor"
+        # keep-sorted end
+      ];
+      enable = [
+        # keep-sorted start
+        "barrier"
+        "clipboard"
+        "cursor"
+        "espanso"
+        "flameshot"
+        "gammastep"
+        "gtk"
+        "kdeconnect"
+        "kmonad"
+        "maim"
+        "picom"
+        "playerctl"
+        "qt"
+        "redshift"
+        "rofi"
+        "sbsrf"
+        "scream"
+        "sxhkd"
+        "wired"
+        "xbanish"
+        # keep-sorted end
+      ];
+      value = {
+        colorScheme = inputs.nix-colors.colorSchemes.gigavolt;
+        xdg.portal = enabled // {
+          extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+          config.common.default = "*";
+        };
+        ${namespace}.desktop.system = {
+          waybar = {
+            hyprland = {
+              slickbar = enabled;
+              clock24h = enabled;
+            };
           };
         };
       };
-    };
-  }
+    }
+  )
 )
