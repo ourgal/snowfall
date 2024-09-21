8f1f08f38d35da9fe9ff5db0b6cec749b7b6bd60
ourgal <git@fairever.aleeas.com>
2024-09-21 18:35:06 +0800

    feat: add fzf-buku fish function

--- modules/home/cli/shell/buku/default.nix
+++ modules/home/cli/shell/buku/default.nix
@@ -12,6 +12,23 @@ args.module (
         shellAbbrs = {
           bb = "buku";
         };
+        functions = {
+          fzf-buku = {
+            body = ''
+              # Save newline-separated string into an array
+              set -l website (buku -p -f 5 | column -ts\t | fzf --multi)
+
+              # Open each website
+              for i in $website
+                  set index (echo "$i" | awk '{print $1}')
+                  buku -p $index
+                  buku -o $index
+              end
+            '';
+            description = "fzf buku";
+          };
+        };
+
       };
     };
   }
