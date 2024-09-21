7e899b6674c115b56874826c8e6bbb7358932388
ourgal <git@fairever.aleeas.com>
2024-09-22 01:41:23 +0800

    feat: add netease-cloud-music-gtk

--- modules/home/desktop/audio/default.nix
+++ modules/home/desktop/audio/default.nix
@@ -4,9 +4,12 @@ args.module (
   // {
     path = ./.;
     nixPkgs = [
+      # keep-sorted start
       "alsa-utils"
       "blanket"
+      "netease-cloud-music-gtk"
       "qpwgraph"
+      # keep-sorted end
     ];
     enable = "easyeffects";
   }
