e616b213d73a7f0d0c780e030194282bb6719bb7
ourgal <git@fairever.aleeas.com>
2024-09-22 18:52:36 +0800

    fix: add bukubrow native hosts

--- modules/home/cli/shell/buku/default.nix
+++ modules/home/cli/shell/buku/default.nix
@@ -1,35 +1,64 @@
 { ... }@args:
 args.module (
   args
-  // {
-    path = ./.;
-    nixPkgs = [
-      "buku"
-      "bukubrow"
-    ];
-    progs = {
-      fish = {
-        shellAbbrs = {
-          bb = "buku";
-        };
-        functions = {
-          fzf-buku = {
-            body = ''
-              # Save newline-separated string into an array
-              set -l website (buku -p -f 5 | column -ts\t | fzf --multi)
-
-              # Open each website
-              for i in $website
-                  set index (echo "$i" | awk '{print $1}')
-                  buku -p $index
-                  buku -o $index
-              end
-            '';
-            description = "fzf buku";
-          };
-        };
-
+  // (
+    let
+      inherit (args) pkgs;
+      chromiumNative = {
+        name = "com.samhh.bukubrow";
+        description = "Bukubrow is a WebExtension for Buku, a command-line bookmark manager. This is the corresponding host that facilitates interfacing with the Buku database via native messaging.";
+        path = "${pkgs.bukubrow}/bin/bukubrow";
+        type = "stdio";
+        allowed_origins = [ "chrome-extension://ghniladkapjacfajiooekgkfopkjblpn/" ];
       };
-    };
-  }
+      firefoxNative = {
+        name = "com.samhh.bukubrow";
+        description = "Bukubrow is a WebExtension for Buku, a command-line bookmark manager. This is the corresponding host that facilitates interfacing with the Buku database via native messaging.";
+        path = "${pkgs.bukubrow}/bin/bukubrow";
+        type = "stdio";
+        allowed_extensions = [ "bukubrow@samhh.com" ];
+      };
+    in
+    {
+      path = ./.;
+      nixPkgs = [
+        "buku"
+        "bukubrow"
+      ];
+      progs = {
+        fish = {
+          shellAbbrs = {
+            bb = "buku";
+          };
+          functions = {
+            fzf-buku = {
+              body = ''
+                # Save newline-separated string into an array
+                set -l website (buku -p -f 5 | column -ts\t | fzf --multi)
+
+                # Open each website
+                for i in $website
+                    set index (echo "$i" | awk '{print $1}')
+                    buku -p $index
+                    buku -o $index
+                end
+              '';
+              description = "fzf buku";
+            };
+          };
+
+        };
+      };
+      confs = {
+        "BraveSoftware/Brave-Browser/NativeMessagingHosts/com.samhh.bukubrow.json" = builtins.toJSON chromiumNative;
+        "google-chrome/NativeMessagingHosts/com.samhh.bukubrow.json" = builtins.toJSON chromiumNative;
+        "chromium/NativeMessagingHosts/com.samhh.bukubrow.json" = builtins.toJSON chromiumNative;
+        "vivaldi/NativeMessagingHosts/com.samhh.bukubrow.json" = builtins.toJSON chromiumNative;
+      };
+      files = {
+        ".mozilla/native-messaging-hosts/com.samhh.bukubrow.json" = builtins.toJSON firefoxNative;
+        ".librewolf/native-messaging-hosts/com.samhh.bukubrow.json" = builtins.toJSON firefoxNative;
+      };
+    }
+  )
 )
