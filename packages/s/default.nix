9ac4c8814660dfac8e2a15cfbe96f7290dbdc990
ourgal <git@fairever.aleeas.com>
2024-09-23 09:32:59 +0800

    feat: update s to 0.7.1

--- packages/s/default.nix
+++ packages/s/default.nix
@@ -1,28 +1,42 @@
 {
-  stdenv,
   lib,
-  fetchzip,
-  ...
+  buildGoModule,
+  fetchFromGitHub,
+  installShellFiles,
 }:
 
-stdenv.mkDerivation rec {
-  pname = "s-search";
-  version = "0.7.0";
+buildGoModule rec {
+  pname = "s";
+  version = "0.7.1";
 
-  src = fetchzip {
-    url = "https://github.com/zquestz/s/releases/download/v${version}/s-linux_386.zip";
-    hash = "sha256-P3DPdRPFjDFI7ozfVqR4k0+jid0IJLWyG2/aGDP0AwY=";
+  src = fetchFromGitHub {
+    owner = "zquestz";
+    repo = "s";
+    rev = "v${version}";
+    hash = "sha256-cu2XGA5rRO+cHZ9QkJvMBjbxNZigxsx12ziCJG1LCQs=";
   };
 
-  installPhase = ''
-    runHook preInstall
-    install -m755 -D s $out/bin/s
-    runHook postInstall
+  vendorHash = "sha256-0E/9fONanSxb2Tv5wKIpf1J/A6Hdge23xy3r6pFyV9E=";
+
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  postInstall = ''
+    $out/bin/s --completion bash > s.bash
+    $out/bin/s --completion zsh > s.zsh
+    $out/bin/s --completion fish > s.fish
+    installShellCompletion s.{bash,fish,zsh}
   '';
 
   meta = with lib; {
+    description = "Open a web search in your terminal";
     homepage = "https://github.com/zquestz/s";
-    description = "Open a web search in your terminal. ";
-    platforms = platforms.linux;
+    license = licenses.mit;
+    maintainers = with maintainers; [ zxc ];
+    mainProgram = "s";
   };
 }
