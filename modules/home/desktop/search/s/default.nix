53cd6e1607163f6c90897fc550a7c8ec0dd4280e
ourgal <git@fairever.aleeas.com>
2024-09-23 10:26:33 +0800

    chore: add s fish alias

--- modules/home/desktop/search/s/default.nix
+++ modules/home/desktop/search/s/default.nix
@@ -1,8 +1,23 @@
 { ... }@args:
 args.module (
   args
-  // {
-    path = ./.;
-    myPkgs = "s";
-  }
+  // (
+    let
+      inherit (args) config namespace pkgs;
+      inherit (config.${namespace}.user) browser;
+    in
+    {
+      path = ./.;
+      myPkgs = "s";
+      progs = {
+        fish.functions.sp = {
+          body = ''
+            s -b ${browser} -p $argv[1] $argv[2..-1]
+            ${pkgs.wmctrl}/bin/wmctrl -a ${browser}
+          '';
+          description = "alias of s -p";
+        };
+      };
+    }
+  )
 )
