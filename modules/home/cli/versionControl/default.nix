0dc8144dbbf1120ff3b7184cd44d8dcb10361096
ourgal <git@fairever.aleeas.com>
2024-09-26 22:44:58 +0800

    chore: enable git hook

--- modules/home/cli/versionControl/default.nix
+++ modules/home/cli/versionControl/default.nix
@@ -1,27 +1,28 @@
 { ... }@args:
 args.module (
   args
-  // {
-    path = ./.;
-    nixPkgs = [
-      # keep-sorted start
-      "gitu"
-      "tig"
-      "tokei"
-      # keep-sorted end
-    ];
-    enable = [
-      # keep-sorted start
-      "gh"
-      "git"
-      "gitui"
-      "jujutsu"
-      {
-        lazygit = {
-          diff = "difftastic";
-        };
-      }
-      # keep-sorted end
-    ];
-  }
+  // (
+    let
+      inherit (args) enabled;
+    in
+    {
+      path = ./.;
+      nixPkgs = [
+        # keep-sorted start
+        "gitu"
+        "tig"
+        "tokei"
+        # keep-sorted end
+      ];
+      enable = [
+        # keep-sorted start
+        "gh"
+        "gitui"
+        "jujutsu"
+        { git.hook = enabled; }
+        { lazygit.diff = "difftastic"; }
+        # keep-sorted end
+      ];
+    }
+  )
 )
