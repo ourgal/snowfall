c1e9465015c87b0c1a3cb9c42802b0c44c813a56
ourgal <git@fairever.aleeas.com>
2024-09-26 18:15:47 +0800

    refactor: move awesome config from chezmoi to nix

--- modules/home/awesome/minimalFnl/default.nix
+++ modules/home/awesome/minimalFnl/default.nix
@@ -3,21 +3,34 @@ args.module (
   args
   // (
     let
-      inherit (args) pkgs lib config;
+      inherit (args) pkgs;
+      conf =
+        pkgs.runCommand "awesome-minimalFnl"
+          {
+            src = ./config;
+            nativeBuildInputs = [
+              pkgs.gnumake
+              pkgs.fennel
+            ];
+          }
+          ''
+            mkdir $out
+            cp $src/Makefile $out
+            cp $src/*.fnl $out
+            mkdir -p $out/keys
+            cp $src/keys/*.fnl $out/keys
+            mkdir -p $out/rules
+            cp $src/rules/*.fnl $out/rules
+            mkdir -p $out/themes/default
+            cp $src/themes/default/*.fnl $out/themes/default
+            make -C $out
+            find $out -name "*.fnl" -delete
+            rm $out/Makefile
+          '';
     in
     {
       path = ./.;
-      value = {
-        home.activation.aweome_minimalFnl = config.lib.dag.entryAfter [ "chezmoi" ] ''
-          PATH=$PATH:${
-            lib.makeBinPath [
-              pkgs.gnumake
-              pkgs.fennel
-            ]
-          }
-          make -C ${config.xdg.configHome}/awesome
-        '';
-      };
+      confs.awesome = conf;
     }
   )
 )
