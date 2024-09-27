2abf2acdc4d757a36e52d40856ec340b3f6839b4
ourgal <git@fairever.aleeas.com>
2024-09-28 01:32:52 +0800

    chore: update pot-desktop

--- packages/pot/default.nix
+++ packages/pot/default.nix
@@ -8,23 +8,20 @@
 let
   pname = "pot";
   pnameCap = lib.${namespace}.capitalize pname;
-  version = "2.7.10";
+  version = "3.0.5";
 
   src = fetchurl {
     url = "https://github.com/pot-app/pot-desktop/releases/download/${version}/pot_${version}_amd64.AppImage";
-    hash = "sha256-QrIVOWx3iefpmnzbcy+WMnruWT9t/s/eFw9NjDzm6eo=";
+    hash = "sha256-anBEMfVyiiLtXd24/sNUV7luoPPXb5gFJctDx95PFRA=";
   };
 
   appimageContents = appimageTools.extract { inherit pname version src; };
 in
 appimageTools.wrapType2 {
   inherit pname version src;
-  #    mv $out/bin/{${pname}-${version},${pname}}
 
   extraInstallCommands = ''
     install -Dm444 ${appimageContents}/${pname}.desktop $out/share/applications/${pnameCap}.desktop
     install -Dm444 ${appimageContents}/usr/share/icons/hicolor/256x256@2/apps/${pname}.png $out/share/pixmaps/${pnameCap}.png
-    substituteInPlace $out/share/applications/${pnameCap}.desktop \
-      --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${pname}'
   '';
 }
