2be9b6fb82bca285c8c2ca588266a42c14a6eeb1
ourgal <git@fairever.aleeas.com>
2024-09-27 20:10:46 +0800

    refactor: replace local file with fetchurl

--- packages/better-git-branch/default.nix
+++ packages/better-git-branch/default.nix
@@ -1,4 +1,9 @@
-{ writeShellApplication, lib, ... }:
+{
+  writeShellApplication,
+  lib,
+  git,
+  fetchurl,
+}:
 writeShellApplication rec {
   name = "better-git-branch";
 
@@ -9,7 +14,10 @@ writeShellApplication rec {
 
   checkPhase = "";
 
-  runtimeInputs = [ ];
+  runtimeInputs = [ git ];
 
-  text = builtins.readFile ./better-git-branch.sh;
+  text = builtins.readFile (fetchurl {
+    url = "https://gist.githubusercontent.com/schacon/e9e743dee2e92db9a464619b99e94eff/raw/2e3ae498c2177f5974679a6ab33849cbf33b209e/better-git-branch.sh";
+    hash = "sha256-KkKWV+OB1uve0csofMvPz7mK39miYt2JllN+Kpipk4g=";
+  });
 }
