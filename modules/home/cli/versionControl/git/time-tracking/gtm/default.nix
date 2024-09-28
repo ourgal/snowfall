5ca0ecf83b32dad37996618c35c1d18cdca89f96
ourgal <git@fairever.aleeas.com>
2024-09-29 01:11:14 +0800

    chore: add gtm config

---
+++ modules/home/cli/versionControl/git/time-tracking/gtm/default.nix
@@ -0,0 +1,23 @@
+{ ... }@args:
+args.module (
+  args
+  // {
+    path = ./.;
+    myPkgs = "gtm";
+    progs = {
+      fish = {
+        interactiveShellInit = "__gtm";
+        functions.__gtm = {
+          body = ''
+            ;
+            function __gtm --on-event fish_prompt
+                gtm record -terminal
+            end
+          '';
+          description = "gtm terminal hook";
+        };
+      };
+    };
+  }
+
+)
