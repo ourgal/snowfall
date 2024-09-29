9057c4d420cf5ae60f5b138a480840707fef0e24
ourgal <git@fairever.aleeas.com>
2024-09-29 18:25:21 +0800

    chore: update pipelight

--- packages/pipelight/default.nix
+++ packages/pipelight/default.nix
@@ -8,22 +8,28 @@
   zlib,
   stdenv,
   darwin,
+  deno,
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "pipelight";
-  version = "0.7.24";
+  version = "0.8.0";
 
   src = fetchFromGitHub {
     owner = "pipelight";
     repo = "pipelight";
     rev = "v${version}";
-    hash = "sha256-BgSOmmVDJvAGT2SdFaR0a1wqE1NH8ERjZGg0gOXwPu8=";
+    hash = "sha256-yJfE8fKjPaVdrMMOiY6VIBCjBfZZoFQohCZ3iHgfhZI=";
   };
 
-  cargoHash = "sha256-OmhGdhCxAiTMaNeksMAMzZmBqsayeclMdhAERR3DVeU=";
+  cargoHash = "sha256-CoOVJ86bPXZNkKHICRN3sQ2/GFyTI6hPgZlSRe95lL8=";
 
   nativeBuildInputs = [ pkg-config ];
+
+  postPatch = ''
+    substituteInPlace ./cast/src/config/load/typescript/mod.rs \
+      --replace-fail 'deno' '${deno}/bin/deno'
+  '';
 
   buildInputs =
     [
@@ -43,6 +49,7 @@ rustPlatform.buildRustPackage rec {
   meta = with lib; {
     description = "Tiny automation pipelines. Bring CI/CD to the smallest projects. Self-hosted, Lightweight, CLI only";
     homepage = "https://github.com/pipelight/pipelight";
+    changelog = "https://github.com/pipelight/pipelight/blob/${src.rev}/CHANGELOG.md";
     license = licenses.gpl2Only;
     maintainers = with maintainers; [ zxc ];
     mainProgram = "pipelight";
