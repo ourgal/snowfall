3afc054e56f9bde105d923a953e6d97e86582470
ourgal <git@fairever.aleeas.com>
2024-09-30 01:34:12 +0800

    feat: add gitwatch

--- modules/home/cli/versionControl/git/commit/default.nix
+++ modules/home/cli/versionControl/git/commit/default.nix
@@ -1,22 +1,28 @@
 { ... }@args:
 args.module (
   args
-  // {
-    path = ./.;
-    myPkgs = [
-      # keep-sorted start
-      "git-sumi"
-      "gogitmoji"
-      "goji"
-      "koji"
-      # keep-sorted end
-    ];
-    nixPkgs = [
-      # keep-sorted start
-      "commitizen"
-      "gitmoji-cli"
-      # keep-sorted end
-    ];
-    enable = "geminicommit";
-  }
+  // (
+    let
+      inherit (args) inputs pkgs;
+    in
+    {
+      path = ./.;
+      inputPkgs = [ inputs.gitwatch.packages."${pkgs.system}".default ];
+      myPkgs = [
+        # keep-sorted start
+        "git-sumi"
+        "gogitmoji"
+        "goji"
+        "koji"
+        # keep-sorted end
+      ];
+      nixPkgs = [
+        # keep-sorted start
+        "commitizen"
+        "gitmoji-cli"
+        # keep-sorted end
+      ];
+      enable = "geminicommit";
+    }
+  )
 )
