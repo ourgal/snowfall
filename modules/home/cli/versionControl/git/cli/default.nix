da0c75b191f2007e76ebf45995b46dd2fa32834f
ourgal <git@fairever.aleeas.com>
2024-09-30 00:41:29 +0800

    feat: add forgit

--- modules/home/cli/versionControl/git/cli/default.nix
+++ modules/home/cli/versionControl/git/cli/default.nix
@@ -1,13 +1,29 @@
 { ... }@args:
 args.module (
   args
-  // {
-    path = ./.;
-    progs = "scmpuff";
-    nixPkgs = [
-      # keep-sorted start
-      "bit"
-      # keep-sorted end
-    ];
-  }
+  // (
+    let
+      inherit (args) pkgs;
+    in
+    {
+      path = ./.;
+      progs = [
+        "scmpuff"
+        {
+          git.aliases = {
+            f = "!${pkgs.zsh-forgit}/bin/git-forgit";
+            fa = "!${pkgs.zsh-forgit}/bin/git-forgit add";
+            fr = "!${pkgs.zsh-forgit}/bin/git-forgit reset_head";
+            fchf = "!${pkgs.zsh-forgit}/bin/git-forgit checkout_file";
+          };
+        }
+      ];
+      nixPkgs = [
+        # keep-sorted start
+        "bit"
+        "zsh-forgit"
+        # keep-sorted end
+      ];
+    }
+  )
 )
