2514a455dfc815a5eccfe8d93eb0e58b93b62604
ourgal <git@fairever.aleeas.com>
2024-09-29 14:13:46 +0800

    feat: add bat-extras

--- modules/home/cli/shell/utils/cat/default.nix
+++ modules/home/cli/shell/utils/cat/default.nix
@@ -3,6 +3,15 @@ args.module (
   args
   // {
     path = ./.;
+    batPkgs = [
+      # keep-sorted start
+      "batdiff"
+      "batgrep"
+      "batman"
+      "batpipe"
+      "batwatch"
+      # keep-sorted end
+    ];
     nixPkgs = [
       # keep-sorted start
       "bat"
