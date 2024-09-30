6de46b70f3241db32ba3ae9507f6c73075128c8d
ourgal <git@fairever.aleeas.com>
2024-09-30 15:25:55 +0800

    feat: add gitqlient

--- modules/home/desktop/versionControl/default.nix
+++ modules/home/desktop/versionControl/default.nix
@@ -3,6 +3,11 @@ args.module (
   args
   // {
     path = ./.;
-    nixPkgs = "gittyup";
+    nixPkgs = [
+      # keep-sorted start
+      "gitqlient"
+      "gittyup"
+      # keep-sorted end
+    ];
   }
 )
