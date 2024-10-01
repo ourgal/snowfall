53fecb6cf3c0cb504a5b030c4e2ccf0126e79fc2
ourgal <git@fairever.aleeas.com>
2024-10-01 23:57:48 +0800

    feat: add rst module

---
+++ modules/home/dev/rst/default.nix
@@ -0,0 +1,13 @@
+{ ... }@args:
+args.module (
+  args
+  // {
+    path = ./.;
+    nixPkgs = [
+      # keep-sorted start
+      "sphinx"
+      "sphinx-autobuild"
+      # keep-sorted end
+    ];
+  }
+)
