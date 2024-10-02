9517f55e0fd5d982e343c7d1c19c5890d82b042d
ourgal <git@fairever.aleeas.com>
2024-10-02 20:47:57 +0800

    feat: add hledger module

---
+++ modules/home/cli/note/hledger/default.nix
@@ -0,0 +1,19 @@
+{ ... }@args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) config;
+    in
+    {
+      path = ./.;
+      nixPkgs = "hledger";
+      dataFiles = {
+        "hledger/.keep" = "";
+      };
+      env = {
+        LEDGER_FILE = "${config.xdg.dataHome}/hledger/main.journal";
+      };
+    }
+  )
+)
