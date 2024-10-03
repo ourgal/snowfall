4e1f615a549492265d781cd17d8b001c741e0984
ourgal <git@fairever.aleeas.com>
2024-10-03 18:49:15 +0800

    chore: add gh plugins module

---
+++ modules/home/cli/versionControl/gh/plugins/default.nix
@@ -0,0 +1,28 @@
+{ ... }@args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) pkgs namespace;
+    in
+    {
+      path = ./.;
+      progs = {
+        gh.extensions =
+          [
+            pkgs.gh-dash
+            pkgs.gh-eco
+            pkgs.gh-f
+            pkgs.gh-s
+            pkgs.gh-markdown-preview
+          ]
+          ++ [
+            pkgs.${namespace}.gh-branch
+            pkgs.${namespace}.gh-cp
+            pkgs.${namespace}.gh-screensaver
+            pkgs.${namespace}.gh-stars
+          ];
+      };
+    }
+  )
+)
