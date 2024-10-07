69eed1d298de79c87af9452d38eb1c8888284599
ourgal <git@fairever.aleeas.com>
2024-10-08 03:29:05 +0800

    feat: add tp

---
+++ packages/tp/default.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  stdenv,
+  fetchzip,
+  namespace,
+}:
+let
+  pname = "tp";
+  source = lib.${namespace}.sources.${pname};
+in
+stdenv.mkDerivation rec {
+  inherit pname;
+  version = lib.substring 1 (-1) source.version;
+
+  src = fetchzip {
+    url = "https://github.com/minefuto/tp/releases/download/v${version}/tp_Linux_x86_64.tar.gz";
+    hash = "sha256-+ziOARrOL03AuMb9axU9BlPD+ZAp4XC1N0YAJLr0SfA=";
+    stripRoot = false;
+  };
+
+  installPhase = ''
+    runHook preInstall
+    install -m755 -D tp $out/bin/tp
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Display the result of the commands at every keystroke";
+    homepage = "https://github.com/minefuto/tp";
+    license = licenses.mit;
+    maintainers = with maintainers; [ zxc ];
+    mainProgram = "tp";
+  };
+}
