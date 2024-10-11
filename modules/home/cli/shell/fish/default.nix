a78bee92344bd0614c2f214f0ccabc3171019008
ourgal <git@fairever.aleeas.com>
2024-10-12 06:50:37 +0800

    chore: enable fish vi mode

--- modules/home/cli/shell/fish/default.nix
+++ modules/home/cli/shell/fish/default.nix
@@ -19,6 +19,7 @@ args.module (
             ''
               set fish_greeting
               scheme set tokyonight
+              set -g fish_key_bindings fish_vi_key_bindings
             '';
           shellAbbrs = {
             md = "mkdir -p";
