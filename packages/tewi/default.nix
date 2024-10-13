e7c1bc2ca85c4be9169a6a594dc2bc3543487d54
ourgal <git@fairever.aleeas.com>
2024-10-13 10:41:12 +0800

    feat: add tewi

---
+++ packages/tewi/default.nix
@@ -0,0 +1,59 @@
+{
+  lib,
+  python3,
+  pkgs,
+  namespace,
+}:
+let
+  textual = python3.pkgs.buildPythonApplication rec {
+    pname = "textual";
+    inherit (pkgs.${namespace}.sources.${pname}) version src;
+    pyproject = true;
+
+    nativeBuildInputs = [ python3.pkgs.poetry-core ];
+
+    propagatedBuildInputs = with python3.pkgs; [
+      markdown-it-py
+      rich
+      typing-extensions
+      platformdirs
+    ];
+
+    passthru.optional-dependencies = with python3.pkgs; {
+      syntax = [
+        tree-sitter
+        tree_sitter_languages
+      ];
+    };
+
+    pythonImportsCheck = [ "textual" ];
+  };
+  pname = "twei";
+  source = pkgs.${namespace}.sources.${pname};
+in
+python3.pkgs.buildPythonApplication rec {
+  inherit (source) pname version src;
+  pyproject = true;
+
+  nativeBuildInputs = with python3.pkgs; [
+    setuptools
+    wheel
+    pythonRelaxDepsHook
+  ];
+
+  propagatedBuildInputs = with python3.pkgs; [
+    textual
+    transmission-rpc
+  ];
+
+  pythonRelaxDeps = true;
+
+  meta = with lib; {
+    description = "Text-based interface for the Transmission BitTorrent daemon";
+    homepage = "https://github.com/anlar/tewi";
+    changelog = "https://github.com/anlar/tewi/blob/${src.rev}/CHANGELOG.md";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ zxc ];
+    mainProgram = "tewi";
+  };
+}
