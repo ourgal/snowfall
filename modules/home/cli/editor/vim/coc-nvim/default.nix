5a45e801125dcec76a5bf379a745fead24dc8703
ourgal <git@fairever.aleeas.com>
2024-10-18 23:55:37 +0800

    chore: add vim config

--- modules/home/cli/editor/vim/coc-nvim/default.nix
+++ modules/home/cli/editor/vim/coc-nvim/default.nix
@@ -4,7 +4,22 @@ args.module (
   // {
     path = ./.;
     progs.vim = {
-      plugins = with args.pkgs.vimPlugins; [ coc-nvim ];
+      plugins = with args.pkgs.vimPlugins; [
+        # keep-sorted start
+        coc-json
+        coc-nvim
+        coc-pyright
+        coc-r-lsp
+        coc-sh
+        coc-snippets
+        coc-sumneko-lua
+        coc-texlab
+        coc-toml
+        coc-tsserver
+        coc-vimlsp
+        coc-yaml
+        # keep-sorted end
+      ];
       extraConfig = builtins.readFile ./config.vim;
     };
   }
