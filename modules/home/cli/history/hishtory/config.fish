2f6e18555215bc7baaab82186b3ae79c9e5e660d
ourgal <git@fairever.aleeas.com>
2024-10-23 07:58:42 +0800

    feat: add hishtory

---
+++ modules/home/cli/history/hishtory/config.fish
@@ -0,0 +1,52 @@
+# For detecting color rendering support for this terminal, see #134
+hishtory getColorSupport
+export _hishtory_tui_color=$status
+
+function _hishtory_post_exec --on-event fish_preexec
+    # Runs after <ENTER>, but before the command is executed
+    set --global _hishtory_command $argv
+    set --global _hishtory_start_time (hishtory getTimestamp)
+    hishtory presaveHistoryEntry fish "$_hishtory_command" $_hishtory_start_time &
+    disown 2>&1 >/dev/null # Background Run
+    # hishtory presaveHistoryEntry fish "$_hishtory_command" $_hishtory_start_time 2>&1 >/dev/null # Foreground Run
+end
+
+set --global _hishtory_first_prompt 1
+
+function __hishtory_on_prompt --on-event fish_prompt
+    # Runs after the command is executed in order to render the prompt
+    # $? contains the exit code
+    set _hishtory_exit_code $status
+    if [ -n "$_hishtory_first_prompt" ]
+        set --global -e _hishtory_first_prompt
+    else if [ -n "$_hishtory_command" ]
+        hishtory saveHistoryEntry fish $_hishtory_exit_code "$_hishtory_command" $_hishtory_start_time &
+        disown 2>&1 >/dev/null # Background Run
+        # hishtory saveHistoryEntry fish $_hishtory_exit_code "$_hishtory_command" $_hishtory_start_time 2>&1 >/dev/null # Foreground Run
+        hishtory updateLocalDbFromRemote &
+        disown
+        set --global -e _hishtory_command # Unset _hishtory_command so we don't double-save entries when fish_prompt is invoked but fish_postexec isn't
+    end
+end
+
+function __hishtory_on_control_r
+    set -l tmp (mktemp -t fish.XXXXXX)
+    set -x init_query (commandline -b)
+    HISHTORY_TERM_INTEGRATION=1 HISHTORY_SHELL_NAME=fish hishtory tquery $init_query >$tmp
+    set -l res $status
+    commandline -f repaint
+    if [ -s $tmp ]
+        commandline -r -- (cat $tmp)
+    end
+    rm -f $tmp
+end
+
+if test (hishtory config-get enable-control-r) = true
+    if test $fish_key_bindings = fish_vi_key_bindings
+        bind --mode insert \cr __hishtory_on_control_r
+    else
+        bind \cr __hishtory_on_control_r
+    end
+end
+
+hishtory completion fish | source
