7ea01fa0f529bcf09bc6a5cb71289a328eadc997
ourgal <git@fairever.aleeas.com>
2024-10-23 20:21:18 +0800

    chore: replace vscode with vscodium

--- modules/home/desktop/editor/vscode/default.nix
+++ modules/home/desktop/editor/vscode/default.nix
@@ -1,24 +1,30 @@
 args:
 args.module (
   args
-  // {
-    path = ./.;
-    progs.vscode = {
-      mutableExtensionsDir = false;
-      enableUpdateCheck = false;
-      enableExtensionUpdateCheck = false;
-      userSettings = {
-        "files.autoSave" = "afterDelay";
-        "files.autoSaveDelay" = 1000;
-        "editor.fontFamily" = "'FiraCode Nerd Font Mono', 'Fira Code', monospace";
+  // (
+    let
+      inherit (args) pkgs;
+    in
+    {
+      path = ./.;
+      progs.vscode = {
+        mutableExtensionsDir = false;
+        enableUpdateCheck = false;
+        enableExtensionUpdateCheck = false;
+        userSettings = {
+          "files.autoSave" = "afterDelay";
+          "files.autoSaveDelay" = 1000;
+          "editor.fontFamily" = "'FiraCode Nerd Font Mono', 'Fira Code', monospace";
+        };
+        package = pkgs.vscodium;
       };
-    };
-    enable = [
-      # keep-sorted start
-      "even_better_toml"
-      "nix-ide"
-      "vim"
-      # keep-sorted end
-    ];
-  }
+      enable = [
+        # keep-sorted start
+        "even_better_toml"
+        "nix-ide"
+        "vim"
+        # keep-sorted end
+      ];
+    }
+  )
 )
