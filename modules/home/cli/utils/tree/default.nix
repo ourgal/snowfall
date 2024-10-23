942bfa34530ad2e983daa608c22a196818c4256f
ourgal <git@fairever.aleeas.com>
2024-10-23 10:27:11 +0800

    feat: add as-tree

--- modules/home/cli/utils/tree/default.nix
+++ modules/home/cli/utils/tree/default.nix
@@ -3,7 +3,12 @@ args.module (
   args
   // {
     path = ./.;
-    nixPkgs = "tre-command";
+    nixPkgs = [
+      # keep-sorted start
+      "as-tree"
+      "tre-command"
+      # keep-sorted end
+    ];
     progs.fish.functions.tre = {
       body = "command tre $argv -e; and source /tmp/tre_aliases_$USER ^/dev/null";
       description = "Tree";
