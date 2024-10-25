0113bb0077f976967b7eecb5916eec5bfd733cd8
ourgal <git@fairever.aleeas.com>
2024-10-25 15:30:26 +0800

    chore: add pet module

---
+++ modules/home/cli/snippet/pet/default.nix
@@ -0,0 +1,34 @@
+args:
+args.module (
+  args
+  // {
+    path = ./.;
+    progs.pet = {
+      settings = {
+        General = {
+          format = "[$description]	$tags	$command";
+        };
+      };
+      snippets = [
+        {
+          command = ''
+            set -o errexit
+            set -o nounset
+            set -o pipefail
+
+            nap ci/treefmt.toml > treefmt.toml
+            nap ci/lefthook.yml > lefthook.yml
+            mkdir -p .lefthook/commit-msg/
+            nap ci/commitlint.sh > .lefthook/commit-msg/commitlint.sh
+            lefthook install
+          '';
+          description = "lefthook";
+          tag = [
+            "git"
+            "hook"
+          ];
+        }
+      ];
+    };
+  }
+)
