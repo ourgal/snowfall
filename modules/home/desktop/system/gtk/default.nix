3304211ef6f82ccb092cf29991aa77bd1f456c2b
ourgal <git@fairever.aleeas.com>
2024-10-25 13:07:47 +0800

    chore: remove gtk catppuccin theme

--- modules/home/desktop/system/gtk/default.nix
+++ modules/home/desktop/system/gtk/default.nix
@@ -10,18 +10,17 @@ args.module (
       value = {
         # Theme GTK
         gtk = enabled // {
-          catppuccin = enabled;
+          theme = {
+            name = "Adwaita";
+            package = pkgs.gnome.gnome-themes-extra;
+          };
           font = {
             name = "Ubuntu";
             size = 12;
             package = pkgs.ubuntu_font_family;
           };
-          gtk3.extraConfig = {
-            gtk-application-prefer-dark-theme = 1;
-          };
-          gtk4.extraConfig = {
-            gtk-application-prefer-dark-theme = 1;
-          };
+          gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+          gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
         };
       };
     }
