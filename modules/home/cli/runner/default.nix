479bf12631cc803f76b41f5c2bc199f34abd31a6
ourgal <git@fairever.aleeas.com>
2024-10-28 10:39:08 +0800

    feat: add mmake

--- modules/home/cli/runner/default.nix
+++ modules/home/cli/runner/default.nix
@@ -8,6 +8,7 @@ args.module (
       "await"
       "gnumake"
       "just"
+      "mmake"
       "mprocs"
       "runme" # https://github.com/stateful/runme/issues/647#issuecomment-2327538200
       # keep-sorted end
