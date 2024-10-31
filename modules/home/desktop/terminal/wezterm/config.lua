b2cd85c5fc13ee6afad483c887ce8d8eb4fbdf96
ourgal <git@fairever.aleeas.com>
2024-10-31 17:08:41 +0800

    chore: change wezterm config

---
+++ modules/home/desktop/terminal/wezterm/config.lua
@@ -0,0 +1,14 @@
+local wezterm = require("wezterm")
+
+return {
+  color_scheme = "tokyonight_night",
+  window_decorations = "NONE",
+  font = wezterm.font_with_fallback({
+    "Fira Code",
+    "Source Han Sans SC",
+  }),
+  font_size = 14.0,
+  window_background_opacity = 0.8,
+  default_cursor_style = "SteadyBar",
+  hide_tab_bar_if_only_one_tab = true,
+}
