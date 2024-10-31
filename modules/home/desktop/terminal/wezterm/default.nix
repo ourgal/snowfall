91345641cd68e5416f5142b2b7ccd358d3a3d332
ourgal <git@fairever.aleeas.com>
2024-10-31 17:08:41 +0800

    chore: change wezterm config

--- modules/home/desktop/terminal/wezterm/default.nix
+++ modules/home/desktop/terminal/wezterm/default.nix
@@ -3,21 +3,6 @@ args.module (
   args
   // {
     path = ./.;
-    progs.wezterm.extraConfig = # lua
-      ''
-        local wezterm = require("wezterm")
-
-        return {
-        	color_scheme = "tokyonight_night",
-        	window_decorations = "NONE",
-          font = wezterm.font_with_fallback({
-            "Fira Code",
-            "Source Han Sans SC",
-          }),
-        	font_size = 14.0,
-        	window_background_opacity = 0.8,
-        	hide_tab_bar_if_only_one_tab = true,
-        }
-      '';
+    progs.wezterm.extraConfig = builtins.readFile ./config.lua;
   }
 )
