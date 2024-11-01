54ac3f3e365b7070bd40dc6560c7cc28c9b84510
ourgal <git@fairever.aleeas.com>
2024-11-01 13:13:05 +0800

    refactor: rework yt

--- packages/yt/default.nix
+++ packages/yt/default.nix
@@ -1,22 +1,45 @@
 {
-  writeShellApplication,
-  lib,
+  pog,
   yt-dlp,
   mpv,
   ...
 }:
-writeShellApplication rec {
+pog.pog {
   name = "yt";
+  version = "0.1.0";
+  description = "A script for Youtube";
 
-  meta = {
-    mainProgram = name;
-    platforms = lib.platforms.linux;
-  };
+  arguments = [ { name = "url"; } ];
+
+  flags = [
+    {
+      name = "audio";
+      description = "download mp3 audio";
+      bool = true;
+    }
+    {
+      name = "audio_dir";
+      short = "";
+      argument = "dir";
+      description = "dir for audio";
+      default = "$HOME/Music/youtube_dl/";
+    }
+  ];
 
   runtimeInputs = [
     yt-dlp
     mpv
   ];
 
-  text = builtins.readFile ./yt.sh;
+  script =
+    helpers: with helpers; ''
+      if ${flag "audio"}; then
+        mkdir -p "$audio_dir" > /dev/null 2>&1
+        yt-dlp --trim-filenames 50 --no-write-subs --no-embed-subs --no-write-auto-subs -P "$audio_dir" -f 'ba' -x --audio-format mp3 "$1"
+      else
+        filename=$(yt-dlp --print filename --no-simulate -P ~/.cache/yt-dlp "$1")
+        mpv --speed=3 "$filename"
+        rm "$filename"
+      fi
+    '';
 }
