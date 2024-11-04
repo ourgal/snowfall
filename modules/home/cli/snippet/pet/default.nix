e7e728b015c939cce093f4387375ce7a0523e90b
ourgal <git@fairever.aleeas.com>
2024-11-04 17:36:23 +0800

    chore: add pet snippet

--- modules/home/cli/snippet/pet/default.nix
+++ modules/home/cli/snippet/pet/default.nix
@@ -36,6 +36,11 @@ args.module (
             "motherboard"
           ];
         }
+        {
+          command = "ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'";
+          description = "current lan ip";
+          tag = [ "network" ];
+        }
       ];
     };
   }
