5240d72f90687d2c1a3e97222ea5ddc6a855aff8
ourgal <git@fairever.aleeas.com>
2024-11-11 13:02:41 +0800

    chore: replace emacs-gtk with emacs

--- modules/home/desktop/editor/emacs/default.nix
+++ modules/home/desktop/editor/emacs/default.nix
@@ -15,7 +15,7 @@ args.module (
       ];
       progs.emacs = {
         extraPackages = epkgs: [ epkgs.vterm ];
-        package = pkgs.emacs-gtk;
+        package = pkgs.emacs;
       };
       servs = "emacs";
       value.systemd.user.services.emacs.Service.ExecStartPre = "${pkgs.coreutils-full}/bin/sleep 10";
