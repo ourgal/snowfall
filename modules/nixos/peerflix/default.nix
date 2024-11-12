69354102fe4940a0c654aec1d8e97b5b138f1616
ourgal <git@fairever.aleeas.com>
2024-11-13 02:32:21 +0800

    feat: add peerflix-server

---
+++ modules/nixos/peerflix/default.nix
@@ -0,0 +1,17 @@
+args:
+let
+  inherit (args) namespace lib config;
+  inherit (lib.${namespace}) nixosModule enabled;
+  user = config.${namespace}.user.name;
+  value = {
+    services.peerflix = enabled // {
+      downloadDir = "/home/${user}/Downloads/peerflix";
+    };
+    users.users.${user}.extraGroups = [ "peerflix" ];
+  };
+  path = ./.;
+  _args = {
+    inherit value path args;
+  };
+in
+nixosModule _args
