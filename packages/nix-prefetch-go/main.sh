3b1cc260a7897dddaf847937ce50518e7790641c
ourgal <git@fairever.aleeas.com>
2024-11-12 16:29:54 +0800

    revert: remove nix-prefetch-go and nix-prefetch-rust

--- packages/nix-prefetch-go/main.sh
+++
@@ -1,1 +0,0 @@
-nix-prefetch --option extra-experimental-features flakes "{ sha256 }: (builtins.getFlake (toString ./.)).packages.x86_64-linux.$1.goModules.overrideAttrs (_: { vendorSha256 = sha256; })" | tee /dev/tty | xclip
