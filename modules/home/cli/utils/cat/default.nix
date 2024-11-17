462ddce4ed04fa69740cd50ec72f6faf4c1cc890
ourgal <git@fairever.aleeas.com>
2024-11-17 19:17:41 +0800

    chore: add bat config

--- modules/home/cli/utils/cat/default.nix
+++ modules/home/cli/utils/cat/default.nix
@@ -4,21 +4,12 @@ args.module (
   // {
     path = ./.;
     myPkgs = "batwhich";
-    batPkgs = [
-      # keep-sorted start
-      "batdiff"
-      "batgrep"
-      "batman"
-      "batpipe"
-      "batwatch"
-      # keep-sorted end
-    ];
     nixPkgs = [
       # keep-sorted start
       "as-tree"
-      "bat"
       "gat"
       # keep-sorted end
     ];
+    enable = "bat";
   }
 )
