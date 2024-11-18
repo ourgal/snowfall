c672332fcd70103c7f431ee24fff6f9d0bdb1a8a
ourgal <git@fairever.aleeas.com>
2024-11-18 14:13:52 +0800

    chore: add mpv config modules

---
+++ modules/home/desktop/video/mpv/base/default.nix
@@ -0,0 +1,35 @@
+args:
+args.module (
+  args
+  // {
+    path = ./.;
+    progs.mpv = {
+      catppuccin.enable = false;
+      config = {
+        # profile = "gpu-hq";
+        # icc-profile = "auto";
+        # vf-append = "format=gamma=gamma2.2";
+        blend-subtitles = "video";
+        # video-sync = "display-resample";
+        # interpolation = true;
+        # tscale = "oversample";
+        af = "scaletempo2";
+        save-position-on-quit = true;
+        pause = "no";
+        fullscreen = "yes";
+        force-seekable = "yes";
+        keep-open = "no";
+        sub-auto = "fuzzy";
+        alang = "ja,jp,jpn,en,eng";
+        slang = "en,eng,chs,sc,zh,chi,zho,zh-Hant,zh-CN,zh-Hans";
+        volume = 100;
+        audio-file-auto = "fuzzy";
+        volume-max = 200;
+        audio-pitch-correction = "yes";
+        hwdec = "auto";
+        audio-device = "alsa";
+        input-ipc-server = "/tmp/mpvsocket";
+      };
+    };
+  }
+)
---
+++ modules/home/desktop/video/mpv/plugins/default.nix
@@ -0,0 +1,78 @@
+args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) namespace lib pkgs;
+      inherit (lib.${namespace}) with';
+    in
+    {
+      path = ./.;
+      progs.mpv = {
+        scripts =
+          with' pkgs.mpvScripts [
+            "mpris"
+            "quack"
+            "sponsorblock"
+            "thumbfast"
+            "uosc"
+          ]
+          ++ with' pkgs.${namespace} [
+            "mpv-autoloop"
+            "mpv-smartcopypaste_ii"
+            "mpv-smartskip"
+            "mpv-M-x"
+            "mpv-btfs-stream"
+          ];
+        scriptOpts = {
+          uosc = {
+            autohide = "yes";
+          };
+          SmartSkip = {
+            silence_audio_level = "-60";
+            silence_duration = "0.85";
+            silence_skip_to_end = "cancel";
+            min_skip_duration = 0;
+            max_skip_duration = 0;
+            autoload_playlist = "no";
+            ignore_silence_duration = 5;
+          };
+          autoloop = {
+            autoloop_duration = 360;
+          };
+          M-x = {
+            # strip cmd name
+            strip_cmd_at = 65;
+            # available values: priority, command_name
+            sort_commands_by = "priority";
+            #- options for extended menu ------------------------------------------------
+            toggle_menu_binding = "t";
+            lines_to_show = 17;
+            pause_on_open = "yes";
+            resume_on_exit = "only-if-was-paused";
+
+            #- styles
+            font_size = 21;
+            # cursor 'width', useful to change if you have hidpi monitor
+            cursor_x_border = 0.3;
+            line_bottom_margin = 1;
+            menu_x_padding = 5;
+            menu_y_padding = 2;
+
+            # heading text of a search bar
+            search_heading = "M-x";
+            # will determine in which fields to look for your search query. For instance if
+            # you don't want to search in comments to commands then just omit it from initial
+            # table.
+            filter_by_fields = [
+              "cmd"
+              "key"
+              "comment"
+            ];
+            column_layout = "no";
+          };
+        };
+      };
+    }
+  )
+)
