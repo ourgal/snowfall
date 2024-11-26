64de9e7578f7b75699fcf6a20d355f0090eaca37
ourgal <git@fairever.aleeas.com>
2024-11-26 14:17:02 +0800

    feat: add miniflux-sync

---
+++ packages/miniflux-sync/default.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  buildGoModule,
+  _sources,
+}:
+
+buildGoModule {
+  inherit (_sources.miniflux-sync) pname version src;
+
+  vendorHash = "sha256-+Y4MD9HBjKn09+1KhgU9u7xaj/MhG9k7S48g/sprB1o=";
+
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  meta = with lib; {
+    description = "Manage and sync your Miniflux feeds with YAML";
+    homepage = "https://github.com/revett/miniflux-sync";
+    license = licenses.mit;
+    maintainers = with maintainers; [ zxc ];
+    mainProgram = "miniflux-sync";
+  };
+}
