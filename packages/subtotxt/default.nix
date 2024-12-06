030c545f82cb80dc3ed76349d01ceed0ebc55881
ourgal <git@fairever.aleeas.com>
2024-12-06 17:39:32 +0800

    feat: add subtotxt

---
+++ packages/subtotxt/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  python3,
+  _sources,
+}:
+python3.pkgs.buildPythonApplication {
+  inherit (_sources.subtotxt) pname version src;
+  format = "other";
+
+  propagatedBuildInputs = with python3.pkgs; [
+    send2trash
+    charset-normalizer
+    setuptools
+  ];
+
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+    sed -i "1i #!/usr/bin/env python" subtotxt.py
+    install -m755 -D subtotxt.py $out/bin/subtotxt
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Quickly convert an .srt or .vtt to plain text, removes timestamps and .srt subtitle line numbers";
+    homepage = "https://github.com/NebularNerd/subtotxt";
+    license = lib.licenses.cc0;
+    maintainers = with lib.maintainers; [ zxc ];
+    mainProgram = "subtotxt";
+    platforms = lib.platforms.all;
+  };
+}
