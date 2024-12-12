dce3e9663535de3b81e297f9247c8748693c8a29
ourgal <git@fairever.aleeas.com>
2024-12-12 23:26:57 +0800

    fix: exit if emacs not install

--- modules/home/cli/file-manager/chezmoi/config/dot_config/run_onchange_after_doom.sh
+++ modules/home/cli/file-manager/chezmoi/config/dot_config/run_onchange_after_doom.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 # hash {{ output "nix" "hash" "path" (joinPath .chezmoi.homeDir ".config/doom" | quote) | trim }}
+if ! command -v emacs >/dev/null; then
+  echo 'emacs is not installed'
+  exit
+fi
 cd doom || exit
 make
