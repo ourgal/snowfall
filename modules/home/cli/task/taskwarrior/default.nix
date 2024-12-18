4cb67e8de48aa86203fd70d0cd202dcb2555927c
ourgal <git@fairever.aleeas.com>
2024-12-18 14:47:03 +0800

    fix: set taskwarrior hooks executable

--- modules/home/cli/task/taskwarrior/default.nix
+++ modules/home/cli/task/taskwarrior/default.nix
@@ -6,6 +6,7 @@ args.module (
       inherit (args) pkgs;
       better-recur = pkgs._sources.taskwarrior-better-recur.src;
       relative-recur = pkgs._sources.task-relative-recur.src;
+      timewarrior = pkgs._sources.timewarrior.src;
     in
     {
       path = ./.;
@@ -20,12 +21,12 @@ args.module (
       progs.taskwarrior.package = pkgs.taskwarrior3;
       value = {
         home.file = {
-          ".local/share/task/hooks/on-modify.timewarrior".source = ./on-modify.timewarrior;
+          ".local/share/task/hooks/on-modify.timewarrior".source = timewarrior + "/ext/on-modify.timewarrior";
           ".local/share/task/hooks/on-modify.relative-recur".source =
             relative-recur + "/on-modify.relative-recur";
           ".local/share/task/hooks/on-modify-complete-recur".source =
             better-recur + "/on-modify-complete-recur";
-          ".local/share/task/hooks/on-add-expire".source = ./on-add-expire;
+          ".local/share/task/hooks/on-add-expire".source = better-recur + "/on-add-expire";
         };
       };
     }
