09e7d404f569e0972256798f463ef2dbbb8adc85
ourgal <git@fairever.aleeas.com>
2024-12-25 13:11:37 +0800

    chore: increase atuin sync interval

--- modules/home/cli/history/atuin/default.nix
+++ modules/home/cli/history/atuin/default.nix
@@ -6,7 +6,7 @@ args.module (
     progs.atuin.settings = {
       enter_accept = true;
       sync_address = "https://api.atuin.sh";
-      sync_frequency = "5m";
+      sync_frequency = "1h";
       dotfiles.enabled = true;
       sync.records = true;
     };
