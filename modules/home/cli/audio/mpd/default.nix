2aa11eea4d7035f76d655221b667d8122fe6d5dd
ourgal <git@fairever.aleeas.com>
2024-12-28 20:41:43 +0800

    chore: enable mpd http output

--- modules/home/cli/audio/mpd/default.nix
+++ modules/home/cli/audio/mpd/default.nix
@@ -40,8 +40,30 @@ args.module (
         "mpd-mpris"
         {
           mpd = {
+            network = {
+              listenAddress = "any";
+              startWhenNeeded = true;
+            };
             extraConfig = ''
               auto_update "yes"
+              audio_output {
+                  type        "pulse"
+                  name        "My Pulse Output"
+              #   server      "remote_server"     # optional
+              #   sink        "remote_server_sink"    # optional
+              }
+
+              audio_output {
+                  type        "httpd"
+                  name        "My HTTP Stream"
+                  encoder     "opus"      # optional
+                  port        "8000"
+                  # quality   "5.0"       # do not define if bitrate is defined
+                  bitrate     "128000"    # do not define if quality is defined
+                  format      "48000:16:1"
+                  always_on   "yes"       # prevent MPD from disconnecting all listeners when playback is stopped.
+                  tags        "yes"       # httpd supports sending tags to listening streams.
+              }
             '';
           };
         }
