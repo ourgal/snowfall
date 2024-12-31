7c2787bcd50a1b3e1efda6d6d1a1841de182f6da
ourgal <git@fairever.aleeas.com>
2024-12-31 11:17:12 +0800

    feat: add ashuffle

---
+++ modules/home/cli/audio/mpd/ashuffle/default.nix
@@ -0,0 +1,21 @@
+args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) pkgs;
+    in
+    {
+      path = ./.;
+      nixPkgs = "ashuffle";
+      systemdServices.ashuffle = {
+        Unit = {
+          Requires = "dbus.socket";
+          After = "mpd.service";
+        };
+        restart = "always";
+        start = "${pkgs.ashuffle}/bin/ashuffle";
+      };
+    }
+  )
+)
