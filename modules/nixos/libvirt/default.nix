8c7d2738eac7abf442831564987296ceeea41e30
ourgal <git@fairever.aleeas.com>
2025-01-15 03:35:16 +0800

    feat: add libvirt

---
+++ modules/nixos/libvirt/default.nix
@@ -0,0 +1,18 @@
+args:
+let
+  inherit (args) namespace lib config;
+  inherit (lib.${namespace}) nixosModule enabled;
+  user = config.${namespace}.user.name;
+  value = {
+    virtualisation.libvirtd = enabled;
+    users.users.${user}.extraGroups = [
+      "libvirtd"
+      "qemu-libvirtd"
+    ];
+  };
+  path = ./.;
+  _args = {
+    inherit value path args;
+  };
+in
+nixosModule _args
