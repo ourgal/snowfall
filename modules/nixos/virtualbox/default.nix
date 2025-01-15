57ea6a33ce55798caf8775c114fa486710bcfc99
ourgal <git@fairever.aleeas.com>
2025-01-15 12:52:41 +0800

    feat: add virtualbox

---
+++ modules/nixos/virtualbox/default.nix
@@ -0,0 +1,17 @@
+args:
+let
+  inherit (args) namespace lib config;
+  inherit (lib.${namespace}) nixosModule enabled;
+  user = config.${namespace}.user.name;
+  value = {
+    virtualisation.virtualbox.host = enabled // {
+      headless = true;
+    };
+    users.users.${user}.extraGroups = [ "vboxusers" ];
+  };
+  path = ./.;
+  _args = {
+    inherit value path args;
+  };
+in
+nixosModule _args
