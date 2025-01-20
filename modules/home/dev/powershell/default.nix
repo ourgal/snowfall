d607ac02851117a10d0975c0d103f49248ad2006
ourgal <git@fairever.aleeas.com>
2025-01-20 15:32:06 +0800

    feat: add powershell-beautifier

--- modules/home/dev/powershell/default.nix
+++ modules/home/dev/powershell/default.nix
@@ -3,6 +3,7 @@ args.module (
   args
   // {
     path = ./.;
+    myPkgs = "powershell-beautifier";
     nixPkgs = [
       # keep-sorted start
       "powershell"
---
+++ packages/powershell-beautifier/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  stdenv,
+  powershell,
+  writeShellScriptBin,
+  fetchFromGitHub,
+}:
+let
+  powershell-beautifier = stdenv.mkDerivation rec {
+    pname = "powershell-beautifier";
+    version = "1.2.5";
+
+    src = fetchFromGitHub {
+      owner = "DTW-DanWard";
+      repo = "PowerShell-Beautifier";
+      rev = "V${version}";
+      hash = "sha256-N/Wz2TDgQFEW//QR5SwzFFJAiRx8+RoMS842m27mYjM=";
+    };
+
+    dontBuild = true;
+
+    installPhase = ''
+      mkdir -p $out/share/powershell/Modules/PowerShell-Beautifier
+      cp -r ./* $out/share/powershell/Modules/PowerShell-Beautifier
+      substituteInPlace $out/share/powershell/Modules/PowerShell-Beautifier/src/DTW.PS.Beautifier.PopulateValidNames.psm1 \
+        --replace-fail 'Join-Path -Path $PSScriptRoot -ChildPath "DTW.PS.BeautifierValidValuesCache.txt"' 'Join-Path -Path $HOME -ChildPath ".cache/DTW.PS.BeautifierValidValuesCache.txt"'
+    '';
+
+    meta = {
+      description = "A whitespace reformatter and code cleaner for Windows PowerShell and PowerShell Core";
+      homepage = "https://github.com/DTW-DanWard/PowerShell-Beautifier";
+      license = lib.licenses.mit;
+      maintainers = with lib.maintainers; [ zxc ];
+      mainProgram = "power-shell-beautifier";
+      platforms = lib.platforms.all;
+    };
+  };
+in
+writeShellScriptBin "powershell-beautifier" ''
+  ${powershell}/bin/pwsh -NoProfile -NoLogo \
+  -Command "Import-Module ${powershell-beautifier}/share/powershell/Modules/PowerShell-Beautifier/PowerShell-Beautifier.psd1; Edit-DTWBeautifyScript -SourcePath \"$@\""
+''
