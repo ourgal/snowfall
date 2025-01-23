a77f6a08d6ca0e06eb86a49e5709a98d20cd82e5
ourgal <git@fairever.aleeas.com>
2025-01-23 23:13:09 +0800

    feat: add ack-vim

---
+++ modules/home/cli/editor/vim/search/ack-vim/default.nix
@@ -0,0 +1,16 @@
+args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) pkgs;
+    in
+    {
+      path = ./.;
+      progs.vim = {
+        plugins = with pkgs.vimPlugins; [ ack-vim ];
+        extraConfig = builtins.readFile ./config.vim;
+      };
+    }
+  )
+)
