f74ddb09868da14b4a3a348ef830fc5eb0d34ea8
ourgal <git@fairever.aleeas.com>
2025-01-23 20:43:18 +0800

    feat: add vim-ipython

---
+++ modules/home/cli/editor/vim/lang/python/vim-ipython/default.nix
@@ -0,0 +1,15 @@
+args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) pkgs;
+    in
+    {
+      path = ./.;
+      progs.vim = {
+        plugins = with pkgs.vimPlugins; [ vim-ipython ];
+      };
+    }
+  )
+)
