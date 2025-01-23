86e8ded383e6f1a6dc89ee9c89a28da2a50a4215
ourgal <git@fairever.aleeas.com>
2025-01-23 19:21:21 +0800

    chore: update hash

--- packages/vim-dirvish-dovish/default.nix
+++ packages/vim-dirvish-dovish/default.nix
@@ -1,19 +1,11 @@
 {
   lib,
   vimUtils,
-  fetchFromGitHub,
+  _sources,
 }:
 
 vimUtils.buildVimPlugin {
-  pname = "vim-dirvish-dovish";
-  version = "unstable-2022-06-10";
-
-  src = fetchFromGitHub {
-    owner = "roginfarrer";
-    repo = "vim-dirvish-dovish";
-    rev = "04c77b6010f7e45e72b4d3c399c120d42f7c5d47";
-    hash = "sha256-8uw1Ft48JVphhdhd9TBcB3b9NFBRkwdEQH2LZblE1dk=";
-  };
+  inherit (_sources.vim-dirvish-dovish) pname version src;
 
   meta = {
     description = "File manipulation commands for vim-dirvish";
