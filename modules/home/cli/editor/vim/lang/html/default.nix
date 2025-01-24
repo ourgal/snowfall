88d9e3beda6d4740fad352dd0cf6f0d1e18de595
ourgal <git@fairever.aleeas.com>
2025-01-24 17:05:06 +0800

    feat: add vim-closetag

--- modules/home/cli/editor/vim/lang/html/default.nix
+++ modules/home/cli/editor/vim/lang/html/default.nix
@@ -3,6 +3,11 @@ args.module (
   args
   // {
     path = ./.;
-    enable = "tagalong-vim";
+    enable = [
+      # keep-sorted start
+      "tagalong-vim"
+      "vim-closetag"
+      # keep-sorted end
+    ];
   }
 )
