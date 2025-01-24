6064f310160b8b5f0c524d14c292ccb00d506833
ourgal <git@fairever.aleeas.com>
2025-01-25 00:08:15 +0800

    feat: add vim-markdown

--- modules/home/cli/editor/vim/lang/markdown/default.nix
+++ modules/home/cli/editor/vim/lang/markdown/default.nix
@@ -5,10 +5,15 @@ args.module (
     path = ./.;
     enable = [
       # keep-sorted start
-      "mkdx"
       "pangu-vim"
-      "vim-table-mode"
+      "vim-markdown"
       "wiki-vim"
+      # keep-sorted end
+    ];
+    disable = [
+      # keep-sorted start
+      "mkdx"
+      "vim-table-mode"
       # keep-sorted end
     ];
   }
