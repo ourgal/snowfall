7f8c76c3f6da26480c78409488744932872522e8
ourgal <git@fairever.aleeas.com>
2025-01-26 03:00:53 +0800

    fix: change geoclue2 url

--- modules/nixos/location/default.nix
+++ modules/nixos/location/default.nix
@@ -2,8 +2,12 @@ args:
 let
   inherit (args) namespace lib;
   inherit (lib.${namespace}) nixosModule enabled;
+  url = "https://beacondb.net/v1/geolocate"; # https://bugzilla.redhat.com/show_bug.cgi?id=2284621
   value = {
-    services.geoclue2 = enabled;
+    services.geoclue2 = enabled // {
+      geoProviderUrl = url;
+      submissionUrl = url;
+    };
   };
   path = ./.;
   _args = {
