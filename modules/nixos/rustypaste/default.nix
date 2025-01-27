cb2c71504996c3704776bc5e63b67b24029a47a2
ourgal <git@fairever.aleeas.com>
2025-01-28 03:55:40 +0800

    chore: generate rustypaste config by nix lib

--- modules/nixos/rustypaste/default.nix
+++ modules/nixos/rustypaste/default.nix
@@ -1,45 +1,31 @@
 args:
 let
   inherit (args) namespace lib pkgs;
-  inherit (lib.${namespace}) nixosModule enabled domains;
+  inherit (lib.${namespace})
+    nixosModule
+    enabled
+    domains
+    toTOML
+    ;
   port = 8000;
   user = "rustypaste";
   dataDir = "/var/lib/rustypaste";
   value = {
-    systemd.tmpfiles.rules = [ "d '${dataDir}' 0750 ${user} ${user} - -" ];
-    systemd.services.rustypaste = {
-      description = "Rustypaste service";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        Restart = "on-failure";
-        preStart = ''
-          umask u=rwx,g=rx,o=
-          cat > "${dataDir}/config.toml" <<EOF
-          [config]
-          refresh_rate = "1s"
-
-          [server]
-          address = "127.0.0.1:8000"
-          #url = "https://paste.example.com"
-          #workers=4
-          max_content_length = "10MB"
-          upload_path = "./upload"
-          timeout = "30s"
-          expose_version = false
-          expose_list = false
-          #auth_tokens = [
-          #  "super_secret_token1",
-          #  "super_secret_token2",
-          #]
-          #delete_tokens = [
-          #  "super_secret_token1",
-          #  "super_secret_token3",
-          #]
-          handle_spaces = "replace" # or "encode"
-
-          [landing_page]
-          text = """
+    environment.etc."rustypaste/config.toml".text = toTOML {
+      config = {
+        refresh_rate = "1s";
+      };
+      server = {
+        address = "127.0.0.1:${toString port}";
+        max_content_length = "10MB";
+        upload_path = "./upload";
+        timeout = "30s";
+        expose_version = false;
+        expose_list = false;
+        handle_spaces = "replace";
+      };
+      landing_page = {
+        text = ''
           ┬─┐┬ ┬┌─┐┌┬┐┬ ┬┌─┐┌─┐┌─┐┌┬┐┌─┐
           ├┬┘│ │└─┐ │ └┬┘├─┘├─┤└─┐ │ ├┤
           ┴└─└─┘└─┘ ┴  ┴ ┴  ┴ ┴└─┘ ┴ └─┘
@@ -60,39 +46,70 @@ let
 
           Check out the GitHub repository at https://github.com/orhun/rustypaste
           Command line tool is available  at https://github.com/orhun/rustypaste-cli
-          """
-          #file = "index.txt"
-          content_type = "text/plain; charset=utf-8"
-
-          [paste]
-          random_url = { type = "petname", words = 2, separator = "-" }
-          #random_url = { type = "alphanumeric", length = 8 }
-          #random_url = { type = "alphanumeric", length = 6, suffix_mode = true }
-          default_extension = "txt"
-          mime_override = [
-            { mime = "image/jpeg", regex = "^.*\\.jpg$" },
-            { mime = "image/png", regex = "^.*\\.png$" },
-            { mime = "image/svg+xml", regex = "^.*\\.svg$" },
-            { mime = "video/webm", regex = "^.*\\.webm$" },
-            { mime = "video/x-matroska", regex = "^.*\\.mkv$" },
-            { mime = "application/octet-stream", regex = "^.*\\.bin$" },
-            { mime = "text/plain", regex = "^.*\\.(log|txt|diff|sh|rs|toml)$" },
-          ]
-          mime_blacklist = [
-            "application/x-dosexec",
-            "application/java-archive",
-            "application/java-vm",
-          ]
-          duplicate_files = true
-          # default_expiry = "1h"
-          delete_expired_files = { enabled = true, interval = "1h" }
-          EOF
         '';
+        content_type = "text/plain; charset=utf-8";
+      };
+      paste = {
+        random_url = {
+          type = "petname";
+          words = 2;
+          separator = "-";
+        };
+        default_extension = "txt";
+        mime_override = [
+          {
+            mime = "image/jpeg";
+            regex = "^.*\\.jpg$";
+          }
+          {
+            mime = "image/png";
+            regex = "^.*\\.png$";
+          }
+          {
+            mime = "image/svg+xml";
+            regex = "^.*\\.svg$";
+          }
+          {
+            mime = "video/webm";
+            regex = "^.*\\.webm$";
+          }
+          {
+            mime = "video/x-matroska";
+            regex = "^.*\\.mkv$";
+          }
+          {
+            mime = "application/octet-stream";
+            regex = "^.*\\.bin$";
+          }
+          {
+            mime = "text/plain";
+            regex = "^.*\\.(log|txt|diff|sh|rs|toml)$";
+          }
+        ];
+        mime_blacklist = [
+          "application/x-dosexec"
+          "application/java-archive"
+          "application/java-vm"
+        ];
+        duplicate_files = true;
+        delete_expired_files = {
+          enabled = true;
+          interval = "1h";
+        };
+      };
+    };
+    systemd.tmpfiles.rules = [ "d '${dataDir}' 0750 ${user} ${user} - -" ];
+    systemd.services.rustypaste = {
+      description = "Rustypaste service";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Restart = "on-failure";
         ExecStart = "${pkgs.rustypaste}/bin/rustypaste";
         WorkingDirectory = dataDir;
       };
       environment = {
-        CONFIG = "${dataDir}/config.toml";
+        CONFIG = "/etc/rustypaste/config.toml";
         SERVER__ADDRESS = "0.0.0.0:${toString port}";
       };
     };
