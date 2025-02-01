40a9d9d11ce7abb7fe7d62651662bea40f929a1c
ourgal <git@fairever.aleeas.com>
2025-02-01 19:01:55 +0800

    chore: enable home syncthing

--- modules/home/cli/share/syncthing/default.nix
+++ modules/home/cli/share/syncthing/default.nix
@@ -3,9 +3,14 @@ args.module (
   args
   // (
     let
-      inherit (args) config pkgs;
+      inherit (args)
+        config
+        pkgs
+        enabled
+        lib
+        ;
       ln = config.lib.file.mkOutOfStoreSymlink;
-      sync = "/var/lib/syncthing";
+      sync = "${config.xdg.dataHome}/syncthing";
     in
     {
       path = ./.;
@@ -25,6 +30,34 @@ args.module (
         Music = ln "${sync}/music";
         Documents = ln "${sync}/documents";
       };
+      servs.syncthing = enabled // {
+        # inherit guiAddress;
+        # key = config.sops.secrets."syncthing/${host}/key".path;
+        # cert = config.sops.secrets."syncthing/${host}/cert".path;
+        extraOptions = [ "--no-default-folder" ];
+        # openDefaultPorts = true;
+        # settings = {
+        #   gui = {
+        #     insecureAdminAccess = true;
+        #   };
+        #   options = {
+        #     relaysEnabled = true;
+        #     natEnabled = true;
+        #     localAnnounceEnabled = true;
+        #     localAnnouncePort = 21027;
+        #     urAccepted = -1;
+        #   };
+        #   devices = lib.attrsets.filterAttrs (n: _: n != host) devices;
+        #   folders = mkSyncthingFolders { inherit host folders; };
+        # };
+      };
+      # value = {
+      #   systemd.user.syncthing-init.Service.ExecStartPost = updateConfig;
+      #   sops.secrets = {
+      #     "syncthing/${host}/key".mode = "0400";
+      #     "syncthing/${host}/cert".mode = "0400";
+      #   };
+      # };
     }
   )
 )
