6d52c5814fb72033ead2b2566befd8534850a847
ourgal <git@fairever.aleeas.com>
2025-02-02 20:11:13 +0800

    chore: add mbr and uefi

---
+++ systems/x86_64-linux/nixos-uefi/default.nix
@@ -0,0 +1,25 @@
+{
+  lib,
+  namespace,
+  config,
+  ...
+}:
+let
+  inherit (lib.${namespace}) enabled disabled;
+  user = config.${namespace}.user.name;
+in
+{
+
+  dot = {
+    user.host = "nixos-uefi";
+    boot.systemd = enabled;
+    common = enabled;
+    nh = enabled;
+    disko = enabled;
+    tuptime = enabled;
+  };
+
+  snowfallorg.users.${user}.home = disabled;
+
+  system.stateVersion = "24.11";
+}
