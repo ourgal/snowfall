80e104eebfef66ddb9eeb06545dfeabfcdffeb2b
ourgal <git@fairever.aleeas.com>
2025-02-07 20:10:09 +0800

    feat: add jackett

---
+++ modules/nixos/jackett/default.nix
@@ -0,0 +1,24 @@
+args:
+let
+  inherit (args) namespace lib;
+  inherit (lib.${namespace}) nixosModule enabled domains;
+  port = 9117;
+  value = {
+    services = {
+      jackett = enabled // {
+        openFirewall = true;
+        inherit port;
+      };
+      caddy = enabled // {
+        virtualHosts = {
+          "http://${domains.jackett}".extraConfig = ''
+            reverse_proxy http://localhost:${toString port}
+          '';
+        };
+      };
+    };
+  };
+  path = ./.;
+  _args = { inherit value path args; };
+in
+nixosModule _args
