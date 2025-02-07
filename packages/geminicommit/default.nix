fe9cd380fb5b9a6fffd27e76470e8e90767e4eed
ourgal <git@fairever.aleeas.com>
2025-02-07 10:24:52 +0800

    chore: build geminicommit from source

--- packages/geminicommit/default.nix
+++ packages/geminicommit/default.nix
@@ -1,46 +1,24 @@
 {
   lib,
-  stdenv,
-  fetchzip,
-  writeShellScriptBin,
-  curl,
-  jq,
-  gnused,
+  buildGoModule,
   _sources,
 }:
-let
-  hash = "g5UnX6exJXC1+mlJsdbyNt4gleNp4x9R54bFsRSdgww=";
-in
-stdenv.mkDerivation rec {
-  inherit (_sources.geminicommit) pname version;
 
-  src = fetchzip {
-    url = "https://github.com/tfkhdyt/geminicommit/releases/download/v${version}/geminicommit-v${version}-linux-amd64.tar.gz";
-    sha256 = hash;
-    stripRoot = false;
-  };
+buildGoModule {
+  inherit (_sources.geminicommit) pname version src;
 
-  dontBuild = true;
+  vendorHash = "sha256-+eKJLXgKuUHelUjD8MpMa+cRP+clmYK+1olcb/jmabk=";
 
-  installPhase = ''
-    runHook preInstall
-    install -Dm755 geminicommit -t $out/bin
-    runHook postInstall
-  '';
+  ldflags = [
+    "-s"
+    "-w"
+  ];
 
-  passthru.update = writeShellScriptBin "update-package" ''
-    set -euo pipefail
-
-    latest="$(${curl}/bin/curl -s "https://api.github.com/repos/tfkhdyt/geminicommit/releases?per_page=1" | ${jq}/bin/jq -r ".[0].tag_name" | ${gnused}/bin/sed 's/^v//')"
-
-    drift rewrite --auto-hash --new-version "$latest"
-  '';
-
-  meta = with lib; {
-    description = "A CLI that writes git commit messages for you with Google Gemini AI";
+  meta = {
+    description = "CLI that writes git commit messages for you with Google Gemini AI";
     homepage = "https://github.com/tfkhdyt/geminicommit";
-    license = licenses.gpl3Only;
-    maintainers = with maintainers; [ zxc ];
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ zxc ];
     mainProgram = "geminicommit";
   };
 }
