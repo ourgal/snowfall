e7fba799344d3c9bf8466a808084a5b39ffcb0f1
ourgal <git@fairever.aleeas.com>
2025-02-07 13:28:11 +0800

    chore: build rustywatch from source

--- packages/rustywatch/default.nix
+++ packages/rustywatch/default.nix
@@ -1,46 +1,26 @@
 {
   lib,
+  rustPlatform,
   stdenv,
-  fetchzip,
-  writeShellScriptBin,
-  curl,
-  jq,
-  gnused,
+  darwin,
+  namespace,
   _sources,
 }:
-let
-  hash = "4ZqsjzyZTv6gNIpLGkWS5i+WBFrnrOzvFSWTwuGrwRA=";
-in
-stdenv.mkDerivation rec {
-  inherit (_sources.rustywatch) pname version;
 
-  src = fetchzip {
-    url = "https://github.com/ak9024/rustywatch/releases/download/v${version}/rustywatch-${version}-x86_64-unknown-linux-gnu.tar.gz";
-    sha256 = hash;
-  };
+rustPlatform.buildRustPackage (
+  lib.${namespace}.mkRustSource _sources.rustywatch
+  // {
+    buildInputs = lib.optionals stdenv.isDarwin [
+      darwin.apple_sdk.frameworks.CoreFoundation
+      darwin.apple_sdk.frameworks.CoreServices
+    ];
 
-  dontBuild = true;
-
-  installPhase = ''
-    runHook preInstall
-    install -Dm755 rustywatch -t $out/bin
-    runHook postInstall
-  '';
-
-  passthru.update = writeShellScriptBin "update-package" ''
-    set -euo pipefail
-
-    latest="$(${curl}/bin/curl -s "https://api.github.com/repos/ak9024/rustywatch/releases?per_page=1" | ${jq}/bin/jq -r ".[0].tag_name" | ${gnused}/bin/sed 's/^v//')"
-
-    drift rewrite --auto-hash --new-version "$latest"
-  '';
-
-  meta = with lib; {
-    description = "Live Reloading Built with Rust";
-    homepage = "https://github.com/ak9024/rustywatch";
-    changelog = "https://github.com/ak9024/rustywatch/blob/v${version}/CHANGELOG.md";
-    license = licenses.mit;
-    maintainers = with maintainers; [ zxc ];
-    mainProgram = "rustywatch";
-  };
-}
+    meta = {
+      description = "Live Reloading Built with Rust";
+      homepage = "https://github.com/ak9024/rustywatch";
+      license = lib.licenses.mit;
+      maintainers = with lib.maintainers; [ zxc ];
+      mainProgram = "rustywatch";
+    };
+  }
+)
