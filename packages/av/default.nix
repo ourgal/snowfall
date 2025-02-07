6374c96af93cc518c5f7907b1dde7710eddb69b7
ourgal <git@fairever.aleeas.com>
2025-02-07 12:08:47 +0800

    chore: build av from source

--- packages/av/default.nix
+++ packages/av/default.nix
@@ -1,50 +1,24 @@
 {
   lib,
-  stdenv,
-  fetchzip,
-  installShellFiles,
-  writeShellScriptBin,
-  curl,
-  jq,
-  gnused,
+  buildGoModule,
   _sources,
+  git,
 }:
-let
-  hash = "wb4SsOKjSBuykE32l0WUTEOyR97neZUptC5nqe9YIYk=";
-in
-stdenv.mkDerivation rec {
-  inherit (_sources.av) pname version;
 
-  src = fetchzip {
-    url = "https://github.com/aviator-co/av/releases/download/v${version}/av_${version}_linux_arm64.tar.gz";
-    sha256 = hash;
-    stripRoot = false;
-  };
+buildGoModule rec {
+  inherit (_sources.av) pname version src;
 
-  nativeBuildInputs = [ installShellFiles ];
+  vendorHash = "sha256-j/f/j8cYJi1ESmjHQpy+ilpy5Y++n9aNwpe1b02Eusw=";
 
-  dontBuild = true;
+  ldflags = [ "-X=github.com/aviator-co/av/internal/config.Version=v${version}" ];
 
-  installPhase = ''
-    runHook preInstall
-    install -Dm755 av -t $out/bin
-    installManPage man/*/*
-    runHook postInstall
-  '';
+  nativeBuildInputs = [ git ];
 
-  passthru.update = writeShellScriptBin "update-package" ''
-    set -euo pipefail
-
-    latest="$(${curl}/bin/curl -s "https://api.github.com/repos/aviator-co/av/releases?per_page=1" | ${jq}/bin/jq -r ".[0].tag_name" | ${gnused}/bin/sed 's/^v//')"
-
-    drift rewrite --auto-hash --new-version "$latest"
-  '';
-
-  meta = with lib; {
+  meta = {
     description = "A command line tool to manage stacked PRs with Aviator";
     homepage = "https://github.com/aviator-co/av";
-    license = licenses.mit;
-    maintainers = with maintainers; [ zxc ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ zxc ];
     mainProgram = "av";
   };
 }
