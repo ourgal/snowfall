7fdcff7a5e3d8b3f0b12ef2d13b4585541fdfd6b
ourgal <git@fairever.aleeas.com>
2025-02-08 13:50:25 +0800

    feat: add ccsum

---
+++ packages/ccsum/default.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  rustPlatform,
+  namespace,
+  _sources,
+}:
+
+rustPlatform.buildRustPackage (
+  lib.${namespace}.mkRustSource _sources.ccsum
+  // {
+    meta = {
+      description = "Convenient Checksum";
+      homepage = "https://github.com/sevenc-nanashi/ccsum";
+      license = lib.licenses.mit;
+      maintainers = with lib.maintainers; [ zxc ];
+      mainProgram = "ccsum";
+    };
+  }
+)
