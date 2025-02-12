bc2a24d7bd2b9c62fad6adaadcadc43f8a40caa0
ourgal <git@fairever.aleeas.com>
2025-02-12 17:23:16 +0800

    feat: add rx

---
+++ packages/rx/default.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  buildGoModule,
+  _sources,
+}:
+
+buildGoModule {
+  inherit (_sources.rx) pname version src;
+
+  vendorHash = "sha256-cPBx5+30G41aGRU4bjk70ZtRqU5bVYaK9ob/eyljp1g=";
+
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  meta = {
+    description = "Regex eXplorer, allows you to test your regexes with live-reload";
+    homepage = "https://github.com/f01c33/rx";
+    license = lib.licenses.unfree; # FIXME: nix-init did not find a license
+    maintainers = with lib.maintainers; [ zxc ];
+    mainProgram = "rx";
+  };
+}
