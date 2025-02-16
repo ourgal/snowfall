cf56a45b749093dbccb78f32046161cf0478768e
ourgal <git@fairever.aleeas.com>
2025-02-16 18:30:02 +0800

    chore: update taskwarrior hooks

---
+++ modules/home/cli/task/taskwarrior/relative-recur/default.nix
@@ -0,0 +1,19 @@
+args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) pkgs;
+      relative-recur = pkgs._sources.task-relative-recur.src;
+    in
+    {
+      path = ./.;
+      value = {
+        home.file = {
+          ".local/share/task/hooks/on-modify.relative-recur".source =
+            relative-recur + "/on-modify.relative-recur";
+        };
+      };
+    }
+  )
+)
