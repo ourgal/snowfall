4b7decaeecc04c59b765c7d362f5c32d0c20e247
ourgal <git@fairever.aleeas.com>
2025-02-22 21:33:53 +0800

    chore: change doom emacs dirvish keymap

--- modules/home/desktop/editor/doom-emacs/config/plugins/dirvish.el
+++ modules/home/desktop/editor/doom-emacs/config/plugins/dirvish.el
@@ -1,5 +1,5 @@
 ;;; plugins/dirvish.el -*- lexical-binding: t; -*-
 
 (after! dirvish
-	(dirvish-side-follow-mode))
-(map! :n "SPC e" #'dirvish)
+  (dirvish-side-follow-mode))
+(map! :n "-" #'dirvish)
