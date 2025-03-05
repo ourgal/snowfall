1432864a90396262b45cd62b4b371b5649ddad97
ourgal <94423745+ourgal@users.noreply.github.com>
2025-03-05 16:39:36 +0000

    chore: auto update nix hash

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -3158,18 +3158,18 @@
   };
   v2ray-rules-dat-geoip = {
     pname = "v2ray-rules-dat-geoip";
-    version = "202503032212";
+    version = "202503042212";
     src = fetchurl {
-      url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/202503032212/geoip.dat";
+      url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/202503042212/geoip.dat";
       sha256 = "sha256-MAe2RwZqHHm63MnMYvOKbsNSLTnfwpl6cvH6d1gyzHU=";
     };
   };
   v2ray-rules-dat-geosite = {
     pname = "v2ray-rules-dat-geosite";
-    version = "202503032212";
+    version = "202503042212";
     src = fetchurl {
-      url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/202503032212/geosite.dat";
-      sha256 = "sha256-TeIOuk8XyC4n/eq9zTT0wVz48FtokaWl7ErKcnQd0AI=";
+      url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/202503042212/geosite.dat";
+      sha256 = "sha256-ixOBBEw4j8wp9CioFLy3Rw2EN27odm1Cyp3+sNbVLf8=";
     };
   };
   vim-auto-save = {
