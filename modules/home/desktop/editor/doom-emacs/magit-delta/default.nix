527364b02d77e931e1912253844b93c2240abdaf
ourgal <git@fairever.aleeas.com>
2025-03-07 16:24:22 +0800

    chore: build emacs-magit-delta from source

--- modules/home/desktop/editor/doom-emacs/magit-delta/default.nix
+++ modules/home/desktop/editor/doom-emacs/magit-delta/default.nix
@@ -3,6 +3,15 @@ args.module (
   args
   // {
     path = ./.;
-    progs.doom-emacs.extraPackages = epkgs: [ epkgs.magit-delta ];
+    progs.doom-emacs.extraPackages = epkgs: [
+      (epkgs.melpaBuild {
+        inherit (args.pkgs._sources.emacs-magit-delta) pname version src;
+        packageRequires = with epkgs; [
+          dash
+          magit
+          xterm-color
+        ];
+      })
+    ];
   }
 )
