26db66b237e84eac2a8d85965d98ac722ed79e60
ourgal <git@fairever.aleeas.com>
2025-03-08 10:42:13 +0800

    chore: disable floorp librewolf and lem

--- modules/home/cli/editor/default.nix
+++ modules/home/cli/editor/default.nix
@@ -1,31 +1,25 @@
 args:
 args.module (
   args
-  // (
-    let
-      inherit (args) pkgs;
-    in
-    {
-      path = ./.;
-      inputPkgs = p: [ p.lem.packages."${pkgs.system}".default ];
-      myPkgs = "v";
-      nixPkgs = [
-        # keep-sorted start
-        "micro"
-        "ox"
-        "vimer"
-        "vis"
-        # keep-sorted end
-      ];
-      enable = [
-        # keep-sorted start
-        "helix"
-        "kakoune"
-        "ki"
-        "nixvim"
-        "vim"
-        # keep-sorted end
-      ];
-    }
-  )
+  // {
+    path = ./.;
+    myPkgs = "v";
+    nixPkgs = [
+      # keep-sorted start
+      "micro"
+      "ox"
+      "vimer"
+      "vis"
+      # keep-sorted end
+    ];
+    enable = [
+      # keep-sorted start
+      "helix"
+      "kakoune"
+      "ki"
+      "nixvim"
+      "vim"
+      # keep-sorted end
+    ];
+  }
 )
