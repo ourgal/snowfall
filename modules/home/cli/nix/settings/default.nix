4d3273a4ae2d4383a96013e5e536722687c51bd3
ourgal <git@fairever.aleeas.com>
2025-03-10 10:58:09 +0800

    chore: change mirror to nju

--- modules/home/cli/nix/settings/default.nix
+++ modules/home/cli/nix/settings/default.nix
@@ -9,8 +9,10 @@ args.module (
         lib
         namespace
         inputs
+        cfgHome
         ;
-      inherit (lib.${namespace}) domains;
+      inherit (lib.${namespace}) domains mkOpt';
+      cfg = cfgHome config.${namespace} ./.;
     in
     {
       path = ./.;
@@ -26,7 +28,11 @@ args.module (
         xdg.configFile = {
           "nix/nix.conf".text =
             let
-              mirrorUrls = "https://mirrors.cernet.edu.cn/nix-channels/store";
+              mirrorUrls =
+                if cfg.mirror == "nju" then
+                  "https://mirror.nju.edu.cn/nix-channels/store"
+                else
+                  "https://mirrors.cernet.edu.cn/nix-channels/store";
               aaglUrls = "https://ezkea.cachix.org";
               aaglKeys = "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=";
             in
@@ -36,6 +42,12 @@ args.module (
               trusted-public-keys = cache.example.org-1:lFI4YUR1ZKE8dz1JoXTRBvIEHaeKmW3LHBlDTJDW1V8= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= ${aaglKeys}
             '';
         };
+      };
+      extraOpts = {
+        mirror = mkOpt' (lib.types.enum [
+          "cernet"
+          "nju"
+        ]) "nju";
       };
     }
   )
