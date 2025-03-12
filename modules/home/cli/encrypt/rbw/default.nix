92659a23b462f77851462e9430d0db8a85387263
ourgal <git@fairever.aleeas.com>
2025-03-13 00:48:31 +0800

    feat: add rofi-rbw

--- modules/home/cli/encrypt/rbw/default.nix
+++ modules/home/cli/encrypt/rbw/default.nix
@@ -1,22 +1,32 @@
 args:
 args.module (
   args
-  // {
-    path = ./.;
-    nixPkgs = "rbw";
-    confs = {
-      "rbw/config.json" = {
-        email = args.lib.strings.fileContents ./email.key;
-        sso_id = null;
-        base_url = args.lib.strings.fileContents ./base_url.key;
-        identity_url = null;
-        ui_url = null;
-        notifications_url = null;
-        lock_timeout = 3600;
-        sync_interval = 3600;
-        pinentry = "pinentry";
-        client_cert_path = null;
+  // (
+    let
+      inherit (args) lib;
+    in
+    {
+      path = ./.;
+      nixPkgs = [
+        # keep-sorted start
+        "rbw"
+        "rofi-rbw"
+        # keep-sorted end
+      ];
+      confs = {
+        "rbw/config.json" = {
+          email = lib.strings.fileContents ./email.key;
+          sso_id = null;
+          base_url = lib.strings.fileContents ./base_url.key;
+          identity_url = null;
+          ui_url = null;
+          notifications_url = null;
+          lock_timeout = 3600;
+          sync_interval = 3600;
+          pinentry = "pinentry";
+          client_cert_path = null;
+        };
       };
-    };
-  }
+    }
+  )
 )
