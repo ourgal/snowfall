d250b53cb1bfface3272f7188cc0ea4f3c9f84e5
ourgal <git@fairever.aleeas.com>
2025-03-12 23:14:36 +0800

    chore: add laptop module

--- systems/x86_64-linux/air/default.nix
+++ systems/x86_64-linux/air/default.nix
@@ -5,48 +5,17 @@
   ...
 }:
 let
-  inherit (lib.${namespace}) enabled disabled enabledList;
+  inherit (lib.${namespace}) enabled disabled settings;
   user = config.${namespace}.user.name;
 in
 {
-  dot =
-    {
-      user.host = "air";
-      boot.systemd = enabled;
-      gpu.amd = enabled;
-      firewall.disable = enabled;
-    }
-    // enabledList [
-      # keep-sorted start
-      "appimage"
-      "audio"
-      "bandwhich"
-      "bluetooth"
-      "common"
-      "desktop"
-      "disko"
-      "envfs"
-      "fonts"
-      "gpu-screen-recorder"
-      "harmonia"
-      "input"
-      "kmonad"
-      "laptop"
-      "location"
-      "logrotate"
-      "nfsMount"
-      "nh"
-      "pcscd"
-      "power"
-      "scrutiny"
-      "sops-nix"
-      "syncthing"
-      "tpm"
-      "tuptime"
-      "udev"
-      "udisks2"
-      # keep-sorted end
-    ];
+  dot = {
+    user.host = "air";
+    boot.systemd = enabled;
+    gpu.amd = enabled;
+    firewall.disable = enabled;
+    disko = enabled;
+  } // settings.laptopModule;
 
   snowfallorg.users.${user}.home = disabled;
 
