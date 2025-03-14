ac703c9b6f2112150ef8feaf0390498fc9a55f06
ourgal <git@fairever.aleeas.com>
2025-03-14 20:36:25 +0800

    fix: typo

--- systems/x86_64-linux/air/default.nix
+++ systems/x86_64-linux/air/default.nix
@@ -15,7 +15,7 @@ in
     gpu.amd = enabled;
     firewall.disable = enabled;
     disko = enabled;
-  } // settings.laptopModule;
+  } // settings.laptopModules;
 
   snowfallorg.users.${user}.home = disabled;
 
