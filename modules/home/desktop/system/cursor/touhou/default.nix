6d7c116b2e813a43cb31620b056d7a134a565dc2
ourgal <git@fairever.aleeas.com>
2025-03-16 00:49:18 +0800

    feat: add touhou-cursors

---
+++ modules/home/desktop/system/cursor/touhou/default.nix
@@ -0,0 +1,27 @@
+args:
+args.module (
+  args
+  // (
+    let
+      inherit (args)
+        enabled
+        pkgs
+        disabled
+        namespace
+        ;
+    in
+    {
+      path = ./.;
+      value = {
+        home.pointerCursor = {
+          gtk = enabled;
+          x11 = enabled;
+          package = pkgs.${namespace}.touhou-cursors;
+          name = "Reimu";
+          size = 24;
+        };
+        catppuccin.cursors = disabled;
+      };
+    }
+  )
+)
