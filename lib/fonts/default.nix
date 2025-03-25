63737c35196ad69ebad3d5105df325799bb84e0d
ourgal <git@fairever.aleeas.com>
2025-03-25 20:08:58 +0800

    chore: convert nix to fontconfig

--- lib/fonts/default.nix
+++ lib/fonts/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
   font = {
     defaults = [
@@ -23,4 +23,51 @@
       "Symbols Nerd Font"
     ];
   };
+  mkFontconfig =
+    header: json:
+    lib.attrsets.foldlAttrs (
+      acc: name: value:
+      let
+        go =
+          name: value:
+          let
+            v =
+              if (builtins.isList value) then
+                lib.pipe value [
+                  (builtins.map (x: go name x))
+                  (builtins.concatStringsSep "")
+                ]
+              else if (builtins.isAttrs value) then
+                let
+                  res =
+                    lib.attrsets.foldlAttrs
+                      (
+                        a: n: v:
+                        if (lib.strings.hasPrefix "@" n) then
+                          a // { attr = a.attr + " ${(lib.strings.removePrefix "@" n)}=\"${v}\""; }
+                        else if (n == "test") then
+                          a // { test = a.test + go n v; }
+                        else if (n == "edit") then
+                          a // { edit = a.edit + go n v; }
+                        else
+                          a // { other = a.other + go n v; }
+                      )
+                      {
+                        attr = "";
+                        test = "";
+                        edit = "";
+                        other = "";
+                      }
+                      value;
+                in
+                "<${name}${res.attr}>${res.test}${res.edit}${res.other}</${name}>"
+              else if (builtins.isString value) then
+                "<${name}>${value}</${name}>"
+              else
+                builtins.throw "not supported type";
+          in
+          v;
+      in
+      acc + go name value
+    ) header json;
 }
--- modules/home/fonts/default.nix
+++ modules/home/fonts/default.nix
@@ -4,135 +4,181 @@ args.module (
   // (
     let
       inherit (args) lib namespace;
-      inherit (lib.${namespace}) font;
+      inherit (lib.${namespace}) font mkFontconfig;
     in
     {
       path = ./.;
       nixPkgs = font.defaults;
-      confs."fontconfig/fonts.conf" =
-        let
-          item = list: builtins.concatStringsSep "\n" (builtins.map (x: "<string>${x}</string>") list);
-          emoji = item font.emoji;
-          cnSans = item [ font.cnSans ];
-          cnSerif = item [ font.cnSerif ];
-          enSans = item [ font.enSans ];
-          enSerif = item [ font.enSerif ];
-          mono = item [ font.mono ];
-        in
-        ''
-          <?xml version="1.0"?>
-          <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
-
-          <fontconfig>
-
-          <!-- Default system-ui fonts -->
-          <match target="pattern">
-            <test name="family">
-              <string>system-ui</string>
-            </test>
-            <edit name="family" mode="prepend" binding="strong">
-              <string>sans-serif</string>
-            </edit>
-          </match>
-
-          <!-- Default sans-serif fonts-->
-          <match target="pattern">
-            <test name="family">
-              <string>sans-serif</string>
-            </test>
-            <edit name="family" mode="prepend" binding="strong">
-              ${cnSans}
-              ${enSans}
-              ${emoji}
-            </edit>
-          </match>
-
-          <!-- Default serif fonts-->
-          <match target="pattern">
-            <test name="family">
-              <string>serif</string>
-            </test>
-            <edit name="family" mode="prepend" binding="strong">
-              ${cnSerif}
-              ${enSerif}
-              ${emoji}
-            </edit>
-          </match>
-
-          <!-- Default monospace fonts-->
-          <match target="pattern">
-            <test name="family">
-              <string>monospace</string>
-            </test>
-            <edit name="family" mode="prepend" binding="strong">
-              ${mono}
-              ${cnSans}
-              ${emoji}
-            </edit>
-          </match>
-
-          <match target="pattern">
-            <test name="lang">
-              <string>zh-HK</string>
-            </test>
-            <test name="family">
-              ${cnSans}
-            </test>
-            <edit name="family" binding="strong">
-              <string>Source Han Sans HC</string>
-            </edit>
-          </match>
-
-          <match target="pattern">
-            <test name="lang">
-              <string>zh-TW</string>
-            </test>
-            <test name="family">
-              ${cnSans}
-            </test>
-            <edit name="family" binding="strong">
-              <string>Source Han Sans TC</string>
-            </edit>
-          </match>
-
-          <match target="pattern">
-            <test name="lang">
-              <string>ja</string>
-            </test>
-            <test name="family">
-              ${cnSans}
-            </test>
-            <edit name="family" binding="strong">
-              <string>Source Han Sans HW</string>
-            </edit>
-          </match>
-
-          <match target="pattern">
-            <test name="lang">
-              <string>ko</string>
-            </test>
-            <test name="family">
-              ${cnSans}
-            </test>
-            <edit name="family" binding="strong">
-              <string>Source Han Sans K</string>
-            </edit>
-          </match>
-
-          <match target="pattern">
-            <test name="lang" compare="contains">
-              <string>en</string>
-            </test>
-            <test name="family" compare="contains">
-              <string>Source Han</string>
-            </test>
-            <edit name="family" mode="prepend" binding="strong">
-              ${enSans}
-            </edit>
-          </match>
-
-          </fontconfig>
-        '';
+      confs = {
+        "fontconfig/fonts.conf" =
+          let
+            header = ''
+              <?xml version="1.0"?>
+              <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+            '';
+            settings = {
+              fontconfig = {
+                match = [
+                  {
+                    "@target" = "pattern";
+                    test = {
+                      "@name" = "family";
+                      string = "system-ui";
+                    };
+                    edit = {
+                      "@name" = "family";
+                      "@mode" = "prepend";
+                      "@binding" = "strong";
+                      string = "sans-serif";
+                    };
+                  }
+                  {
+                    "@target" = "pattern";
+                    test = {
+                      "@name" = "family";
+                      string = "sans-serif";
+                    };
+                    edit = {
+                      "@name" = "family";
+                      "@mode" = "prepend";
+                      "@binding" = "strong";
+                      string = [
+                        font.cnSans
+                        font.enSans
+                      ] ++ font.emoji;
+                    };
+                  }
+                  {
+                    "@target" = "pattern";
+                    test = {
+                      "@name" = "family";
+                      string = "serif";
+                    };
+                    edit = {
+                      "@name" = "family";
+                      "@mode" = "prepend";
+                      "@binding" = "strong";
+                      string = [
+                        font.cnSerif
+                        font.enSerif
+                      ] ++ font.emoji;
+                    };
+                  }
+                  {
+                    "@target" = "pattern";
+                    test = {
+                      "@name" = "family";
+                      string = "monospace";
+                    };
+                    edit = {
+                      "@name" = "family";
+                      "@mode" = "prepend";
+                      "@binding" = "strong";
+                      string = [
+                        font.mono
+                        font.cnSans
+                      ] ++ font.emoji;
+                    };
+                  }
+                  {
+                    "@target" = "pattern";
+                    test = [
+                      {
+                        "@name" = "lang";
+                        string = "zh-HK";
+                      }
+                      {
+                        "@name" = "family";
+                        string = font.cnSans;
+                      }
+                    ];
+                    edit = {
+                      "@name" = "family";
+                      "@binding" = "strong";
+                      string = "Source Han Sans HC";
+                    };
+                  }
+                  {
+                    "@target" = "pattern";
+                    test = [
+                      {
+                        "@name" = "lang";
+                        string = "zh-TW";
+                      }
+                      {
+                        "@name" = "family";
+                        string = font.cnSans;
+                      }
+                    ];
+                    edit = {
+                      "@name" = "family";
+                      "@binding" = "strong";
+                      string = "Source Han Sans TC";
+                    };
+                  }
+                  {
+                    "@target" = "pattern";
+                    test = [
+                      {
+                        "@name" = "lang";
+                        string = "ja";
+                      }
+                      {
+                        "@name" = "family";
+                        string = font.cnSans;
+                      }
+                    ];
+                    edit = {
+                      "@name" = "family";
+                      "@binding" = "strong";
+                      string = "Source Han Sans HW";
+                    };
+                  }
+                  {
+                    "@target" = "pattern";
+                    test = [
+                      {
+                        "@name" = "lang";
+                        string = "ko";
+                      }
+                      {
+                        "@name" = "family";
+                        string = font.cnSans;
+                      }
+                    ];
+                    edit = {
+                      "@name" = "family";
+                      "@binding" = "strong";
+                      string = "Source Han Sans K";
+                    };
+                  }
+                  {
+                    "@target" = "pattern";
+                    test = [
+                      {
+                        "@name" = "lang";
+                        "@compare" = "contains";
+                        string = "en";
+                      }
+                      {
+                        "@name" = "family";
+                        "@compare" = "contains";
+                        string = "Source Han";
+                      }
+                    ];
+                    edit = {
+                      "@name" = "family";
+                      "@mode" = "prepend";
+                      "@binding" = "strong";
+                      string = font.enSans;
+                    };
+                  }
+                ];
+              };
+            };
+          in
+          mkFontconfig header settings;
+      };
     }
   )
 )
