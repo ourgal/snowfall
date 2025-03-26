28f5bdd90215397eb46a181d76cd4882a2dc62f0
ourgal <git@fairever.aleeas.com>
2025-03-26 13:54:19 +0800

    chore: build cmdx from source

--- packages/cmdx/default.nix
+++ packages/cmdx/default.nix
@@ -1,46 +1,24 @@
 {
   lib,
-  stdenv,
-  fetchzip,
-  writeShellScriptBin,
-  curl,
-  jq,
-  gnused,
+  buildGoModule,
   _sources',
 }:
-let
-  hash = "sha256-9/uj8W6dPL7ZolP5x0mXFqXuqNLcIUtf/J2rcoWhltU=";
-in
-stdenv.mkDerivation rec {
-  inherit (_sources' ./.) pname version;
 
-  src = fetchzip {
-    url = "https://github.com/suzuki-shunsuke/cmdx/releases/download/v${version}/cmdx_linux_amd64.tar.gz";
-    sha256 = hash;
-    stripRoot = false;
-  };
+buildGoModule {
+  inherit (_sources' ./.) pname version src;
 
-  dontBuild = true;
+  vendorHash = "sha256-njsZKv2++rRA1jtrAaYXcxRwkfpaRnUmiPYga9T90d4=";
 
-  installPhase = ''
-    runHook preInstall
-    install -Dm755 cmdx -t $out/bin
-    runHook postInstall
-  '';
+  ldflags = [
+    "-s"
+    "-w"
+  ];
 
-  passthru.update = writeShellScriptBin "update-package" ''
-    set -euo pipefail
-
-    latest="$(${curl}/bin/curl -s "https://api.github.com/repos/suzuki-shunsuke/cmdx/releases?per_page=1" | ${jq}/bin/jq -r ".[0].tag_name" | ${gnused}/bin/sed 's/^v//')"
-
-    drift rewrite --auto-hash --new-version "$latest"
-  '';
-
-  meta = with lib; {
+  meta = {
     description = "Task runner. It provides useful help messages and supports interactive prompts and validation of arguments";
     homepage = "https://github.com/suzuki-shunsuke/cmdx";
-    license = licenses.mit;
-    maintainers = with maintainers; [ zxc ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ zxc ];
     mainProgram = "cmdx";
   };
 }
