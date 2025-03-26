fe0626e475b08268c927babdcb83c503bc9645c0
ourgal <git@fairever.aleeas.com>
2025-03-26 15:52:38 +0800

    chore: change boilr src

--- packages/boilr/default.nix
+++ packages/boilr/default.nix
@@ -1,39 +1,20 @@
 {
   lib,
   stdenv,
-  fetchzip,
-  writeShellScriptBin,
-  curl,
-  jq,
-  gnused,
   _sources',
 }:
-let
-  hash = "HHqQnUXkaDOjWZ2AwZnndGKl/ZuJxzz5N2mVeL5KKPg=";
-in
-stdenv.mkDerivation rec {
-  inherit (_sources' ./.) pname version;
+stdenv.mkDerivation {
+  inherit (_sources' ./.) pname version src;
 
-  src = fetchzip {
-    url = "https://github.com/tmrts/boilr/releases/download/${version}/boilr-${version}-linux_amd64.tgz";
-    sha256 = hash;
-    stripRoot = false;
-  };
-
-  dontBuild = true;
+  unpackCmd = ''
+    mkdir temp
+    tar -xf $src --directory=temp
+  '';
 
   installPhase = ''
     runHook preInstall
     install -Dm755 boilr -t $out/bin
     runHook postInstall
-  '';
-
-  passthru.update = writeShellScriptBin "update-package" ''
-    set -euo pipefail
-
-    latest="$(${curl}/bin/curl -s "https://api.github.com/repos/tmrts/boilr/releases?per_page=1" | ${jq}/bin/jq -r ".[0].tag_name" | ${gnused}/bin/sed 's/^v//')"
-
-    drift rewrite --auto-hash --new-version "$latest"
   '';
 
   meta = with lib; {
@@ -42,6 +23,6 @@ stdenv.mkDerivation rec {
     license = licenses.asl20;
     maintainers = with maintainers; [ zxc ];
     mainProgram = "boilr";
-    platforms = platforms.all;
+    platforms = platforms.x86_64-linux;
   };
 }
