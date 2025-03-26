cfe3c570284c803c6f402c318797d8ce048bec36
ourgal <git@fairever.aleeas.com>
2025-03-26 14:38:38 +0800

    chore: unpack gif-progress src

--- packages/gif-progress/default.nix
+++ packages/gif-progress/default.nix
@@ -2,15 +2,22 @@
   lib,
   stdenv,
   _sources',
+  p7zip,
 }:
 stdenv.mkDerivation {
   inherit (_sources' ./.) pname version src;
+
+  unpackCmd = ''
+    7z x $src
+  '';
 
   installPhase = ''
     runHook preInstall
     install -Dm755 gif-progress -t $out/bin
     runHook postInstall
   '';
+
+  nativeBuildInputs = [ p7zip ];
 
   meta = with lib; {
     description = "Attach progress bar to animated GIF";
