2c83cfaa1a23faa12798f23a3e8adb75e2f62aca
ourgal <git@fairever.aleeas.com>
2025-03-26 15:13:54 +0800

    chore: change sequin src

--- packages/sequin/default.nix
+++ packages/sequin/default.nix
@@ -1,38 +1,15 @@
 {
   lib,
   stdenv,
-  fetchzip,
-  writeShellScriptBin,
-  curl,
-  jq,
-  gnused,
   _sources',
 }:
-let
-  hash = "sha256-exVp3hrnCR6p2mlmzGBL8IWcY3W7kXnd4uriwNVFtwY=";
-in
-stdenv.mkDerivation rec {
-  inherit (_sources' ./.) pname version;
-
-  src = fetchzip {
-    url = "https://github.com/charmbracelet/sequin/releases/download/v${version}/sequin_${version}_Linux_x86_64.tar.gz";
-    sha256 = hash;
-  };
-
-  dontBuild = true;
+stdenv.mkDerivation {
+  inherit (_sources' ./.) pname version src;
 
   installPhase = ''
     runHook preInstall
     install -Dm755 sequin -t $out/bin
     runHook postInstall
-  '';
-
-  passthru.update = writeShellScriptBin "update-package" ''
-    set -euo pipefail
-
-    latest="$(${curl}/bin/curl -s "https://api.github.com/repos/charmbracelet/sequin/releases?per_page=1" | ${jq}/bin/jq -r ".[0].tag_name" | ${gnused}/bin/sed 's/^v//')"
-
-    drift rewrite --auto-hash --new-version "$latest"
   '';
 
   meta = with lib; {
