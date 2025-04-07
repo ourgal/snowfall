f68624e6bc289d027543c23f46f7a251d674b824
ourgal <git@fairever.aleeas.com>
2025-04-07 10:50:50 +0800

    chore: add podman backend to docker

--- modules/nixos/docker/default.nix
+++ modules/nixos/docker/default.nix
@@ -6,8 +6,11 @@ let
     config
     pkgs
     ;
-  inherit (lib.${namespace}) nixosModule enabled;
+  inherit (lib.${namespace}) nixosModule enabled disabled;
+  inherit (builtins) throw;
   user = config.${namespace}.user.name;
+  isDocker = config.virtualisation.docker.enable;
+  isPodman = config.virtualisation.podman.enable;
   value = {
     virtualisation = {
       docker = enabled // {
@@ -15,15 +18,34 @@ let
           dates = "weekly";
         };
       };
-      oci-containers.backend = "docker";
+      podman = disabled // {
+        dockerCompat = true;
+        dockerSocket = enabled;
+        autoPrune = enabled // {
+          dates = "weekly";
+        };
+      };
+      oci-containers.backend =
+        if isDocker then
+          "docker"
+        else if isPodman then
+          "podman"
+        else
+          throw "unknow oci-containers backend";
       arion = {
         backend = "docker";
         package = pkgs.arion;
       };
     };
-    users.users.${user}.extraGroups = [ "docker" ];
+    users.users.${user}.extraGroups =
+      if isDocker then
+        [ "docker" ]
+      else if isPodman then
+        [ "podman" ]
+      else
+        throw "unknow containers backend";
 
-    environment.systemPackages = with pkgs; [ docker-compose ];
+    environment.systemPackages = lib.optional isDocker pkgs.docker-compose;
   };
   path = ./.;
   _args = { inherit value path args; };
