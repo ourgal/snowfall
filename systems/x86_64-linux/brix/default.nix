ae017b018fc6ef87d63f56468c312b3ecd04891e
ourgal <git@fairever.aleeas.com>
2025-04-10 00:48:46 +0800

    chore: sort brix modules list

--- systems/x86_64-linux/brix/default.nix
+++ systems/x86_64-linux/brix/default.nix
@@ -5,77 +5,87 @@
   ...
 }:
 let
-  inherit (lib.${namespace}) enabled disabled ip;
+  inherit (lib.${namespace})
+    enabled
+    disabled
+    ip
+    enabledList
+    ;
   user = config.${namespace}.user.name;
 in
 {
   imports = [ ./hardware-configuration.nix ];
 
-  dot = {
-    user.host = "brix";
-    boot.systemd = enabled;
-    common = enabled;
-    nh = enabled;
-    sops-nix = enabled // {
-      ssh = enabled;
-      atticd = enabled;
-    };
-    nfs = enabled;
-    caddy = enabled;
-    logrotate = enabled;
-    soft-serve = enabled;
-    atuin = enabled;
-    syncthing = enabled;
-    ariang = enabled;
-    dufs = enabled;
-    pairdrop = enabled;
-    rustypaste = enabled;
-    miniflux = enabled;
-    vaultwarden = enabled;
-    anki-sync-server = enabled;
-    firewall.disable = enabled;
-    firehol = enabled;
-    tuptime = enabled;
-    flaresolverr = enabled;
-    vsftpd = enabled;
-    transmission = enabled;
-    navidrome = enabled;
-    jellyfin = enabled;
-    sonarr = enabled;
-    metacubexd = enabled;
-    scrutiny = enabled;
-    jackett = enabled;
-    zashboard = enabled;
-    bpftune = enabled;
-    immich = enabled;
-    postgres-update = disabled;
-    yacd = enabled;
-    komga = enabled;
-    postgresqlBackup = enabled;
-    memos = enabled;
-    atticd = enabled // {
-      port = 5320;
-    };
-    docker =
-      let
-        nfs = if config.services.resolved.enable then "brix.local" else ip.brix;
-      in
-      enabled
-      // {
-        subconverter = enabled // {
-          inherit nfs;
-        };
-        alist = enabled // {
-          inherit nfs;
-          duckdns = enabled;
-        };
-        qd = enabled // {
-          ports = 8923;
-          inherit nfs;
-        };
+  dot =
+    {
+      user.host = "brix";
+      boot.systemd = enabled;
+      sops-nix = enabled // {
+        ssh = enabled;
+        atticd = enabled;
       };
-    podman = disabled // { };
-  };
+      postgres-update = disabled;
+      firewall.disable = enabled;
+      atticd = enabled // {
+        port = 5320;
+      };
+      docker =
+        let
+          nfs = if config.services.resolved.enable then "brix.local" else ip.brix;
+        in
+        enabled
+        // {
+          subconverter = enabled // {
+            inherit nfs;
+          };
+          alist = enabled // {
+            inherit nfs;
+            duckdns = enabled;
+          };
+          qd = enabled // {
+            ports = 8923;
+            inherit nfs;
+          };
+        };
+      podman = disabled // { };
+    }
+    // enabledList [
+      # keep-sorted start
+      "anki-sync-server"
+      "ariang"
+      "atuin"
+      "bpftune"
+      "caddy"
+      "common"
+      "dufs"
+      "firehol"
+      "flaresolverr"
+      "immich"
+      "jackett"
+      "jellyfin"
+      "komga"
+      "logrotate"
+      "memos"
+      "metacubexd"
+      "miniflux"
+      "navidrome"
+      "nfs"
+      "nh"
+      "pairdrop"
+      "postgresqlBackup"
+      "rustypaste"
+      "scrutiny"
+      "soft-serve"
+      "sonarr"
+      "syncthing"
+      "transmission"
+      "tuptime"
+      "vaultwarden"
+      "vsftpd"
+      "yacd"
+      "zashboard"
+      # keep-sorted end
+    ];
 
   snowfallorg.users.${user}.home = disabled;
 
