4a98cb1ab9ea1dc12eb6c3005700538fcc5a17a5
ourgal <git@fairever.aleeas.com>
2025-04-21 01:36:15 +0800

    chore: add smartcd.fish package

---
+++ packages/smartcd.fish/default.nix
@@ -0,0 +1,17 @@
+{
+  lib,
+  fishPlugins,
+  _sources',
+}:
+
+fishPlugins.buildFishPlugin {
+  inherit (_sources' ./.) pname version src;
+
+  meta = {
+    description = "Mirrored from https://git.sr.ht/~ficd/smartcd.fish";
+    homepage = "https://github.com/ficcdaf/smartcd.fish";
+    license = lib.licenses.isc;
+    maintainers = with lib.maintainers; [ zxc ];
+    platforms = lib.platforms.all;
+  };
+}
