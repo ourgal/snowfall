7a8d3de331be0b336b997698f1ce590fc1a880a4
ourgal <git@fairever.aleeas.com>
2025-05-07 10:50:09 +0800

    chore: remove goldendict dict

--- modules/home/desktop/document/goldendict-ng/default.nix
+++ modules/home/desktop/document/goldendict-ng/default.nix
@@ -18,11 +18,6 @@ args.module (
           url = "https://github.com/Ceelog/DictionaryByGPT4/raw/refs/heads/main/%E5%A8%81%E5%A8%81%E7%9A%84GPT%E5%8D%95%E8%AF%8D%E6%9C%AC(8000%E8%AF%8D).mdx";
           hash = "sha256-705VBBTJkQU6iVLfsghcSBvSls4JldwT2JlKbsAyPc8=";
         };
-        "goldendict/dicts/webster" = pkgs.fetchzip {
-          name = "webster-dict";
-          url = "https://mdx.mdict.org/Recommend/%E9%9F%A6%E6%B0%8F%E5%8F%8C%E8%A7%A3V3.zip";
-          hash = "sha256-VTNNZdUtUUR029N7mbyoQ25GZfXwQNP6iS2Z9e2I7+E=";
-        };
       };
     }
   )
