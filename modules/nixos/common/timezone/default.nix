04667b9fcdcee915bf146a172597fe5578c42560
ourgal <git@fairever.aleeas.com>
2025-05-07 22:19:18 +0800

    chore: add ntp servers

--- modules/nixos/common/timezone/default.nix
+++ modules/nixos/common/timezone/default.nix
@@ -1,10 +1,21 @@
 args:
 let
-  inherit (args) namespace lib;
+  inherit (args) namespace lib config;
   inherit (lib.${namespace}) nixosModule;
   value = {
     time.timeZone = "Asia/Taipei";
-    networking.timeServers = [ "pool.ntp.org" ];
+    networking.timeServers = [
+      "ntp.aliyun.com" # Aliyun
+      "ntp.tencent.com" # Tencent Cloud
+      "time.izatcloud.net" # Qualcomm NTP Server
+      "time.gpsonextra.net"
+      "hik-time.ys7.com" # hikvision
+      "time.ys7.com"
+      "cn.pool.ntp.org" # Pool NTP ORG
+      "cn.ntp.org.cn" # NTP ORG CN
+      "ntp.ntsc.ac.cn" # NTSC NTP Server
+    ];
+    services.timesyncd.servers = config.networking.timeServers;
   };
   _args = { inherit value args; };
 in
