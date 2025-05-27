ce06443d776ff7daebb06843ed4a2bed4bbd0d73
ourgal <git@fairever.aleeas.com>
2025-05-27 20:08:39 +0800

    chore: add fish-lsp config for doomemacs

--- modules/home/desktop/editor/doom-emacs/config/plugins/eglot.el
+++ modules/home/desktop/editor/doom-emacs/config/plugins/eglot.el
@@ -4,4 +4,7 @@
   (add-to-list 'eglot-server-programs
                '(fennel-mode . ("fennel-ls")))
   (add-hook 'fennel-mode-hook 'eglot-ensure)
+  (add-to-list 'eglot-server-programs
+               '(fish-mode . ("fish-lsp" "start")))
+  (add-hook 'fish-mode-hook 'eglot-ensure)
   (setq lua-lsp-dir (expand-file-name "~/.local/share/lua-language-server/")))
