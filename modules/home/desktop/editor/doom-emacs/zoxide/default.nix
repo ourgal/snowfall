5669573a65e29a1b9def6b959a71948351515523
ourgal <git@fairever.aleeas.com>
2025-05-29 21:54:31 +0800

    chore: replace emacs-zoxide with the nixpkgs's one

--- modules/home/desktop/editor/doom-emacs/zoxide/default.nix
+++ modules/home/desktop/editor/doom-emacs/zoxide/default.nix
@@ -1,12 +1,1 @@
-args:
-args.module (
-  args
-  // (
-    let
-      inherit (args) pkgs namespace;
-    in
-    {
-      progs.doom-emacs.extraPackages = _: [ pkgs.${namespace}.emacs-zoxide ];
-    }
-  )
-)
+{ progs.doom-emacs.extraPackages = epkgs: [ epkgs.zoxide ]; }
