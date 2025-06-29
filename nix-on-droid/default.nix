b90323048d29da669bcf690888aa7f29e975edbc
ourgal <git@fairever.aleeas.com>
2025-06-29 09:30:04 +0800

    chore: add mirrors to nix-on-droid

--- nix-on-droid/default.nix
+++ nix-on-droid/default.nix
@@ -32,9 +32,20 @@ in
 
   system.stateVersion = "24.05";
 
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-  '';
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+    settings = {
+      substituters = [
+        "http://192.168.123.100:50000?priority=9"
+        "https://mirrors.cernet.edu.cn/nix-channels/store?priority=10"
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org?priority=100"
+      ];
+      trusted-public-keys = [ "cache.example.org-1:lFI4YUR1ZKE8dz1JoXTRBvIEHaeKmW3LHBlDTJDW1V8=" ];
+    };
+  };
 
   home-manager = {
     config = ./home;
