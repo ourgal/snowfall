f83a283256c69e2a14eed1c1cb46df9c160760b7
ourgal <git@fairever.aleeas.com>
2025-07-12 21:43:32 +0800

    chore: 🧹 enable keyboard only for home host

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -1,23 +1,33 @@
-{
-  enable = [
-    # keep-sorted start
-    "anime"
-    "audio"
-    "browser"
-    "chat"
-    "document"
-    "flashcard"
-    "git"
-    "image"
-    "keyboard"
-    "math"
-    "search"
-    "system"
-    "terminal"
-    "translate"
-    "versionControl"
-    "video"
-    "xprofile"
-    # keep-sorted end
-  ];
-}
+args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) host lib;
+      isHome = host == "home";
+      keyboard = lib.optional isHome "keyboard";
+    in
+    {
+      enable = [
+        # keep-sorted start
+        "anime"
+        "audio"
+        "browser"
+        "chat"
+        "document"
+        "flashcard"
+        "git"
+        "image"
+        "math"
+        "search"
+        "system"
+        "terminal"
+        "translate"
+        "versionControl"
+        "video"
+        "xprofile"
+        # keep-sorted end
+      ] ++ keyboard;
+    }
+  )
+)
