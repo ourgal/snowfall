cea6fe0e26ca619786612e78db21f28c7ac03312
ourgal <git@fairever.aleeas.com>
2025-07-12 21:40:01 +0800

    feat: ✨ add qmk

--- modules/home/desktop/keyboard/default.nix
+++ modules/home/desktop/keyboard/default.nix
@@ -1,3 +1,13 @@
 # miryoku for qmk
 # https://github.com/drashna/qmk_userspace/releases/tag/miryoku
-{ nixPkgs = "vial"; }
+
+# XTips X7S
+# https://github.com/X-Tips/X7S-Keyboard
+{
+  nixPkgs = [
+    # keep-sorted start
+    "qmk"
+    "vial"
+    # keep-sorted end
+  ];
+}
