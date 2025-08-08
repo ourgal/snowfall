19a4bbc36c05d022de98e27dff954533e6add7f9
ourgal <git@fairever.aleeas.com>
2025-08-08 19:06:48 +0800

    chore: 🧹 update jujutsu config

--- modules/home/cli/versionControl/jujutsu/default.nix
+++ modules/home/cli/versionControl/jujutsu/default.nix
@@ -1,14 +1,29 @@
-{
-  myPkgs = "jj-fzf";
-  nixPkgs = "jjui";
-  progs = {
-    jujutsu.settings = {
-      user = {
-        name = "ourgal";
-        email = "git@fairever.aleeas.com";
+args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) config;
+    in
+    {
+      myPkgs = "jj-fzf";
+      nixPkgs = "jjui";
+      progs = {
+        jujutsu.settings = {
+          user = {
+            name = config.programs.git.userName;
+            email = config.programs.git.userEmail;
+          };
+          ui = {
+            default-command = "log";
+            paginate = "never";
+          };
+          template-aliases = {
+            "format_short_signature(signature)" = "signature.name()";
+          };
+        };
+        fish.shellAbbrs.jjp = "jj ci && jj branch set main -r @- && jj git push --all";
       };
-      ui.default-command = "log";
-    };
-    fish.shellAbbrs.jjp = "jj ci && jj branch set main -r @- && jj git push --all";
-  };
-}
+    }
+  )
+)
