aa177be3f3f4df5b7752c2c3e4b4f5b53ea61991
ourgal <git@fairever.aleeas.com>
2025-08-27 15:44:27 +0800

    move yt to x86 package

--- modules/home/cli/download/yt-dlp/default.nix
+++ modules/home/cli/download/yt-dlp/default.nix
@@ -6,7 +6,7 @@ args.module (
       inherit (args) config;
     in
     {
-      myPkgs = "yt";
+      myX86Pkgs = "yt";
       progs.yt-dlp.settings = {
         format = "bestvideo[ext=mp4][width<2000][height<=1200]+bestaudio[ext=m4a]/bestvideo[ext=webm][width<2000][height<=1200]+bestaudio[ext=webm]/bestvideo[width<2000][height<=1200]+bestaudio/best[width<2000][height<=1200]/best";
         concurrent-fragments = 5;
