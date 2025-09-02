0c7a884cde298119a937cf0a7e27fc361a3fcfe0
ourgal <git@fairever.aleeas.com>
2025-09-02 23:55:09 +0800

    feat: ✨ add dcfldd

--- modules/home/cli/system/default.nix
+++ modules/home/cli/system/default.nix
@@ -1,51 +1,50 @@
 args:
 args.module (
   args
-  // (
-    {
-      myPkgs = [
-        # keep-sorted start
-        "cache"
-        "ccsum"
-        "debounce"
-        "is"
-        "quickbench"
-        "recoverpy"
-        "servicer"
-        "wol"
-        "xrestrict"
-        # keep-sorted end
-      ];
-      nixPkgs = [
-        # keep-sorted start
-        "bkt"
-        "charm-freeze"
-        "exfatprogs"
-        "flamelens"
-        "geekbench"
-        "glib"
-        "handlr-regex"
-        "hot-resize"
-        "inxi"
-        "isd"
-        "kbt" # keyboard tester
-        "kmon"
-        "nixos-facter"
-        "pinentry-all"
-        "pipx"
-        "screenkey"
-        "sequin"
-        "sysz"
-        "toolbox"
-        "topgrade"
-        "tray-tui"
-        "ugm"
-        "wmctrl"
-        "xdotool"
-        # keep-sorted end
-      ];
-      nixX86Pkgs = "intentrace";
-      xorgPkgs = "xkill";
-    }
-  )
+  // ({
+    myPkgs = [
+      # keep-sorted start
+      "cache"
+      "ccsum"
+      "debounce"
+      "is"
+      "quickbench"
+      "recoverpy"
+      "servicer"
+      "wol"
+      "xrestrict"
+      # keep-sorted end
+    ];
+    nixPkgs = [
+      # keep-sorted start
+      "bkt"
+      "charm-freeze"
+      "exfatprogs"
+      "dcfldd"
+      "flamelens"
+      "geekbench"
+      "glib"
+      "handlr-regex"
+      "hot-resize"
+      "inxi"
+      "isd"
+      "kbt" # keyboard tester
+      "kmon"
+      "nixos-facter"
+      "pinentry-all"
+      "pipx"
+      "screenkey"
+      "sequin"
+      "sysz"
+      "toolbox"
+      "topgrade"
+      "tray-tui"
+      "ugm"
+      "wmctrl"
+      "xdotool"
+      # keep-sorted end
+    ];
+    nixX86Pkgs = "intentrace";
+    xorgPkgs = "xkill";
+  })
 )
