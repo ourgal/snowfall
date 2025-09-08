589cb3c1bb270e9735358d1daa147d93f4f9ca31
ourgal <git@fairever.aleeas.com>
2025-09-08 16:40:05 +0800

    fix: 🐛 add ctags to vim tagbar plugin

--- modules/home/cli/editor/vim/ui/tagbar/default.nix
+++ modules/home/cli/editor/vim/ui/tagbar/default.nix
@@ -6,6 +6,7 @@ args.module (
       inherit (args) pkgs;
     in
     {
+      nixPkgs = "universal-ctags";
       progs.vim = {
         plugins = with pkgs.vimPlugins; [ tagbar ];
         extraConfig = builtins.readFile ./config.vim;
