8da67fa63645812afcb9f581abeafc8b4b0f612e
ourgal <git@fairever.aleeas.com>
2025-09-08 14:27:22 +0800

    fix: 🐛 disable gtm vim plugin when on aarch64

--- modules/home/cli/editor/vim/integration/default.nix
+++ modules/home/cli/editor/vim/integration/default.nix
@@ -1,33 +1,42 @@
-{
-  enable = [
-    # keep-sorted start
-    "chezmoi-vim"
-    "devdocs-vim"
-    "gtm-vim-plugin"
-    "meson"
-    "vim-bracketed-paste"
-    "vim-carbon-now-sh"
-    "vim-dispatch"
-    "vim-floaterm"
-    "vim-silicon"
-    "vim-tome" # tmux playbook
-    "vimux" # tmux
-    "zeavim-vim"
-    "zoxide-vim"
-    # keep-sorted end
-  ];
-  disable = [
-    # keep-sorted start
-    "dps-ghosttext-vim"
-    "editorconfig-vim"
-    "emodeline" # emacs
-    "hurl"
-    "kmonad-vim"
-    "tup"
-    "vim-ghost" # broken
-    "vim-ledger"
-    "vim-orgmode"
-    "vim-tmux-clipboard"
-    # keep-sorted end
-  ];
-}
+args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) target lib namespace;
+      inherit (lib.${namespace}) isX86;
+    in
+    {
+      enable = [
+        # keep-sorted start
+        "chezmoi-vim"
+        "devdocs-vim"
+        "meson"
+        "vim-bracketed-paste"
+        "vim-carbon-now-sh"
+        "vim-dispatch"
+        "vim-floaterm"
+        "vim-silicon"
+        "vim-tome" # tmux playbook
+        "vimux" # tmux
+        "zeavim-vim"
+        "zoxide-vim"
+        # keep-sorted end
+      ] ++ isX86 target [ "gtm-vim-plugin" ];
+      disable = [
+        # keep-sorted start
+        "dps-ghosttext-vim"
+        "editorconfig-vim"
+        "emodeline" # emacs
+        "hurl"
+        "kmonad-vim"
+        "tup"
+        "vim-ghost" # broken
+        "vim-ledger"
+        "vim-orgmode"
+        "vim-tmux-clipboard"
+        # keep-sorted end
+      ];
+    }
+  )
+)
