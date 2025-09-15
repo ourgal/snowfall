fb957eff696a5d2d0e5f12156bfa14d9120b3c59
ourgal <git@fairever.aleeas.com>
2025-09-15 21:18:54 +0800

    fix: 🐞 disable boot.initrd.systemd on n1

--- modules/nixos/common/boot/default.nix
+++ modules/nixos/common/boot/default.nix
@@ -1,9 +1,10 @@
 args:
 let
   inherit (args) namespace lib config;
-  inherit (lib.${namespace}) nixosModule enabled;
+  inherit (lib.${namespace}) nixosModule enabled disabled;
+  inherit (config.${namespace}.user) host;
   value = {
-    boot.initrd.systemd = enabled // {
+    boot.initrd.systemd = (if host != "n1" then enabled else disabled) // {
       emergencyAccess = config.users.users.root.hashedPassword;
       inherit (config.systemd) network;
     };
