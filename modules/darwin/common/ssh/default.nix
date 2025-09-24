bf5a4d09000849e557b55b8ccb31dbc567166984
ourgal <git@fairever.aleeas.com>
2025-09-25 00:08:34 +0800

    fix: 🐛 remove openssh.settings

--- modules/darwin/common/ssh/default.nix
+++ modules/darwin/common/ssh/default.nix
@@ -3,13 +3,7 @@ let
   inherit (args) namespace lib pkgs;
   inherit (lib.${namespace}) nixosModule enabled;
   value = {
-    services.openssh = enabled // {
-      # require public key authentication for better security
-      # settings.PasswordAuthentication = false;
-      # settings.KbdInteractiveAuthentication = false;
-      settings.PermitRootLogin = "yes";
-      startWhenNeeded = true;
-    };
+    services.openssh = enabled;
     environment.systemPackages = [ pkgs.${namespace}.trzsz-go ];
   };
   _args = { inherit value args; };
