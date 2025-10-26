b67de292d028483aa84b338b13c6b7ff05f34b80
ourgal <git@fairever.aleeas.com>
2025-10-26 12:16:16 +0800

    chore: 🧹 change surface kitty font size

--- modules/home/desktop/terminal/kitty/default.nix
+++ modules/home/desktop/terminal/kitty/default.nix
@@ -1,21 +1,30 @@
-{
-  progs.kitty = {
-    themeFile = "Tomorrow_Night_Bright";
-    font = {
-      name = "monospace";
-      size = 14;
-    };
-    settings = {
-      copy_on_select = "yes";
-      listen_on = "unix:/tmp/kitty";
-      enabled_layouts = "splits";
-      background_opacity = "0.8";
-      cursor_trail = 0;
-      cursor_trail_decay = "0.1 0.4";
-    };
-    keybindings = {
-      "ctrl+shift+p>u" = "kitten hints --type url --program @";
-    };
-  };
-  value.catppuccin.kitty.enable = false;
-}
+args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) host;
+    in
+    {
+      progs.kitty = {
+        themeFile = "Tomorrow_Night_Bright";
+        font = {
+          name = "monospace";
+          size = if host == "surface" then 21 else 14;
+        };
+        settings = {
+          copy_on_select = "yes";
+          listen_on = "unix:/tmp/kitty";
+          enabled_layouts = "splits";
+          background_opacity = "0.8";
+          cursor_trail = 0;
+          cursor_trail_decay = "0.1 0.4";
+        };
+        keybindings = {
+          "ctrl+shift+p>u" = "kitten hints --type url --program @";
+        };
+      };
+      value.catppuccin.kitty.enable = false;
+    }
+  )
+)
