793a6b778b8537475c60e34c246fc0ecac7eeb88
ourgal <git@fairever.aleeas.com>
2025-10-26 12:05:46 +0800

    chore: 🧹 change surface screen resolution

--- modules/home/desktop/xprofile/default.nix
+++ modules/home/desktop/xprofile/default.nix
@@ -10,7 +10,7 @@ args.module (
         home.file = {
           ".xprofile".text =
             if (host == "surface") then
-              "xrandr --output eDP-1 --scale 0.7x0.7"
+              "xrandr --output eDP-1 --mode 2736x1824"
             else if (host == "home") then
               "xrandr --output DVI-D-0 --auto --pos 0x0 --primary --output HDMI-0 --auto --pos 1920x0"
             else
