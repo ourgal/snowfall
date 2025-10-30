9661c8764969dc6cff1eb613d59beebdad0ac952
ourgal <git@fairever.aleeas.com>
2025-10-31 07:44:34 +0800

    chore: 🧹 remvoe some nix mirrors

--- modules/nixos/common/nix/default.nix
+++ modules/nixos/common/nix/default.nix
@@ -18,24 +18,15 @@ let
   value = {
     nix =
       let
-        isNotVM = builtins.elem host (
-          lib.pipe lib.${namespace}.settings.allHosts [
-            (lib.lists.remove "livecd")
-            (lib.lists.remove "livecd-arm")
-          ]
-        );
         mirrors = [
           "https://mirrors.cernet.edu.cn/nix-channels/store?priority=10"
           "https://cache.nixos.org"
           "https://nix-community.cachix.org?priority=100"
         ]
-        ++ lib.optionals isNotVM [ "http://${ip.home}:50000?priority=9" ]
         ++ lib.optionals (host == "home") [
           "http://${ip.brix}:50000?priority=9"
           "http://${ip.router}:50000?priority=9"
-        ]
-        ++ lib.optionals (host == "t4") [ "http://${ip.macmini}:50000?priority=9" ]
-        ++ lib.optionals (host == "macmini") [ "http://${ip.t4}:50000?priority=9" ];
+        ];
       in
       {
         settings = enableOpt [ "auto-optimise-store" ] // {
