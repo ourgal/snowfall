55707e33fa0ec854e7d72bec5ceefebbbc04e005
ourgal <git@fairever.aleeas.com>
2025-10-30 20:27:57 +0800

    chore: 🧹 update hash

--- packages/aicommit2/default.nix
+++ packages/aicommit2/default.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
 
   pnpmDeps = pnpm.fetchDeps {
     inherit pname version src;
-    hash = "sha256-4raHSzuc2/3o68HHfLZFcOv1vnI/c0Grlw1nip2txZA=";
+    hash = "sha256-BkJZldFkAGqDsTQ2a/NM4W1vVGNzc1HysqeYFG25Hd4=";
   };
 
   nativeBuildInputs = [
