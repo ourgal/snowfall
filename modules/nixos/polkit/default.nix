b2eebe3c9a6f33d7fc9d9f7df12e5867e2f9537e
ourgal <git@fairever.aleeas.com>
2025-11-07 19:29:52 +0800

    feat: ✨ add polkit module

---
+++ modules/nixos/polkit/default.nix
@@ -0,0 +1,23 @@
+args:
+let
+  inherit (args) namespace lib pkgs;
+  inherit (lib.${namespace}) nixosModule enabled;
+  value = {
+    security.polkit = enabled;
+    systemd.user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+  _args = { inherit value args; };
+in
+nixosModule _args
