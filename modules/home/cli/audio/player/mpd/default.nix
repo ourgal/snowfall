65996275d76f2fd46b4eb0c6104821c0afbc6960
ourgal <git@fairever.aleeas.com>
2025-11-09 23:56:24 +0800

    chore: 🧹 change mpd music directory

--- modules/home/cli/audio/player/mpd/default.nix
+++ modules/home/cli/audio/player/mpd/default.nix
@@ -1,41 +1,51 @@
-{
-  nixPkgs = [
-    # keep-sorted start
-    "inori"
-    "mmtc"
-    "mpc-cli"
-    "pms"
-    "rmpc"
-    "vimpc"
-    # keep-sorted end
-  ];
-  servs.mpd = {
-    network = {
-      listenAddress = "any";
-      startWhenNeeded = true;
-    };
-    extraConfig = ''
-      auto_update "yes"
-      audio_output {
-              type            "pipewire"
-              name            "PipeWire Sound Server"
-      }
-    '';
-  };
-  enable = [
-    # keep-sorted start
-    "ashuffle"
-    "http"
-    "mpd-mpris"
-    "mympd"
-    "ncmpcpp"
-    "sptlrx"
-    # keep-sorted end
-  ];
-  value = {
-    xdg.userDirs = {
-      enable = true;
-      createDirectories = true;
-    };
-  };
-}
+args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) config;
+    in
+    {
+      nixPkgs = [
+        # keep-sorted start
+        "inori"
+        "mmtc"
+        "mpc-cli"
+        "pms"
+        "rmpc"
+        "vimpc"
+        # keep-sorted end
+      ];
+      servs.mpd = {
+        musicDirectory = "${config.home.homeDirectory}/Music/mpd";
+        network = {
+          listenAddress = "any";
+          startWhenNeeded = true;
+        };
+        extraConfig = ''
+          auto_update "yes"
+          audio_output {
+                  type            "pipewire"
+                  name            "PipeWire Sound Server"
+          }
+        '';
+      };
+      enable = [
+        # keep-sorted start
+        "ashuffle"
+        "http"
+        "mpd-mpris"
+        "mympd"
+        "ncmpcpp"
+        "sptlrx"
+        # keep-sorted end
+      ];
+      value = {
+        xdg.userDirs = {
+          enable = true;
+          createDirectories = true;
+        };
+      };
+    }
+  )
+)
