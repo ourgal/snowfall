54b7f881292f58437e2b5a574be9c5cd11b9b130
ourgal <git@fairever.aleeas.com>
2025-11-11 15:32:20 +0800

    fix: 🐛 enable some dependences of vim plugins

--- modules/home/cli/editor/vim/integration/vim-floaterm/default.nix
+++ modules/home/cli/editor/vim/integration/vim-floaterm/default.nix
@@ -1,6 +1,20 @@
-{
-  progs.vim = {
-    plugins = "vim-floaterm";
-    extraConfig = ./config.vim;
-  };
-}
+args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) namespace enabled;
+    in
+    {
+      value.${namespace}.cli = {
+        utils.cd.broot = enabled;
+        file-manager.lf = enabled;
+        versionControl.lazygit = enabled;
+      };
+      progs.vim = {
+        plugins = "vim-floaterm";
+        extraConfig = ./config.vim;
+      };
+    }
+  )
+)
