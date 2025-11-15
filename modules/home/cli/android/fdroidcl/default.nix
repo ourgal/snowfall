884d93bce33c3f1c869e0d9efd131092862832a1
ourgal <git@fairever.aleeas.com>
2025-11-15 09:57:00 +0800

    chore: 🧹 remove smartdock from m6

--- modules/home/cli/android/fdroidcl/default.nix
+++ modules/home/cli/android/fdroidcl/default.nix
@@ -170,7 +170,6 @@ args.module (
                 ++ git
                 ++ termux
                 ++ editor
-                ++ ui
                 ++ keyboard;
               inherit repos;
             }
