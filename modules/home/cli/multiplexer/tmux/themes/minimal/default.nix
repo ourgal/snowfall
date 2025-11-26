481e584498bd418623bf7ccdd79325a4b6949298
ourgal <git@fairever.aleeas.com>
2025-11-26 22:00:47 +0800

    chore: 🧹 add tmux minimal theme config

--- modules/home/cli/multiplexer/tmux/themes/minimal/default.nix
+++ modules/home/cli/multiplexer/tmux/themes/minimal/default.nix
@@ -4,14 +4,33 @@ args.module (
   // (
     let
       inherit (args) pkgs namespace;
+      selectionBox = {
+        default = "";
+        edged = ''
+          set -g @minimal-tmux-use-arrow true
+        '';
+        rounded = ''
+          set -g @minimal-tmux-use-arrow true
+          set -g @minimal-tmux-right-arrow ""
+          set -g @minimal-tmux-left-arrow ""
+        '';
+      };
     in
     {
       progs.tmux.plugins = [
         {
           plugin = pkgs.${namespace}.minimal-tmux-status;
           extraConfig = ''
-            set -g @minimal-tmux-justify "left"
+            set -g @minimal-tmux-justify "centre"
             set -g @minimal-tmux-status "top"
+
+            set -g @minimal-tmux-indicator-str " tmux "
+            set -g @minimal-tmux-indicator true
+
+            set -g @minimal-tmux-status-right " #S "
+
+            ${selectionBox.rounded}
+
           '';
         }
       ];
