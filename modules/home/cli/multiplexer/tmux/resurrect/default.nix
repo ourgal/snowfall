9a89a2353518314df06f4a158418766066b98144
ourgal <git@fairever.aleeas.com>
2025-12-03 20:48:42 +0800

    chore: 🧹 change tmux resurrect processes

--- modules/home/cli/multiplexer/tmux/resurrect/default.nix
+++ modules/home/cli/multiplexer/tmux/resurrect/default.nix
@@ -9,7 +9,7 @@ args.module (
           plugin = pkgs.tmuxPlugins.resurrect;
           extraConfig = ''
             set -g @resurrect-capture-pane-contents 'on'
-            set -g @resurrect-processes 'lazygit newsboat clifm "devbox shell" "hx *" ncmpcpp "~nnn-wrapped->nnn" "~lf->lf" rmpc "~ranger->ranger-cd" "yazi->yy"'
+            set -g @resurrect-processes 'lazygit newsboat clifm "devbox shell" "hx *" ncmpcpp "~nnn-wrapped->nnn" lf rmpc "~ranger->ranger-cd" "yazi->yy"'
           '';
         }
         {
