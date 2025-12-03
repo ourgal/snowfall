e89a64ec9be964d8b03caeda58b139db416dcb29
ourgal <git@fairever.aleeas.com>
2025-12-03 20:57:56 +0800

    chore: 🧹 set stylix theme to google-dark

--- lib/stylix/default.nix
+++ lib/stylix/default.nix
@@ -2,6 +2,7 @@ _: {
   # https://github.com/tinted-theming/schemes/tree/spec-0.11/base16
   stylix = {
     theme = builtins.head [
+      "google-dark"
       "tokyo-night-terminal-dark"
       "ayu-dark"
       "github-dark"
