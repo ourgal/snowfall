b542d6dc4882a934e2280341b46bbb560fb3926f
ourgal <git@fairever.aleeas.com>
2025-12-06 16:15:29 +0800

    chore: 🧹 add home-manager backupCommand

--- modules/home/cli/nix/home-manager/default.nix
+++ modules/home/cli/nix/home-manager/default.nix
@@ -1,5 +1,5 @@
 {
-  progs = [ "home-manager" ];
+  progs = "home-manager";
   value = {
     manual.html.enable = true;
     systemd.user.startServices = "sd-switch";
--- modules/nixos/home-manager/default.nix
+++ modules/nixos/home-manager/default.nix
@@ -1,10 +1,11 @@
 args:
 let
-  inherit (args) namespace lib;
+  inherit (args) namespace lib pkgs;
   inherit (lib.${namespace}) nixosModule;
   value = {
     home-manager = {
       extraSpecialArgs = lib.${namespace}.SpecialArgs;
+      backupCommand = lib.getExe pkgs.trash-cli;
       backupFileExtension = "backup";
     };
   };
