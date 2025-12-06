719ba0023f2a53058cc28e35f694c4538e615d46
ourgal <git@fairever.aleeas.com>
2025-12-06 15:05:45 +0800

    chore: 🧹 migrate discord

--- modules/home/desktop/chat/default.nix
+++ modules/home/desktop/chat/default.nix
@@ -1,12 +1,22 @@
-{
-  nixX86Pkgs = "discord";
-  nixPkgs = [
-    # keep-sorted start
-    "ayugram-desktop"
-    "element-desktop"
-    "kotatogram-desktop"
-    "materialgram"
-    "telegram-desktop"
-    # keep-sorted end
-  ];
-}
+args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) target lib namespace;
+      inherit (lib.${namespace}) isX86;
+    in
+    {
+      enable = isX86 target "discord";
+      nixPkgs = [
+        # keep-sorted start
+        "ayugram-desktop"
+        "element-desktop"
+        "kotatogram-desktop"
+        "materialgram"
+        "telegram-desktop"
+        # keep-sorted end
+      ];
+    }
+  )
+)
