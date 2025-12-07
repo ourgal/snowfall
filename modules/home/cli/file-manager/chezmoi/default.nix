203a1b8f8ee0cdee38acfdfc3804caa81644e166
ourgal <git@fairever.aleeas.com>
2025-12-07 17:14:55 +0800

    chore: 🧹 disable chezmoi config link

--- modules/home/cli/file-manager/chezmoi/default.nix
+++ modules/home/cli/file-manager/chezmoi/default.nix
@@ -3,27 +3,40 @@ args.module (
   args
   // (
     let
-      inherit (args) config pkgs lib;
+      inherit (args)
+        config
+        pkgs
+        lib
+        switch
+        namespace
+        cfgHome
+        ;
       ln = config.lib.file.mkOutOfStoreSymlink;
       dir = "${config.home.homeDirectory}/workspace/snowfall/modules/home/cli/file-manager/chezmoi/config";
+      cfg = cfgHome config.${namespace} ./.;
     in
     {
       nixPkgs = "chezmoi";
-      dataFiles = {
-        chezmoi = ln dir;
-      };
+      dataFiles = if cfg.link.enable then { chezmoi = ln dir; } else { };
       value = {
-        home.activation.chezmoi = config.lib.dag.entryAfter [ "reloadSystemd" ] ''
-          PATH=$PATH:${
-            lib.makeBinPath [
-              pkgs.gnumake
-              pkgs.git
-              pkgs.fnlfmt
-              pkgs.luaPackages.fennel
-            ]
-          }
-          ${lib.getExe pkgs.chezmoi} apply --force
-        '';
+        home.activation.chezmoi =
+          if cfg.link.enable then
+            config.lib.dag.entryAfter [ "reloadSystemd" ] ''
+              PATH=$PATH:${
+                lib.makeBinPath [
+                  pkgs.gnumake
+                  pkgs.git
+                  pkgs.fnlfmt
+                  pkgs.luaPackages.fennel
+                ]
+              }
+              ${lib.getExe pkgs.chezmoi} apply --force
+            ''
+          else
+            "";
+      };
+      extraOpts = {
+        link = switch;
       };
     }
   )
