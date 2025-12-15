3e57eefae75d166bf52cfd5835b461c809cd3270
ourgal <git@fairever.aleeas.com>
2025-12-15 23:11:05 +0800

    fix: 🐛 add ansible to global python

--- modules/home/dev/ansible/default.nix
+++ modules/home/dev/ansible/default.nix
@@ -1,9 +1,23 @@
-{
-  myPkgs = "ansible-language-server";
-  nixPkgs = [
-    # keep-sorted start
-    "ansible"
-    "ansible-lint"
-    # keep-sorted end
-  ];
-}
+args:
+args.module (
+  args
+  // (
+    let
+      inherit (args) namespace;
+    in
+    {
+      myPkgs = "ansible-language-server";
+      nixPkgs = [
+        # keep-sorted start
+        "ansible"
+        "ansible-lint"
+        # keep-sorted end
+      ];
+      value = {
+        ${namespace} = {
+          dev.python.global.pkgs = p: [ p.ansible ];
+        };
+      };
+    }
+  )
+)
