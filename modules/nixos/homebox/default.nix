4246fe0d31b54608988dbc8c7d22d3597adf282a
ourgal <git@fairever.aleeas.com>
2025-12-19 20:07:14 +0800

    fix: 🐛 upgrade homebox config

--- modules/nixos/homebox/default.nix
+++ modules/nixos/homebox/default.nix
@@ -15,7 +15,8 @@ let
     services = {
       homebox = enabled // {
         settings = {
-          HBOX_STORAGE_DATA = "/var/lib/homebox/data";
+          HBOX_STORAGE_PREFIX_PATH = "data";
+          HBOX_STORAGE_CONN_STRING = "file:///var/lib/homebox";
           HBOX_STORAGE_SQLITE_URL = "/var/lib/homebox/data/homebox.db?_pragma=busy_timeout=999&_pragma=journal_mode=WAL&_fk=1";
           HBOX_OPTIONS_ALLOW_REGISTRATION = "true";
           HBOX_OPTIONS_CHECK_GITHUB_RELEASE = "false";
