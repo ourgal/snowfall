72965f2643cd3f1b07115636ac72bc4584192525
ourgal <git@fairever.aleeas.com>
2026-03-18 01:53:06 +0800

    chore: update scream args

--- modules/home/desktop/system/scream/default.nix
+++ modules/home/desktop/system/scream/default.nix
@@ -3,12 +3,12 @@ args.module (
   args
   // (
     let
-      inherit (args) pkgs lib;
+      inherit (args) pkgs lib namespace;
     in
     {
       systemdServices.scream = {
         after = "pipewire-pulse.service";
-        start = lib.getExe pkgs.scream;
+        start = "${lib.getExe pkgs.scream} -u -i ${lib.${namespace}.ip.home} -p 4011";
       };
     }
   )
