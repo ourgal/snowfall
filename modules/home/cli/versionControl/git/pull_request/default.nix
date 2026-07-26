2ccf4ff1051964f804e2c1f921df5ad9a60fb5b6
ourgal <git@fairever.aleeas.com>
2026-07-26 13:15:22 +0800

    feat: ✨ add git-pr

---
+++ modules/home/cli/versionControl/git/pull_request/default.nix
@@ -0,0 +1,1 @@
+{ nixPkgs = "git-pr"; }
--- waiting.py
+++ waiting.py
@@ -234,7 +234,7 @@ with sqlite3.connect("waiting.db") as conn:
                 Categories.GIT,
                 "05/17/2025",
                 None,
-                Waiting_Status.WAITING,
+                Waiting_Status.DONE,
                 Types.PACKAGE,
             ],
             [
