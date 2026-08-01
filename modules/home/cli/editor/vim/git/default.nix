172a271221127ca3469c635b0e98820be8ed34c1
ourgal <git@fairever.aleeas.com>
2026-08-01 23:17:43 +0800

    chore: 🧹 disable gv-vim vim-flog vim-github-link vim-rhubarb

--- modules/home/cli/editor/vim/git/default.nix
+++ modules/home/cli/editor/vim/git/default.nix
@@ -3,19 +3,8 @@
     # keep-sorted start
     "conflict-marker-vim"
     "git-lens-vim"
-    "gv-vim"
-    "vim-flog"
     "vim-fugitive"
     "vim-gitgutter"
-    "vim-github-link"
-    "vim-rhubarb" # Github support
-    # keep-sorted end
-  ];
-  disable = [
-    # keep-sorted start
-    "gitv"
-    "vim-gh-line" # open the browser
-    "vim-signify"
     # keep-sorted end
   ];
 }
