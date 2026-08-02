4b15da26e2bce6eb4477a53952a4f027985fab70
ourgal <git@fairever.aleeas.com>
2026-08-02 12:01:11 +0800

    chore: 🧹 disable gtm

--- modules/home/cli/versionControl/git/time-tracking/default.nix
+++ modules/home/cli/versionControl/git/time-tracking/default.nix
@@ -1,18 +1,7 @@
-args:
-args.module (
-  args
-  // (
-    let
-      inherit (args) target lib namespace;
-      inherit (lib.${namespace}) isX86;
-    in
-    {
-      myPkgs = [
-        # keep-sorted start
-        "git-spend"
-        # keep-sorted end
-      ];
-      enable = isX86 target "gtm";
-    }
-  )
-)
+{
+  myPkgs = [
+    # keep-sorted start
+    "git-spend"
+    # keep-sorted end
+  ];
+}
