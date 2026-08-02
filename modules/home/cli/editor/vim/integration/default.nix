c037bbf775a31dc398b10a44a826390f386d6c73
ourgal <git@fairever.aleeas.com>
2026-08-02 12:00:05 +0800

    chore: 🧹 disable gtm-vim-plugin

--- modules/home/cli/editor/vim/integration/default.nix
+++ modules/home/cli/editor/vim/integration/default.nix
@@ -1,33 +1,8 @@
-args:
-args.module (
-  args
-  // (
-    let
-      inherit (args) target lib namespace;
-      inherit (lib.${namespace}) isX86;
-    in
-    {
-      enable = [
-        # keep-sorted start
-        "vim-dispatch"
-        "vim-floaterm"
-        # keep-sorted end
-      ]
-      ++ isX86 target [ "gtm-vim-plugin" ];
-      disable = [
-        # keep-sorted start
-        "dps-ghosttext-vim"
-        "editorconfig-vim"
-        "emodeline" # emacs
-        "hurl"
-        "kmonad-vim"
-        "tup"
-        "vim-ghost" # broken
-        "vim-ledger"
-        "vim-orgmode"
-        "vim-tmux-clipboard"
-        # keep-sorted end
-      ];
-    }
-  )
-)
+{
+  enable = [
+    # keep-sorted start
+    "vim-dispatch"
+    "vim-floaterm"
+    # keep-sorted end
+  ];
+}
