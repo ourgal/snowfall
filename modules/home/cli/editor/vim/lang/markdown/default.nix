f68907bf91d2e3a54e88a25285dee074f9bf385a
ourgal <git@fairever.aleeas.com>
2026-08-02 15:20:29 +0800

    chore: 🧹 disable pangu-vim

--- modules/home/cli/editor/vim/lang/markdown/default.nix
+++ modules/home/cli/editor/vim/lang/markdown/default.nix
@@ -2,16 +2,9 @@
   enable = [
     # keep-sorted start
     "markdown-preview-nvim"
-    "pangu-vim"
     "vim-markdown"
     "vim-markdown-toc"
     "wiki-vim"
-    # keep-sorted end
-  ];
-  disable = [
-    # keep-sorted start
-    "mkdx"
-    "vim-table-mode"
     # keep-sorted end
   ];
 }
