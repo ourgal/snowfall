167ac6c080b8ecdf29e494050c949693060f265c
ourgal <git@fairever.aleeas.com>
2026-08-02 22:48:59 +0800

    chore: 🧹 add vim plugin pear-tree
    disable delimitMate

--- modules/home/cli/editor/vim/pair/default.nix
+++ modules/home/cli/editor/vim/pair/default.nix
@@ -1,10 +1,9 @@
 {
   enable = [
     # keep-sorted start
-    "delimitMate" # auto quotes
+    "pear-tree"
     "switch-vim"
     "vim-endwise"
     # keep-sorted end
   ];
-  disable = "lexima-vim"; # auto quotes
 }
