35677302ce3236a6b5ff3eb032e4fb6f87673fd7
ourgal <git@fairever.aleeas.com>
2026-09-03 19:00:22 +0800

    chore: 🧹 enable mmv

--- modules/home/cli/utils/mv/bak/default.nix
+++ modules/home/cli/utils/mv/bak/default.nix
@@ -1,1 +1,23 @@
-{ nixPkgs = "mmv-go"; }
+{
+  myPkgs = [
+    # keep-sorted start
+    "classifier"
+    "dext"
+    "dlorg"
+    "files-sort"
+    # keep-sorted end
+  ];
+  nixPkgs = [
+    # keep-sorted start
+    "detox"
+    "edir"
+    "f2"
+    "massren"
+    "mnamer"
+    "nomino"
+    "regname"
+    "ren-find"
+    "rnr"
+    # keep-sorted end
+  ];
+}
